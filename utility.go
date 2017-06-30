9d8b2ff0622c0b8580231fc75552445120ab0cdf
Katie Li <katie31@berkeley.edu>
2017-06-30 15:03:58 -0700

    Configure to WAL-E settings

--- utility.go
+++ utility.go
@@ -1,14 +1,34 @@
 package extract
 
 import (
-	"log"
+	"fmt"
 	"os"
 	"time"
 )
 
-func timeTrack(start time.Time, name string) {
+type BackupTime struct {
+	Name string
+	Time time.Time
+}
+
+type TimeSlice []BackupTime
+
+func (p TimeSlice) Len() int {
+	return len(p)
+}
+
+func (p TimeSlice) Less(i, j int) bool {
+	return p[i].Time.After(p[j].Time)
+}
+
+func (p TimeSlice) Swap(i, j int) {
+	p[i], p[j] = p[j], p[i]
+}
+
+//defer timeTrack(time.Now(), "EXTRACT ALL")
+func TimeTrack(start time.Time, name string) {
 	elapsed := time.Since(start)
-	log.Printf("%s took %s", name, elapsed)
+	fmt.Printf("%s took %s\n", name, elapsed)
 }
 
 func MakeDir(name string) {
