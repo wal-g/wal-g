19bcfaffa2f542e09f52b2ce580f65aa23c5cc7e
Katie Li <katie31@berkeley.edu>
2017-07-20 10:47:08 -0700

    Add directories on excluded list

--- create.go
+++ create.go
@@ -19,25 +19,24 @@ func (bundle *Bundle) TarWalker(path string, info os.FileInfo, err error) error 
 		panic(err)
 	}
 
-	_, ok := EXCLUDE[info.Name()]
-
-	if ok && info.IsDir() {
-		return filepath.SkipDir
-	}
-
 	if bundle.Tb.Size() <= bundle.MinSize {
 		fmt.Println("---SIZE:", bundle.MinSize)
 		err = HandleTar(bundle, path, info)
+		if err == filepath.SkipDir {
+			return err
+		}
 		if err != nil {
 			panic(err)
 		}
 	} else {
 		oldTB := bundle.Tb
 		oldTB.CloseTar()
-
 		fmt.Println("------------------------------------------NEW------------------------------------------")
 		bundle.NewTarBall()
 		err = HandleTar(bundle, path, info)
+		if err == filepath.SkipDir {
+			return err
+		}
 		if err != nil {
 			panic(err)
 		}
@@ -65,7 +64,7 @@ func HandleTar(bundle TarBundle, path string, info os.FileInfo) error {
 	var hdr *tar.Header
 	var err error
 
-	if !ok {
+	if !ok && info.Mode().IsRegular() {
 		fmt.Println("------------------------------------------", fileName)
 		hdr, err = tar.FileInfoHeader(info, fileName)
 		if err != nil {
@@ -79,31 +78,43 @@ func HandleTar(bundle TarBundle, path string, info os.FileInfo) error {
 		if err != nil {
 			panic(err)
 		}
-
-		if info.Mode().IsRegular() {
-			f, err := os.Open(path)
-			if err != nil {
-				panic(err)
-			}
-
-			lim := &io.LimitedReader{
-				R: f,
-				N: int64(hdr.Size),
-			}
-
-			fmt.Println("Writing tar ...")
-
-			_, err = io.Copy(tarWriter, lim)
-
-			if err != nil {
-				panic(err)
-			}
-			tarBall.SetSize(hdr.Size)
-			f.Close()
+		f, err := os.Open(path)
+		if err != nil {
+			panic(err)
 		}
+		lim := &io.LimitedReader{
+			R: f,
+			N: int64(hdr.Size),
+		}
+
+		fmt.Println("Writing tar ...")
+
+		_, err = io.Copy(tarWriter, lim)
+		if err != nil {
+			panic(err)
+		}
+
+		tarBall.SetSize(hdr.Size)
+		f.Close()
+	} else if ok && info.Mode().IsDir() {
+		fmt.Println("------------------------------------------EXCLUDED")
+		fmt.Println("------------------------------------------", fileName)
+		hdr, err = tar.FileInfoHeader(info, fileName)
+		if err != nil {
+			panic(err)
+		}
+
+		hdr.Name = strings.TrimPrefix(path, tarBall.Trim())
+		fmt.Println("NAME:", hdr.Name)
+
+		err = tarWriter.WriteHeader(hdr)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("RUNNING:", tarBall.Size())
+		return filepath.SkipDir
 	}
 
 	fmt.Println("RUNNING:", tarBall.Size())
-
 	return nil
 }
