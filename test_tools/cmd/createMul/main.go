750edc687418a1d49e3097a3e032a1ed80cda1d0
Katie Li <katie31@berkeley.edu>
2017-08-16 16:03:08 -0700

    fix test tools

---
+++ test_tools/cmd/createMul/main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/katie31/wal-g"
+	"github.com/katie31/wal-g/testTools"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime/pprof"
+	"strconv"
+	"time"
+)
+
+var profile bool
+var mem bool
+var nop bool
+var s3 bool
+var outDir string
+
+func init() {
+	flag.BoolVar(&profile, "p", false, "Profiler (false by default)")
+	flag.BoolVar(&mem, "m", false, "Memory profiler (false by default)")
+	flag.BoolVar(&nop, "n", false, "Use a NOP writer (false by default).")
+	flag.BoolVar(&s3, "s", false, "Upload compressed tar files to s3 (write to disk by default)")
+	flag.StringVar(&outDir, "out", "", "Directory compressed tar files will be written to (unset by default)")
+}
+
+func main() {
+	flag.Parse()
+	all := flag.Args()
+	part, err := strconv.Atoi(all[0])
+	if err != nil {
+		panic(err)
+	}
+	in := all[1]
+
+	bundle := &walg.Bundle{
+		MinSize: int64(part),
+	}
+
+	if nop {
+		bundle.Tbm = &tools.NOPTarBallMaker{
+			BaseDir: filepath.Base(in),
+			Trim:    in,
+			Nop:     true,
+		}
+	} else if !s3 && outDir == "" {
+		fmt.Printf("Please provide a directory to write to.\n")
+		os.Exit(1)
+	} else if !s3 {
+		if profile {
+			f, err := os.Create("cpu.prof")
+			if err != nil {
+				log.Fatal(err)
+			}
+			pprof.StartCPUProfile(f)
+			defer pprof.StopCPUProfile()
+		}
+
+		if mem {
+			f, err := os.Create("mem.prof")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			pprof.WriteHeapProfile(f)
+			f.Close()
+		}
+
+		bundle.Tbm = &tools.FileTarBallMaker{
+			BaseDir: filepath.Base(in),
+			Trim:    in,
+			Out:     outDir,
+		}
+		os.MkdirAll(outDir, 0766)
+
+	} else if s3 {
+		tu, _, _ := walg.Configure()
+		c, err := walg.Connect()
+		if err != nil {
+			panic(err)
+		}
+
+		n, err := walg.StartBackup(c, time.Now().String())
+		if err != nil {
+			fmt.Printf("%+v\n", err)
+			os.Exit(1)
+		}
+		
+		bundle.Tbm = &walg.S3TarBallMaker{
+			BaseDir:  filepath.Base(in),
+			Trim:     in,
+			BkupName: n,
+			Tu:       tu,
+		}
+
+		bundle.NewTarBall()
+		bundle.HandleLabelFiles(c)
+
+	}
+
+	bundle.NewTarBall()
+	defer tools.TimeTrack(time.Now(), "MAIN")
+	fmt.Println("Walking ...")
+	err = filepath.Walk(in, bundle.TarWalker)
+	if err != nil {
+		panic(err)
+	}
+	err = bundle.Tb.CloseTar()
+	if err != nil {
+		panic(err)
+	}
+	err = bundle.Tb.Finish()
+	if err != nil {
+		panic(err)
+	}
+
+}
---
+++ test_tools/tarballMaker.go
@@ -0,0 +1,50 @@
+package tools
+
+import (
+	"github.com/katie31/wal-g"
+)
+
+// FileTarBallMaker creates a new FileTarBall
+// with the directory that files should be
+// extracted to.
+type FileTarBallMaker struct {
+	number  int
+	size    int64
+	BaseDir string
+	Trim    string
+	Out     string
+}
+
+// Make creates a new FileTarBall.
+func (f *FileTarBallMaker) Make() walg.TarBall {
+	f.number++
+	return &FileTarBall{
+		number:  f.number,
+		size:    f.size,
+		baseDir: f.BaseDir,
+		trim:    f.Trim,
+		out:     f.Out,
+	}
+}
+
+// NOPTarBallMaker creates a new NOPTarBall. Used
+// for testing purposes.
+type NOPTarBallMaker struct {
+	number  int
+	size    int64
+	BaseDir string
+	Trim    string
+	Nop     bool
+}
+
+// Make creates a new NOPTarBall.
+func (n *NOPTarBallMaker) Make() walg.TarBall {
+	n.number++
+	return &NOPTarBall{
+		number:  n.number,
+		size:    n.size,
+		baseDir: n.BaseDir,
+		nop:     n.Nop,
+		trim:    n.Trim,
+	}
+}
