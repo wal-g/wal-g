840fa74a91204180ad9c6f93acca25e4a9926706
Andrey Borodin <amborodin@acm.org>
2017-11-20 13:01:44 +0500

    Next WAL file search

--- timeline.go
+++ timeline.go
@@ -17,14 +17,14 @@ func readTimeline(conn *pgx.Conn) (timeline uint32, err error) {
 	return
 }
 
+const (
+	sizeofInt32bits = sizeofInt32 * 8
+)
+
 func ParseLsn(lsnStr string) (lsn uint64, err error) {
 	lsnArray := strings.SplitN(lsnStr, "/", 2)
 
 	//Postgres format it's LSNs as two hex numbers separated by "/"
-	const (
-		sizeofInt32     = 4
-		sizeofInt32bits = sizeofInt32 * 8
-	)
 	highLsn, err := strconv.ParseUint(lsnArray[0], 0x10, sizeofInt32bits)
 	lowLsn, err2 := strconv.ParseUint(lsnArray[1], 0x10, sizeofInt32bits)
 	if err != nil || err2 != nil {
@@ -36,8 +36,9 @@ func ParseLsn(lsnStr string) (lsn uint64, err error) {
 }
 
 const (
-	walSegmentSize = uint64(16 * 1024 * 1024) // xlog.c line 113
-	walFileFormat  = "%08X%08X%08X"           // xlog_internal.h line 155
+	walSegmentSize        = uint64(16 * 1024 * 1024)     // xlog.c line 113
+	walFileFormat         = "%08X%08X%08X"               // xlog_internal.h line 155
+	xLogSegmentsPerXLogId = 0x100000000 / walSegmentSize // xlog_internal.h line 101
 )
 
 func WALFileName(lsn uint64, conn *pgx.Conn) (string, uint32, error) {
@@ -46,8 +47,37 @@ func WALFileName(lsn uint64, conn *pgx.Conn) (string, uint32, error) {
 		return "", 0, err
 	}
 
-	XLogSegmentsPerXLogId := 0x100000000 / walSegmentSize // xlog_internal.h line 101
-	logSegNo := (lsn - uint64(1)) / walSegmentSize        // xlog_internal.h line 121
+	logSegNo := (lsn - uint64(1)) / walSegmentSize // xlog_internal.h line 121
 
-	return fmt.Sprintf(walFileFormat, timeline, logSegNo/XLogSegmentsPerXLogId, logSegNo%XLogSegmentsPerXLogId), timeline, nil
+	return formatWALFileName(timeline, logSegNo), timeline, nil
+}
+func formatWALFileName(timeline uint32, logSegNo uint64) string {
+	return fmt.Sprintf(walFileFormat, timeline, logSegNo/xLogSegmentsPerXLogId, logSegNo%xLogSegmentsPerXLogId)
+}
+
+func NextWALFileName(name string) (nextname string, err error) {
+	if len(name) != 24 {
+		err = errors.New("Not a WAL file name: " + name)
+		return
+	}
+	timelineId, err := strconv.ParseUint(name[0:8], 0x10, sizeofInt32bits)
+	if err != nil {
+		return
+	}
+	logSegNoHi, err := strconv.ParseUint(name[8:16], 0x10, sizeofInt32bits)
+	if err != nil {
+		return
+	}
+	logSegNoLo, err := strconv.ParseUint(name[16:24], 0x10, sizeofInt32bits)
+	if err != nil {
+		return
+	}
+	if logSegNoLo >= xLogSegmentsPerXLogId {
+		err = errors.New("Incrorrect logSegNoLo in WAL file name: " + name)
+		return
+	}
+
+	logSegNo := logSegNoHi*xLogSegmentsPerXLogId + logSegNoLo
+	logSegNo++
+	return formatWALFileName(uint32(timelineId), logSegNo), nil
 }
--- timeline_test.go
+++ timeline_test.go
@@ -11,3 +11,36 @@ func TestLSNParse(t *testing.T) {
 		t.Fatal("LSN was not parsed correctly")
 	}
 }
+
+func TestNextWALFileName(t *testing.T) {
+	nextname, err := NextWALFileName("000000010000000000000051")
+	if err != nil || nextname != "000000010000000000000052" {
+		t.Fatal("TestNextWALFileName 000000010000000000000051 failed")
+	}
+
+	nextname, err = NextWALFileName("00000001000000000000005F")
+	if err != nil || nextname != "000000010000000000000060" {
+		t.Fatal("TestNextWALFileName 00000001000000000000005F failed")
+	}
+
+	nextname, err = NextWALFileName("0000000100000001000000FF")
+	if err != nil || nextname != "000000010000000200000000" {
+		t.Fatal("TestNextWALFileName 0000000100000001000000FF failed")
+	}
+
+	nextname, err = NextWALFileName("0000000100000001000001FF")
+	if err == nil {
+		t.Fatal("TestNextWALFileName 0000000100000001000001FF did not failed")
+	}
+
+	nextname, err = NextWALFileName("00000001000aa001000001FF")
+	if err == nil {
+		t.Fatal("TestNextWALFileName 00000001000aa001000001FF did not failed")
+	}
+
+	nextname, err = NextWALFileName("asdfasdf")
+	if err == nil {
+		t.Fatal("TestNextWALFileName asdfasdf did not failed")
+	}
+
+}
