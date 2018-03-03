25b80b83b37c62b2ce284cb8c7573b8d7cc328ab
Cory Stephenson <aevin@me.com>
2018-03-02 18:03:54 -0600

    Cleanup imports on struct.go

--- structs.go
+++ structs.go
@@ -11,12 +11,11 @@ import (
 	"sync"
 	"time"
 
-	"github.com/jackc/pgx"
-
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/s3/s3iface"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager/s3manageriface"
+	"github.com/jackc/pgx"
 	"github.com/pkg/errors"
 )
 
