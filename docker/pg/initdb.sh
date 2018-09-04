d348fe1320494f693ada94b63cbb4afd8d1afecc
vladimirlesk <icyfalcon@mail.ru>
2018-09-04 16:40:15 +0500

    wal-g delta backup test

--- docker/pg/initdb.sh
+++ docker/pg/initdb.sh
@@ -16,6 +16,22 @@ then
 else
 	echo "Everything is ok, diff returned 0 exit code"
 fi
+pgbench -i -s 20 postgres
+pg_dumpall -f /tmp/dump3
+wal-g backup-push $PGDATA
+pkill -9 postgres
+rm -rf $PGDATA
+wal-g backup-fetch $PGDATA LATEST
+echo "restore_command = 'echo \"WAL file restoration: %f, %p\"&& /usr/bin/wal-g wal-fetch \"%f\" \"%p\"'" > $PGDATA/recovery.conf
+/usr/lib/postgresql/10/bin/pg_ctl -D $PGDATA -w start
+pg_dumpall -f /tmp/dump4
+diff /tmp/dump3 /tmp/dump4
+if [ "$?" -ne 0 ]
+then
+	exit 1
+else
+	echo "Everything is ok, diff returned 0 exit code"
+fi
 pkill -9 postgres
 rm -rf $PGDATA
 exit 0
