0b182f9b73752f2c7cd094795348f3a2158a8da3
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2018-09-14 10:08:05 +0200

    Build brotli static libs and use them for linking

--- build.sh
+++ build.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 set -e
+
+readonly CWD=$PWD
+cd vendor/github.com/google/brotli/go/cbrotli
+readonly LIB_DIR=../../dist  # dist will contain binaries and it is in the google/brotli/.gitignore
+# patch cgo.go to force usage of static libraries for linking
+sed -e "s|#cgo LDFLAGS: -lbrotlicommon|#cgo CFLAGS: -I../../c/include|" \
+    -e "s|\(#cgo LDFLAGS:\) \(-lbrotli.*\)|\1 -L$LIB_DIR \2-static -lbrotlicommon-static|" \
+    -e "/brotlienc/ s|$| -lm|" -i cgo.go
+
+mkdir -p $LIB_DIR
+cd $LIB_DIR
+../configure-cmake --disable-debug
+make
+cd $CWD
+
 make
 cp ./cmd/wal-g/wal-g ./docker/pg
 docker-compose build
