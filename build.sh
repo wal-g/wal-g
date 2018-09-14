54c69d4052bb3da43e2e974f02c38cf58c54d49b
Alexander Kukushkin <alexander.kukushkin@zalando.de>
2018-09-14 10:17:30 +0200

    Make search-replace on go/cbrotli/cgo.go idempotent

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ readonly LIB_DIR=../../dist  # dist will contain binaries and it is in the googl
 # patch cgo.go to force usage of static libraries for linking
 sed -e "s|#cgo LDFLAGS: -lbrotlicommon|#cgo CFLAGS: -I../../c/include|" \
     -e "s|\(#cgo LDFLAGS:\) \(-lbrotli.*\)|\1 -L$LIB_DIR \2-static -lbrotlicommon-static|" \
-    -e "/brotlienc/ s|$| -lm|" -i cgo.go
+    -e "/ -lm$/ n; /brotlienc/ s|$| -lm|" -i cgo.go
 
 mkdir -p $LIB_DIR
 cd $LIB_DIR
