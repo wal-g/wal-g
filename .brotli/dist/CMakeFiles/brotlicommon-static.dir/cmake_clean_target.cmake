2e130c044ff3023386828cc7d8572f9fe72a69c2
vladimirlesk <icyfalcon@mail.ru>
2018-09-21 09:39:09 +0500

    stil trying to fix Travis build process

---
+++ .brotli/dist/CMakeFiles/brotlicommon-static.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libbrotlicommon-static.a"
+)
---
+++ .brotli/dist/CMakeFiles/brotlienc.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/brotlienc.dir/c/enc/backward_references.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/backward_references_hq.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/bit_cost.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/block_splitter.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/brotli_bit_stream.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/cluster.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/compress_fragment.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/compress_fragment_two_pass.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/dictionary_hash.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/encode.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/encoder_dict.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/entropy_encode.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/histogram.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/literal_cost.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/memory.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/metablock.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/static_dict.c.o"
+  "CMakeFiles/brotlienc.dir/c/enc/utf8_util.c.o"
+  "libbrotlienc.pdb"
+  "libbrotlienc.so.1.0.5"
+  "libbrotlienc.so"
+  "libbrotlienc.so.1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/brotlienc.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
