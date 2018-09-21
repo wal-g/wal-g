7ad412231f8f6dd45fb67e5d817b6ec0f607aa19
vladimirlesk <icyfalcon@mail.ru>
2018-09-21 10:16:58 +0500

    removed unnecessary c files

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-mv vendor/github.com/google/brotli/go/cbrotli/cgo.go.tmp vendor/github.com/google/brotli/go/cbrotli/cgo.go
-rm -rf vendor/github.com/google/brotli/dist/
+rm -rf vendor/github.com/google/brotli/*
+mv .brotli.tmp/* vendor/github.com/google/brotli/
+rm -rf .brotli.tmp
 docker rm wal-g_s3_1 wal-g_pg_1
