de7adcaf1a42828c16501d5adcb5242d8f07fcfb
Igor Savichev <igor@savichev.me>
2018-11-29 02:51:01 +0500

    More fast build

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-if $(docker ps --all --format '{{.Names}}' | grep wal-g_*) > /dev/null; then
+if docker ps --all --format '{{.Names}}' | grep wal-g_* > /dev/null; then
     docker rm -f $(docker ps --all --format '{{.Names}}' | grep wal-g_*)
 fi
