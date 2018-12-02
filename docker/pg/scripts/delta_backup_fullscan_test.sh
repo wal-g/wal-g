7a214d429b3454724d0b8ee766087eca3737b56a
Igor Savichev <igor@savichev.me>
2018-12-02 17:44:17 +0500

    Fix bash script

--- docker/pg/scripts/delta_backup_fullscan_test.sh
+++ docker/pg/scripts/delta_backup_fullscan_test.sh
@@ -3,38 +3,38 @@ set -e -x
 
 export WALE_S3_PREFIX=s3://fullscandeltabucket
 
-/usr/lib/postgresql/10/bin/initdb $PGDATA
+/usr/lib/postgresql/10/bin/initdb ${PGDATA}
 
 echo "archive_mode = on" >> /var/lib/postgresql/10/main/postgresql.conf
 echo "archive_command = '/usr/bin/timeout 600 /usr/bin/wal-g wal-push %p'" >> /var/lib/postgresql/10/main/postgresql.conf
 echo "archive_timeout = 600" >> /var/lib/postgresql/10/main/postgresql.conf
 
-/usr/lib/postgresql/10/bin/pg_ctl -D $PGDATA -w start
+/usr/lib/postgresql/10/bin/pg_ctl -D ${PGDATA} -w start
 
 pgbench -i -s 10 postgres
-wal-g backup-push $PGDATA
+wal-g backup-push ${PGDATA}
 
 pgbench -i -s 20 postgres
 pg_dumpall -f /tmp/dump1
 pgbench -c 2 -T 100000000 -S &
 sleep 1
-wal-g backup-push $PGDATA
+wal-g backup-push ${PGDATA}
 
 pkill -9 postgres
 
-rm -rf $PGDATA
+rm -rf ${PGDATA}
 
-wal-g backup-fetch $PGDATA LATEST
+wal-g backup-fetch ${PGDATA} LATEST
 
-echo "restore_command = 'echo \"WAL file restoration: %f, %p\"&& /usr/bin/wal-g wal-fetch \"%f\" \"%p\"'" > $PGDATA/recovery.conf
+echo "restore_command = 'echo \"WAL file restoration: %f, %p\"&& /usr/bin/wal-g wal-fetch \"%f\" \"%p\"'" > ${PGDATA}/recovery.conf
 
-/usr/lib/postgresql/10/bin/pg_ctl -D $PGDATA -w start
+/usr/lib/postgresql/10/bin/pg_ctl -D ${PGDATA} -w start
 
 pg_dumpall -f /tmp/dump2
 
 diff /tmp/dump1 /tmp/dump2
 
 pkill -9 postgres
-rm -rf $PGDATA
+rm -rf ${PGDATA}
 
 echo "Fullscan delta backup success!!!!!!"
