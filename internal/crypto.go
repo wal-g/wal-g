e1a4732db753451c8368881aaa0c009d8f23201a
Andrey <amborodin@acm.org>
2019-02-06 19:47:15 +0300

    Adjust GetKeyRingId() slightly and add bn0ir to contributors

--- internal/crypto.go
+++ internal/crypto.go
@@ -7,7 +7,6 @@ import (
 	"github.com/pkg/errors"
 	"github.com/wal-g/wal-g/internal/tracelog"
 	"io/ioutil"
-	"os"
 	"os/exec"
 	"os/user"
 	"path/filepath"
@@ -28,11 +27,7 @@ func (err GpgKeyExportError) Error() string {
 
 // GetKeyRingId extracts name of a key to use from env variable
 func GetKeyRingId() string {
-	gpgKeyRingId := os.Getenv("WALG_GPG_KEY_ID")
-	if gpgKeyRingId == "" {
-		gpgKeyRingId = os.Getenv("WALE_GPG_KEY_ID")
-	}
-	return gpgKeyRingId
+	return getSettingValue("WALG_GPG_KEY_ID")
 }
 
 const GpgBin = "gpg"
