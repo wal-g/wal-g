e326f0191b851a35e358d87e5d50ce184a02f8ce
iamrz1 <rezoan@appscode.com>
2019-02-18 10:36:17 +0600

    Standardized test

--- test/azure_folder_test.go
+++ test/azure_folder_test.go
@@ -3,13 +3,19 @@ package test
 import (
 	"github.com/stretchr/testify/assert"
 	"github.com/wal-g/wal-g/internal"
+	"os"
 	"testing"
 )
 
 func TestAzureFolder(t *testing.T) {
-	t.Skip("Credentials needed to run S3 tests")
+	t.Skip("Credentials needed to run Azure Storage tests")
 
-	storageFolder, err := internal.ConfigureAzureFolder("azure://kubedb/rezoan/sub0")
+	os.Setenv("AZURE_STORAGE_ACCOUNT", "")
+	os.Setenv("AZURE_STORAGE_ACCESS_KEY", "")
+
+	storageFolder, err := internal.ConfigureAzureFolder("azure://test-container/wal-g-test-folder/Sub0")
+
 	assert.NoError(t, err)
+
 	testStorageFolder(storageFolder, t)
 }
