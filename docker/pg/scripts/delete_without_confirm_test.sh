5d286e1996d79c9daac09ff6cdda3a98faa31f8a
g0djan <g0dj4n@gmail.com>
2019-03-21 10:18:53 +0500

    Integration tests for delete_handler (#214)
    * integration tests for delete
    
    * remove loglevel=devel

---
+++ docker/pg/scripts/delete_without_confirm_test.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+set -e -x
+
+export WALE_S3_PREFIX=s3://deletewithoutconfirm
+export WALG_USE_WAL_DELTA=true
+export WALG_DELTA_MAX_STEPS=0
+
+/usr/lib/postgresql/10/bin/initdb ${PGDATA}
+
+echo "archive_mode = on" >> /var/lib/postgresql/10/main/postgresql.conf
+echo "archive_command = '/usr/bin/timeout 600 /usr/bin/wal-g wal-push %p'" >> /var/lib/postgresql/10/main/postgresql.conf
+echo "archive_timeout = 600" >> /var/lib/postgresql/10/main/postgresql.conf
+
+/usr/lib/postgresql/10/bin/pg_ctl -D ${PGDATA} -w start
+
+for i in 1 2
+do
+    pgbench -i -s 1 postgres &
+    sleep 1
+    wal-g backup-push ${PGDATA}
+done
+
+lines_before_delete=`wal-g backup-list | wc -l`
+wal-g backup-list > /tmp/list_before_delete
+
+wal-g delete retain FULL 1
+
+lines_after_delete=`wal-g backup-list | wc -l`
+wal-g backup-list > /tmp/list_after_delete
+
+if [ $lines_before_delete -ne $lines_after_delete ];
+then
+    echo $lines_before_delete > /tmp/before_delete
+    echo $lines_after_delete > /tmp/after_delete
+    echo "Backups were deleted without --confirm"
+    diff /tmp/before_delete /tmp/after_delete
+fi
+
+diff /tmp/list_before_delete /tmp/list_after_delete
+
+pkill -9 postgres
+rm -rf ${PGDATA}
+
+echo "Delete retain FULL success!!!!!!"
