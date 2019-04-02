fa6a8c995852a6c51c9e3429e5296dbcddba5df2
Khairutdinov Daniyar <31483756+zarix908@users.noreply.github.com>
2019-04-02 10:16:25 +0500

    add FetchSentinel tests (#228)

--- test/backup_test.go
+++ test/backup_test.go
@@ -1,12 +1,14 @@
 package test
 
 import (
+	"bytes"
+	"encoding/json"
 	"github.com/stretchr/testify/assert"
 	"github.com/wal-g/wal-g/internal"
 	"testing"
 )
 
-func TestCheckExistence_Exists(t *testing.T) {
+func TestCheckExistenceWhenBackupExists(t *testing.T) {
 	folder := createMockStorageFolder()
 	backup := internal.NewBackup(folder.GetSubFolder(internal.BaseBackupPath), "base_000")
 	exists, err := backup.CheckExistence()
@@ -14,7 +16,7 @@ func TestCheckExistence_Exists(t *testing.T) {
 	assert.True(t, exists)
 }
 
-func TestCheckExistence_NotExists(t *testing.T) {
+func TestCheckExistenceWhenBackupNotExists(t *testing.T) {
 	folder := createMockStorageFolder()
 	backup := internal.NewBackup(folder.GetSubFolder(internal.BaseBackupPath), "base_321")
 	exists, err := backup.CheckExistence()
@@ -43,3 +45,37 @@ func TestIsPgControlNotRequiredForWALEBackups(t *testing.T) {
 	backup := internal.NewBackup(folder.GetSubFolder(internal.BaseBackupPath), "base_000000010000DD170000000C_00743784")
 	assert.False(t, internal.IsPgControlRequired(backup, internal.BackupSentinelDto{}))
 }
+
+func TestFetchSentinel(t *testing.T) {
+	folder := createMockStorageFolder()
+	expectedSentinel := internal.BackupSentinelDto{}
+	expectedSentinelJson, _ := json.Marshal(expectedSentinel)
+	folder.PutObject("base_789454598_backup_stop_sentinel.json", bytes.NewReader(expectedSentinelJson))
+	backup := internal.NewBackup(folder, "base_789454598")
+
+	actualSentinel, err := backup.FetchSentinel()
+
+	assert.NoError(t, err)
+	assert.Equal(t, expectedSentinel, actualSentinel)
+}
+
+func TestFetchSentinelReturnErrorWhenSentinelNotExist(t *testing.T) {
+	folder := createMockStorageFolder()
+	backup := internal.NewBackup(folder.GetSubFolder(internal.BaseBackupPath), "base_78934085033849")
+
+	_, err := backup.FetchSentinel()
+
+	assert.Error(t, err)
+}
+
+func TestFetchSentinelReturnErrorWhenSentinelUnmarshallable(t *testing.T) {
+	folder := createMockStorageFolder()
+	backup := internal.NewBackup(folder.GetSubFolder(internal.BaseBackupPath), "base_000")
+	errorMessage := "failed to unmarshal sentinel"
+
+	_, err := backup.FetchSentinel()
+
+	assert.Error(t, err)
+	assert.Equal(t, errorMessage, err.Error()[:len(errorMessage)])
+}
+
