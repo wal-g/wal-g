3099bedcb0cc2da860188cabf0730d2ff6bfe567
pohzipohzi <poh.zihow@gmail.com>
2019-06-30 15:10:37 +0800

    remove find_full option

--- docker/pg_tests/scripts/tests/delete_before_permanent_test.sh
+++ docker/pg_tests/scripts/tests/delete_before_permanent_test.sh
@@ -13,36 +13,29 @@ echo "archive_timeout = 600" >> /var/lib/postgresql/10/main/postgresql.conf
 
 /usr/lib/postgresql/10/bin/pg_ctl -D ${PGDATA} -w start
 
-for i in 1
-do
-    pgbench -i -s 1 postgres &
-    sleep 1
-    wal-g backup-push --permanent ${PGDATA}
-done
-
-echo "backup list after pushing permanent backups:"
+# push first backup as permanent
+pgbench -i -s 1 postgres &
+sleep 1
+wal-g backup-push --permanent ${PGDATA}
 wal-g backup-list
 permanent_backup_name=`wal-g backup-list | tail -n 1 | cut -f 1 -d " "`
 
+# push a few more impermanent backups
 for i in 2 3 4
 do
     pgbench -i -s 1 postgres &
     sleep 1
     wal-g backup-push ${PGDATA}
 done
-
-echo "backup list after pushing all backups:"
 wal-g backup-list
 
+# delete all backups
 last_backup_name=`wal-g backup-list | tail -n 1 | cut -f 1 -d " "`
-wal-g delete before FIND_FULL $last_backup_name --confirm
-
-echo "backup list after delete:"
+wal-g delete before $last_backup_name --confirm
 wal-g backup-list
 
-# retrieve name of first backup
+# check that permannet backup still exists
 first_backup_name=`wal-g backup-list | sed '2q;d' | cut -f 1 -d " "`
-
 if [ $first_backup_name -ne $permanent_backup_name ];
 then
     echo "permanent backup does not exist after deletion"
@@ -50,4 +43,4 @@ fi
 
 scripts/drop_pg.sh
 
-echo "Delete before time permanent success!!!!!!"
+echo "Delete before permanent success!!!!!!"
