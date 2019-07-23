d0dd0d04611b55591776c7bbfb82e8c98949958e
Ermilin Sviatoslav <ermilin.c@yandex.ru>
2019-07-23 16:20:16 +0500

    made pg tests parallel

--- docker/pg_tests/scripts/tests/delete_before_name_find_full_test.sh
+++ docker/pg_tests/scripts/tests/delete_before_name_find_full_test.sh
@@ -45,10 +45,12 @@ diff /tmp/list_tail_before_delete /tmp/list_tail_after_delete
 
 echo "____ls____"
 ls
-echo "____ls scripts____"
-ls scripts
+echo "____ls tmp____"
+ls tmp
+echo "____ls tmp/scripts____"
+ls tmp/scripts
 
 
-scripts/drop_pg.sh
+tmp/scripts/drop_pg.sh
 
 echo "Delete before FIND_FULL name success!!!!!!"
