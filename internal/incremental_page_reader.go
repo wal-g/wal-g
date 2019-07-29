7b09a762d21dc4d5cfc63f2d9183f4d9be322631
g0djan <g0dj4n@gmail.com>
2019-07-29 11:33:47 +0500

    delete deepcopy

--- internal/incremental_page_reader.go
+++ internal/incremental_page_reader.go
@@ -10,7 +10,6 @@ import (
 	"github.com/tinsane/tracelog"
 	"github.com/wal-g/wal-g/internal/ioextensions"
 	"github.com/wal-g/wal-g/utility"
-	"github.com/getlantern/deepcopy"
 )
 
 // "wi" at the head stands for "wal-g increment"
