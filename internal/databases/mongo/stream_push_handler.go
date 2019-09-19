ad3f73a91a59eff12eaa28bfde4fcaea8a092bd5
Ermilin Sviatoslav <ermilin.c@yandex.ru>
2019-09-19 17:23:22 +0500

    fix review

--- internal/databases/mongo/stream_push_handler.go
+++ internal/databases/mongo/stream_push_handler.go
@@ -15,15 +15,15 @@ func HandleStreamPush(uploader *Uploader, command []string) {
 	err := uploader.UploadStream(stream)
 	tracelog.ErrorLogger.FatalOnError(err)
 	var errorString string
-	if b, err := ioutil.ReadAll(errorStream); err == nil {
-		errorString = string(b)
+	if errorBytes, err := ioutil.ReadAll(errorStream); err == nil {
+		errorString = string(errorBytes)
 	}
 	err = waitFunc()
-	if err == nil {
-		return
+	if err != nil {
+		tracelog.ErrorLogger.Println(errorString)
+		tracelog.ErrorLogger.FatalOnError(err)
 	}
-	tracelog.ErrorLogger.Println(errorString)
-	tracelog.ErrorLogger.FatalOnError(err)
+
 }
 
 // TODO : unit tests
