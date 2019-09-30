aca153da025472ffd91a62e8ec61a8558c4bec00
Tri0L <dudin.andrey@gmail.com>
2019-10-01 01:12:35 +0300

    Added check for unknown properties Added temporary message with link to create issue if property detected as unknows wrong

--- internal/config.go
+++ internal/config.go
@@ -52,7 +52,86 @@ var (
 		TarSizeThresholdSetting:      "1073741823", // (1 << 30) - 1
 		TotalBgUploadedLimit:         "32",
 	}
+
+	AllowedSettings = map[string]bool{
+		// WAL-G core
+		"WALG_DOWNLOAD_CONCURRENCY":    true,
+		"WALG_UPLOAD_CONCURRENCY":      true,
+		"WALG_UPLOAD_DISK_CONCURRENCY": true,
+		"WALG_UPLOAD_QUEUE":            true,
+		"WALG_SENTINEL_USER_DATA":      true,
+		"WALG_PREVENT_WAL_OVERWRITE":   true,
+		"WALG_DELTA_MAX_STEPS":         true,
+		"WALG_DELTA_ORIGIN":            true,
+		"WALG_COMPRESSION_METHOD":      true,
+		"WALG_DISK_RATE_LIMIT":         true,
+		"WALG_NETWORK_RATE_LIMIT":      true,
+		"WALG_USE_WAL_DELTA":           true,
+		"WALG_LOG_LEVEL":               true,
+		"WALG_TAR_SIZE_THRESHOLD":      true,
+		"GPG_KEY_ID":                   true,
+		"WALG_PGP_KEY":                 true,
+		"WALG_PGP_KEY_PATH":            true,
+		"WALG_PGP_KEY_PASSPHRASE":      true,
+		"TOTAL_BG_UPLOADED_LIMIT":      true,
+		"WALG_STREAM_CREATE_COMMAND":   true,
+
+		// Postgres
+		"PGPORT":     true,
+		"PGUSER":     true,
+		"PGHOST":     true,
+		"PGPASSWORD": true,
+
+		// Swift
+		"WALG_SWIFT_PREFIX": true,
+		"OS_AUTH_URL":       true,
+		"OS_USERNAME":       true,
+		"OS_PASSWORD":       true,
+		"OS_TENANT_NAME":    true,
+		"OS_REGION_NAME":    true,
+
+		// AWS s3
+		"WALE_S3_PREFIX":              true,
+		"AWS_ACCESS_KEY_ID":           true,
+		"AWS_SECRET_ACCESS_KEY":       true,
+		"AWS_SESSION_TOKEN":           true,
+		"AWS_DEFAULT_REGION":          true,
+		"AWS_DEFAULT_OUTPUT":          true,
+		"AWS_PROFILE":                 true,
+		"AWS_ROLE_SESSION_NAME":       true,
+		"AWS_CA_BUNDLE":               true,
+		"AWS_SHARED_CREDENTIALS_FILE": true,
+		"AWS_CONFIG_FILE":             true,
+		"AWS_REGION":                  true,
+		"AWS_ENDPOINT":                true,
+		"AWS_S3_FORCE_PATH_STYLE":     true,
+		"WALG_S3_CA_CERT_FILE":        true,
+		"WALG_S3_STORAGE_CLASS":       true,
+		"WALG_S3_SSE":                 true,
+		"WALG_S3_SSE_KMS_ID":          true,
+		"WALG_CSE_KMS_ID":             true,
+		"WALG_CSE_KMS_REGION":         true,
+
+		// Azure
+		"WALG_AZ_PREFIX":         true,
+		"AZURE_STORAGE_ACCOUNT":  true,
+		"AZURE_STORAGE_KEY":      true,
+		"WALG_AZURE_BUFFER_SIZE": true,
+		"WALG_AZURE_MAX_BUFFERS": true,
+
+		// GS
+		"WALG_GS_PREFIX":                 true,
+		"GOOGLE_APPLICATION_CREDENTIALS": true,
+
+		//File
+		"WALG_FILE_PREFIX": true,
+	}
 )
+
+func IsAllowedSetting(setting string, AllowedSettings map[string]bool) (exists bool) {
+	_, exists = AllowedSettings[setting]
+	return
+}
 
 func GetSetting(key string) (value string, ok bool) {
 	if viper.IsSet(key) {
@@ -116,6 +195,22 @@ func InitConfig() {
 	err := viper.ReadInConfig()
 	if err == nil {
 		tracelog.InfoLogger.Println("Using config file:", viper.ConfigFileUsed())
+	}
+
+	// Сheck allowed settings
+	foundNotAllowed := false
+	for k := range viper.AllSettings() {
+		if !IsAllowedSetting(strings.ToUpper(k), AllowedSettings) {
+			tracelog.WarningLogger.Println(k + " is unknown")
+			foundNotAllowed = true
+		}
+	}
+
+	// TODO delete in the future
+	// Message for the first time.
+	if foundNotAllowed {
+		tracelog.WarningLogger.Println("We found that some variables in your config file detected as 'Unknown'. \n  " +
+			"If this is not right, please create issue https://github.com/wal-g/wal-g/issues/new")
 	}
 
 	// Set compiled config to ENV.
