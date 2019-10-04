51920437594df7d1464f91bd55ec1c626b759d4d
Ermilin Sviatoslav <ermilin.c@yandex.ru>
2019-10-04 12:00:33 +0500

    fix names

--- internal/backup_sentinel_dto.go
+++ internal/backup_sentinel_dto.go
@@ -16,28 +16,28 @@ type BackupSentinelDto struct {
 	Files       BackupFileList      `json:"Files"`
 	TarFileSets map[string][]string `json:"TarFileSets"`
 
-	PgVersion       int             `json:"PgVersion"`
-	BackupFinishLSN *uint64         `json:"FinishLSN"`
-	DataSize        int64           `json:"DataSize"`
-	DataTarSize     int64           `json:"DataTarSize"`
-	TablespaceSpec  *TablespaceSpec `json:"Spec"`
+	PgVersion        int             `json:"PgVersion"`
+	BackupFinishLSN  *uint64         `json:"FinishLSN"`
+	UncompressedSize int64           `json:"UncompressedSize"`
+	CompressedSize   int64           `json:"CompressedSize"`
+	TablespaceSpec   *TablespaceSpec `json:"Spec"`
 
 	UserData interface{} `json:"UserData,omitempty"`
 }
 
 // Extended metadata should describe backup in more details, but be small enough to be downloaded often
 type ExtendedMetadataDto struct {
-	StartTime      time.Time `json:"start_time"`
-	FinishTime     time.Time `json:"finish_time"`
-	DatetimeFormat string    `json:"date_fmt"`
-	Hostname       string    `json:"hostname"`
-	DataDir        string    `json:"data_dir"`
-	PgVersion      int       `json:"pg_version"`
-	StartLsn       uint64    `json:"start_lsn"`
-	FinishLsn      uint64    `json:"finish_lsn"`
-	IsPermanent    bool      `json:"is_permanent"`
-	DataSize       int64     `json:"data_size"`
-	DataTarSize    int64     `json:"data_tar_size"`
+	StartTime        time.Time `json:"start_time"`
+	FinishTime       time.Time `json:"finish_time"`
+	DatetimeFormat   string    `json:"date_fmt"`
+	Hostname         string    `json:"hostname"`
+	DataDir          string    `json:"data_dir"`
+	PgVersion        int       `json:"pg_version"`
+	StartLsn         uint64    `json:"start_lsn"`
+	FinishLsn        uint64    `json:"finish_lsn"`
+	IsPermanent      bool      `json:"is_permanent"`
+	UncompressedSize int64     `json:"uncompressed_size"`
+	CompressedSize   int64     `json:"compressed_size"`
 
 	UserData interface{} `json:"user_data,omitempty"`
 }
