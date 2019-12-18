6513f6071e652a17941f3bec7aa3e70eeb090d92
Todor Todorov <todor@todorov.uk>
2019-12-18 13:25:31 +0200

    internal/backup.go: Fixed restore from delta backup.
    This resolves issue #499.

--- internal/backup.go
+++ internal/backup.go
@@ -180,9 +180,26 @@ func setTablespacePaths(spec TablespaceSpec) error {
 		if err != nil {
 			return fmt.Errorf("Error creating folder for tablespace %v\n", err)
 		}
-		err = os.Symlink(location.Location, filepath.Join(basePrefix, location.Symlink))
-		if err != nil {
-			return fmt.Errorf("Error creating tablespace symkink %v\n", err)
+		if _, err := os.Lstat(filepath.Join(basePrefix, location.Symlink)); err == nil {
+			symlinkTarget, err := os.Readlink(filepath.Join(basePrefix, location.Symlink))
+			if err != nil {
+				return fmt.Errorf("Error reading symkink %v\n", err)
+			}
+			if symlinkTarget != location.Location {
+				errRemove := os.Remove(filepath.Join(basePrefix, location.Symlink))
+				if errRemove != nil {
+					return fmt.Errorf("Error removing tablespace symkink %v\n", errRemove)
+				}
+				errCreate := os.Symlink(location.Location, filepath.Join(basePrefix, location.Symlink))
+				if errCreate != nil {
+					return fmt.Errorf("Error creating tablespace symkink %v\n", errCreate)
+				}
+			}
+		} else {
+			err = os.Symlink(location.Location, filepath.Join(basePrefix, location.Symlink))
+			if err != nil {
+				return fmt.Errorf("Error creating tablespace symkink %v\n", err)
+			}
 		}
 	}
 	return nil
