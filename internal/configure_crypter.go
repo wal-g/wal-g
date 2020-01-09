37ebdca9fabc777baa1b6cc4056c59138ab706ac
Irina Sedova <irihka.sedova2011@yandex.ru>
2020-01-09 23:11:49 +0500

    Add explanation comment

--- internal/configure_crypter.go
+++ internal/configure_crypter.go
@@ -2,6 +2,17 @@
 
 package internal
 
+// This file contains functions that should return `nil`,
+// in order to be able to build wal-g without specific implementations of the crypter.
+// And the configure_crypter_<crypter>.go files must have a real implementation of the function.
+//
+// Thus, if the tag is missing, the condition:
+// if crypter := configure<crypter>Crypter(); crypter != nil {
+//     return crypter
+// }
+// will never be met.
+// If there is a tag, we can configure the correct implementation of crypter.
+
 import (
 	"github.com/wal-g/wal-g/internal/crypto"
 )
