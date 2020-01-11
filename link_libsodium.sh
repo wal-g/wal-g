b0e96b12a69c7707ce807f2ae018588a247c140c
Седова Ирина <irinasedova2011@gmail.com>
2020-01-11 12:21:49 +0500

    Add retry for downloading libsodium (#525)
    Co-authored-by: IrinaSedova <irihka.sedova2011@yandex.ru>

--- link_libsodium.sh
+++ link_libsodium.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 
+set -e
+
 readonly CWD=$PWD
-readonly LIBSODIUM_VERSION="libsodium-1.0.17"
+readonly LIBSODIUM_VERSION="1.0.17"
 
 test -d tmp/libsodium || mkdir -p tmp/libsodium
 
 cd tmp/libsodium
 
-curl -sL https://download.libsodium.org/libsodium/releases/$LIBSODIUM_VERSION.tar.gz -o $LIBSODIUM_VERSION.tar.gz
-tar xfz $LIBSODIUM_VERSION.tar.gz --strip-components=1
+curl --retry 5 --retry-delay 0 -sL https://github.com/jedisct1/libsodium/releases/download/$LIBSODIUM_VERSION/libsodium-$LIBSODIUM_VERSION.tar.gz -o libsodium-$LIBSODIUM_VERSION.tar.gz
+tar xfz libsodium-$LIBSODIUM_VERSION.tar.gz --strip-components=1
 
 ./configure --prefix $PWD
 make && make check && make install
