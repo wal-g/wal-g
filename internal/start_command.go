fa82eefa0b3527fa192f3fde38c42906ecfbbafd
Pavel Perekalov <perekalov@gmail.com>
2020-01-24 17:11:39 +0300

    Fix run cmd: stderr uses buffer instead of pipe

--- internal/start_command.go
+++ internal/start_command.go
@@ -1,8 +1,8 @@
 package internal
 
 import (
+	"bytes"
 	"io"
-	"io/ioutil"
 	"os/exec"
 
 	"github.com/wal-g/tracelog"
@@ -12,22 +12,15 @@ func StartCommand(command []string) (waitFunc func(), stdout io.ReadCloser) {
 	c := exec.Command(command[0], command[1:]...)
 	stdoutResult, err := c.StdoutPipe()
 	tracelog.ErrorLogger.FatalOnError(err)
-	stderrResult, err := c.StderrPipe()
-	tracelog.ErrorLogger.FatalOnError(err)
+	stderr := &bytes.Buffer{}
+	c.Stderr = stderr
 	err = c.Start()
 	waitFuncResult := c.Wait
 	tracelog.ErrorLogger.FatalOnError(err)
 	return func() {
-		var err error
-		var errorString string
-		if errorBytes, err := ioutil.ReadAll(stderrResult); err == nil {
-			errorString = string(errorBytes)
-		}
-		tracelog.ErrorLogger.FatalOnError(err)
-
 		err = waitFuncResult()
 		if err != nil {
-			tracelog.ErrorLogger.Println(errorString)
+			tracelog.ErrorLogger.Printf("%s stderr:\n%s\n", command[0], stderr.String())
 			tracelog.ErrorLogger.FatalOnError(err)
 		}
 	}, stdoutResult
