809831f9cf392f9b9262b6938688c1385fb0c92e
Volkov Denis <volkov.denis.dev@yandex.ru>
2020-03-05 12:13:34 +0500

    add optional backup-name flag

--- cmd/pg/copy.go
+++ cmd/pg/copy.go
@@ -6,9 +6,13 @@ import (
 )
 
 const (
-	backupCopyUsage            = "copy backup_name"
-	backupCopyShortDescription = "Copy backup to another storage"
-	backupCopyLongDescription  = "Copy backup with specific name from one storage to another according to configs with history(by default)"
+	backupCopyUsage            = "copy"
+	backupCopyShortDescription = "copy specific or all backups"
+	backupCopyLongDescription  = "Copy backup(s) from one storage to another according to configs (with history by default)"
+
+	backupNameFlag        = "backup-name"
+	backupNameShorthand   = "b"
+	backupNameDescription = "Copy specific backup"
 
 	fromFlag        = "from"
 	fromShorthand   = "f"
@@ -24,6 +28,7 @@ const (
 )
 
 var (
+	backupName     string
 	fromConfigFile string
 	toConfigFile   string
 	withoutHistory = false
@@ -32,18 +37,19 @@ var (
 		Use:   backupCopyUsage,
 		Short: backupCopyShortDescription,
 		Long:  backupCopyLongDescription,
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.ExactArgs(0),
 		Run:   runBackupCopy,
 	}
 )
 
 func runBackupCopy(cmd *cobra.Command, args []string) {
-	internal.HandleCopy(fromConfigFile, toConfigFile)
+	internal.HandleCopy(fromConfigFile, toConfigFile, backupName)
 }
 
 func init() {
 	Cmd.AddCommand(backupCopyCmd)
 
+	backupCopyCmd.Flags().StringVarP(&backupName, backupNameFlag, backupNameShorthand, "", backupNameDescription)
 	backupCopyCmd.Flags().StringVarP(&toConfigFile, toFlag, toShorthand, "", toDescription)
 	backupCopyCmd.Flags().StringVarP(&fromConfigFile, fromFlag, fromShorthand, "", fromDescription)
 	backupCopyCmd.Flags().BoolVarP(&withoutHistory, withoutHistoryFlag, withoutHistoryShorthand, false, withoutHistoryDescription)
