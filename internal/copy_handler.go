3b7f2e3628b1c0bc3eb95c9e6ab4b211659b8c48
Volkov Denis <volkov.denis.dev@yandex.ru>
2020-03-19 15:17:53 +0500

    added copying a specific backup

--- internal/copy_handler.go
+++ internal/copy_handler.go
@@ -2,32 +2,70 @@ package internal
 
 import (
 	"path"
+	"strings"
 
 	"github.com/spf13/viper"
 	"github.com/wal-g/storages/storage"
 	"github.com/wal-g/tracelog"
+	"github.com/wal-g/wal-g/utility"
 )
 
 // HandleCopy copy backups from current storage to another
 func HandleCopy(fromConfigFile string, toConfigFile string, backupName string) {
-	tracelog.InfoLogger.Printf("Handle backupname '%s'", backupName)
 	var fromFolder, fromError = configureFolderFromConfig(fromConfigFile)
 	var toFolder, toError = configureFolderFromConfig(toConfigFile)
 	if fromError != nil || toError != nil {
 		return
 	}
-	copyObjects(fromFolder, toFolder)
+
+	if backupName == "" {
+		tracelog.InfoLogger.Printf("Copy all backups")
+		copyAll(fromFolder, toFolder)
+		return
+	}
+
+	tracelog.InfoLogger.Printf("Handle backupname '%s'", backupName)
+	backup, err := GetBackupByName(backupName, utility.BaseBackupPath, fromFolder)
+	if err != nil {
+		tracelog.ErrorLogger.FatalOnError(err)
+		return
+	}
+
+	copyBaseBackup(backup, fromFolder, toFolder)
+	copyWals(fromFolder, toFolder)
 }
 
-func copyObjects(from storage.Folder, to storage.Folder) {
+func copyBaseBackup(backup *Backup, from storage.Folder, to storage.Folder) {
+	tracelog.InfoLogger.Print("Copy base backup")
+	var backupPrefix = path.Join(utility.BaseBackupPath, backup.Name)
+	var objects, err = storage.ListFolderRecursively(from)
+	tracelog.DebugLogger.FatalOnError(err)
+	for _, object := range objects {
+		if strings.HasPrefix(object.GetName(), backupPrefix) {
+			copyObject(object, from, to)
+		}
+	}
+}
+
+func copyWals(from storage.Folder, to storage.Folder) {
+	tracelog.InfoLogger.Print("Copy wal files")
+	var walsFolder = from.GetSubFolder(utility.WalPath)
+	copyAll(walsFolder, to)
+}
+
+func copyAll(from storage.Folder, to storage.Folder) {
 	objects, err := storage.ListFolderRecursively(from)
 	tracelog.DebugLogger.FatalOnError(err)
 	for _, object := range objects {
-		tracelog.InfoLogger.Printf("Copy %s from %s to %s ", object.GetName(), from.GetPath(), to.GetPath())
-		var readCloser, _ = from.ReadObject(object.GetName())
-		var filename = path.Join(from.GetPath(), object.GetName())
-		to.PutObject(filename, readCloser)
+		copyObject(object, from, to)
 	}
+}
+
+func copyObject(object storage.Object, from storage.Folder, to storage.Folder) {
+	tracelog.InfoLogger.Printf("Copy %s from %s to %s ", object.GetName(), from.GetPath(), to.GetPath())
+	var readCloser, _ = from.ReadObject(object.GetName())
+	var filename = path.Join(from.GetPath(), object.GetName())
+	to.PutObject(filename, readCloser)
 }
 
 func configureFolderFromConfig(configFile string) (storage.Folder, error) {
