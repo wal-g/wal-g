96db6297b2d029841e4966affc13b0f42f2547f2
vaspahomov <37302383+vaspahomov@users.noreply.github.com>
2020-04-03 09:49:40 +0500

    Add unittests for WritePrettyBackupList func (#589)

--- internal/backup_list_handler_test.go
+++ internal/backup_list_handler_test.go
@@ -1,6 +1,7 @@
 package internal_test
 
 import (
+	"bytes"
 	"errors"
 	"testing"
 	"time"
@@ -95,4 +96,101 @@ func TestHandleBackupListLogNoBackups(t *testing.T) {
 	assert.Equal(t, 1, infoLogger.Stats.PrintLnCallsCount)
 	assert.Equal(t, "No backups found", infoLogger.Stats.PrintMsg)
 	assert.Equal(t, 0, errorLogger.Stats.FatalOnErrorCallsCount)
+}
+
+func TestWritePrettyBackupList_LongColumnsValues(t *testing.T) {
+	expectedRes := `+---+-----------+--------------------------------+-----------------------------------+
+| # | NAME      | LAST MODIFIED                  | WAL SEGMENT BACKUP START          |
++---+-----------+--------------------------------+-----------------------------------+
+| 0 | backup000 | Monday, 01-Jan-01 00:00:00 UTC | veryVeryVeryVeryVeryLongWallName0 |
+| 1 | backup001 | Monday, 01-Jan-01 00:00:00 UTC | veryVeryVeryVeryVeryLongWallName1 |
++---+-----------+--------------------------------+-----------------------------------+
+`
+	backups := []internal.BackupTime{
+		{
+			BackupName:  "backup000",
+			Time:        time.Time{},
+			WalFileName: "veryVeryVeryVeryVeryLongWallName0",
+		},
+		{
+			BackupName:  "backup001",
+			Time:        time.Time{},
+			WalFileName: "veryVeryVeryVeryVeryLongWallName1",
+		},
+	}
+
+	b := bytes.Buffer{}
+	internal.WritePrettyBackupList(backups, &b)
+
+	assert.Equal(t, expectedRes, b.String())
+}
+
+func TestWritePrettyBackupList_ShortColumnsValues(t *testing.T) {
+	expectedRes := `+---+------+--------------------------------+--------------------------+
+| # | NAME | LAST MODIFIED                  | WAL SEGMENT BACKUP START |
++---+------+--------------------------------+--------------------------+
+| 0 | b0   | Monday, 01-Jan-01 00:00:00 UTC | shortWallName0           |
+| 1 | b1   | Monday, 01-Jan-01 00:00:00 UTC | shortWallName1           |
++---+------+--------------------------------+--------------------------+
+`
+	backups := []internal.BackupTime{
+		{
+			BackupName:  "b0",
+			Time:        time.Time{},
+			WalFileName: "shortWallName0",
+		},
+		{
+			BackupName:  "b1",
+			Time:        time.Time{},
+			WalFileName: "shortWallName1",
+		},
+	}
+
+	b := bytes.Buffer{}
+	internal.WritePrettyBackupList(backups, &b)
+
+	assert.Equal(t, expectedRes, b.String())
+}
+
+func TestWritePrettyBackupList_WriteNoBackupList(t *testing.T) {
+	expectedRes := `+---+------+---------------+--------------------------+
+| # | NAME | LAST MODIFIED | WAL SEGMENT BACKUP START |
++---+------+---------------+--------------------------+
++---+------+---------------+--------------------------+
+`
+	backups := make([]internal.BackupTime, 0)
+
+	b := bytes.Buffer{}
+	internal.WritePrettyBackupList(backups, &b)
+
+	assert.Equal(t, expectedRes, b.String())
+}
+
+func TestWritePrettyBackupList_EmptyColumnsValues(t *testing.T) {
+	expectedRes := `+---+------+--------------------------------+--------------------------+
+| # | NAME | LAST MODIFIED                  | WAL SEGMENT BACKUP START |
++---+------+--------------------------------+--------------------------+
+| 0 |      | Monday, 01-Jan-01 00:00:00 UTC | shortWallName0           |
+| 1 | b1   | Monday, 01-Jan-01 00:00:00 UTC |                          |
+| 2 |      | Monday, 01-Jan-01 00:00:00 UTC |                          |
++---+------+--------------------------------+--------------------------+
+`
+	backups := []internal.BackupTime{
+		{
+			Time:        time.Time{},
+			WalFileName: "shortWallName0",
+		},
+		{
+			BackupName: "b1",
+			Time:       time.Time{},
+		},
+		{
+			Time: time.Time{},
+		},
+	}
+
+	b := bytes.Buffer{}
+	internal.WritePrettyBackupList(backups, &b)
+
+	assert.Equal(t, expectedRes, b.String())
 }
