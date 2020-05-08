3b28b14158e6be43670806cd8eea7559dfa1adfd
Andrew Dai <a@andrewdai.co>
2020-05-08 00:24:38 -0700

    Cleanup and add more test cases

--- internal/bguploader_test.go
+++ internal/bguploader_test.go
@@ -15,85 +15,93 @@ import (
 	"github.com/wal-g/wal-g/testtools"
 )
 
-// This test has known race condition. We expect that background worker will
-// upload 32 files. But we have no guarantees for this.
+// This test has known race condition. Since we cannot directly control
+// BgUploader's internals, the test will wait for 1 second to allow BgUploader
+// to run.
 func TestBackgroundWALUpload(t *testing.T) {
 	tests := []struct {
 		name                 string
+		maxParallelism       int
 		numFilesOnDisk       int
 		maxNumFilesUploaded  int
-		maxParallelism       int
-		wantNumFilesUploaded int
+		wantNumFilesUploaded int // This is more of a minimum. BgUploader may actually upload more files.
 	}{
 		{
-			name:                 "parallelism=1",
+			name:                 "parallelism=0 no-op",
+			maxParallelism:       0,
+			numFilesOnDisk:       10,
+			maxNumFilesUploaded:  32,
+			wantNumFilesUploaded: 0,
+		},
+		{
+			name:                 "parallelism=1 with a few files",
+			maxParallelism:       1,
+			numFilesOnDisk:       3,
+			maxNumFilesUploaded:  32,
+			wantNumFilesUploaded: 3,
+		},
+		{
+			name:                 "parallelism=1 with lots of files",
+			maxParallelism:       1,
 			numFilesOnDisk:       100,
 			maxNumFilesUploaded:  32,
-			maxParallelism:       1,
 			wantNumFilesUploaded: 32,
 		},
 		{
 			name:                 "parallelism=10",
+			maxParallelism:       10,
 			numFilesOnDisk:       100,
 			maxNumFilesUploaded:  32,
-			maxParallelism:       10,
 			wantNumFilesUploaded: 32,
 		},
 		{
 			name:                 "parallelism=100 and fewer files on disk",
+			maxParallelism:       100,
 			numFilesOnDisk:       16,
 			maxNumFilesUploaded:  32,
-			maxParallelism:       100,
 			wantNumFilesUploaded: 16,
 		},
 		{
 			name:                 "parallelism=100 and extra files on disk",
+			maxParallelism:       100,
 			numFilesOnDisk:       100,
 			maxNumFilesUploaded:  32,
-			maxParallelism:       100,
 			wantNumFilesUploaded: 32,
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			internal.InitConfig()
-			totalBgUploadedLimitStr, ok := internal.GetSetting(internal.TotalBgUploadedLimit)
-			if !ok {
-				t.Fatalf("failed to get setting for %s", internal.TotalBgUploadedLimit)
-			}
-			numBgUploadFiles, err := strconv.Atoi(totalBgUploadedLimitStr)
-			if err != nil {
-				t.Fatalf("failed to parse setting for %s: %v", internal.TotalBgUploadedLimit, err)
-			}
+			defer cleanup(t, internal.GetDataFolderPath())
 
+			// Use generated data to test uploading WAL.
 			dir, a := setupArchiveStatus(t, "")
-			// create more files than the TotalBgUploadedLimit
 			for i := 0; i < tt.numFilesOnDisk; i++ {
 				addTestDataFile(t, dir, i)
 			}
+			defer cleanup(t, dir)
 
-			// Re-use generated data to test uploading WAL.
+			// Setup BgUploader
 			tu := testtools.NewMockWalUploader(false, false)
 			fakeASM := internal.NewFakeASM()
 			tu.ArchiveStatusManager = fakeASM
-			bu := internal.NewBgUploader(a, int32(tt.maxParallelism), int32(numBgUploadFiles), tu, false)
+			bu := internal.NewBgUploader(a, int32(tt.maxParallelism), int32(tt.maxNumFilesUploaded), tu, false)
 
-			// Look for new WALs while doing main upload
+			// Run BgUploader and wait 1 second before stopping
 			bu.Start()
-			time.Sleep(time.Second) // time to spin up new uploaders
+			// KLUDGE If maxParallelism=0, we expect to do no work. Therefore, do not wait.
+			if tt.maxParallelism > 0 {
+				time.Sleep(time.Second) // time to spin up new uploaders
+			}
 			bu.Stop()
 
-			walgDataDir := internal.GetDataFolderPath()
-
+			// Check that at least the expected number of files were created
 			for i := 0; i < tt.wantNumFilesUploaded; i++ {
 				bname := testFilename(i)
 				wasUploaded := fakeASM.IsWalAlreadyUploaded(bname)
 				assert.True(t, wasUploaded, bname+" was not marked as uploaded")
 			}
 
-			cleanup(t, dir)
-			cleanup(t, walgDataDir)
 		})
 	}
 }
