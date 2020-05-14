d4bb1ccca020ebbcca98771d4ac4cf48f439229b
usernamedt <usernamedt@protonmail.ch>
2020-05-14 22:39:18 +0500

    Change in FileTarInterpreter's unwrap logic
    This method now uses new functions for base and increment files manipulation and handles all other possible scenarios.

--- internal/tar_interpreter.go
+++ internal/tar_interpreter.go
@@ -45,42 +45,56 @@ func (tarInterpreter *FileTarInterpreter) unwrapRegularFile(fileReader io.Reader
 		}
 	}
 	fileDescription, haveFileDescription := tarInterpreter.Sentinel.Files[fileInfo.Name]
+	isIncremented := haveFileDescription && fileDescription.IsIncremented
+	// todo: clearer catchup backup detection logic
+	isCatchup := tarInterpreter.createNewIncrementalFiles
 
-	// If this file is incremental we use it's base version from incremental path
-	if haveFileDescription && tarInterpreter.Sentinel.IsIncremental() && fileDescription.IsIncremented {
-		err := ApplyFileIncrement(targetPath, fileReader, tarInterpreter.createNewIncrementalFiles)
-		return errors.Wrapf(err, "Interpret: failed to apply increment for '%s'", targetPath)
+	if localFileInfo, _ := getLocalFileInfo(targetPath); localFileInfo != nil {
+		isPageFile := isPagedFile(localFileInfo, targetPath)
+		return unwrapToExistFile(fileReader, fileInfo, targetPath, isPageFile, isIncremented, isCatchup)
 	}
-	err := PrepareDirs(fileInfo.Name, targetPath)
-	if err != nil {
-		return errors.Wrap(err, "Interpret: failed to create all directories")
-	}
-	file, err := os.OpenFile(targetPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
-	if err != nil {
-		return errors.Wrapf(err, "failed to create new file: '%s'", targetPath)
-	}
+	return unwrapToNewFile(fileReader, fileInfo, targetPath, isIncremented)
+}
 
-	_, err = io.Copy(file, fileReader)
+// unwrap the file from tar to existing local file
+func unwrapToExistFile(fileReader io.Reader, fileInfo *tar.Header, targetPath string,
+	isPageFile, isIncremented, isCatchup bool) error {
+	localFile, err := os.OpenFile(targetPath, os.O_RDWR, 0666)
 	if err != nil {
-		err1 := file.Close()
-		if err1 != nil {
-			tracelog.ErrorLogger.Printf("Interpret: failed to close file '%s' because of error: %v", targetPath, err1)
+		return err
+	}
+	defer utility.LoggedClose(localFile, "")
+	if isIncremented {
+		err := WritePagesFromIncrement(fileReader, localFile, isCatchup)
+		return errors.Wrapf(err, "Interpret: failed to write increment to file '%s'", targetPath)
+	}
+	if isCatchup {
+		err := clearLocalFile(localFile)
+		if err != nil {
+			return err
 		}
-		err1 = os.Remove(targetPath)
-		if err1 != nil {
-			tracelog.ErrorLogger.Fatalf("Interpret: failed to remove file '%s' because of error: %v", targetPath, err1)
-		}
-		return errors.Wrap(err, "Interpret: copy failed")
+		return writeLocalFile(fileReader, fileInfo, localFile)
 	}
-	defer utility.LoggedClose(file, "")
-
-	mode := os.FileMode(fileInfo.Mode)
-	if err = os.Chmod(file.Name(), mode); err != nil {
-		return errors.Wrap(err, "Interpret: chmod failed")
+	if isPageFile {
+		err := RestoreMissingPages(fileReader, localFile)
+		return errors.Wrapf(err, "Interpret: failed to restore pages for file '%s'", targetPath)
 	}
+	// skip the non-page file because newer version is already on the disk
+	return nil
+}
 
-	err = file.Sync()
-	return errors.Wrap(err, "Interpret: fsync failed")
+// unwrap file from tar to new local file
+func unwrapToNewFile(fileReader io.Reader, fileInfo *tar.Header, targetPath string, isIncremented bool) error {
+	localFile, err := createLocalFile(targetPath, fileInfo)
+	if err != nil {
+		return err
+	}
+	defer utility.LoggedClose(localFile, "")
+	if isIncremented {
+		err := CreateFileFromIncrement(fileReader, localFile)
+		return errors.Wrapf(err, "Interpret: failed to create file from increment '%s'", targetPath)
+	}
+	return writeLocalFile(fileReader, fileInfo, localFile)
 }
 
 // Interpret extracts a tar file to disk and creates needed directories.
@@ -110,6 +124,64 @@ func (tarInterpreter *FileTarInterpreter) Interpret(fileReader io.Reader, fileIn
 		}
 	}
 	return nil
+}
+
+func getLocalFileInfo(filename string) (fileInfo os.FileInfo, err error) {
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return nil, err
+	}
+	if info.IsDir() {
+		return nil, errors.New("Requested file is directory. Aborting.")
+	}
+	return info, nil
+}
+
+func createLocalFile(targetPath string, fileInfo *tar.Header) (*os.File, error) {
+	err := PrepareDirs(fileInfo.Name, targetPath)
+	if err != nil {
+		return nil, errors.Wrap(err, "Interpret: failed to create all directories")
+	}
+	file, err := os.OpenFile(targetPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return nil, errors.Wrapf(err, "failed to create new file: '%s'", targetPath)
+	}
+	return file, nil
+}
+
+func clearLocalFile(file *os.File) error {
+	err := file.Truncate(0)
+	if err != nil {
+		return err
+	}
+	_, err = file.Seek(0,0)
+	return err
+}
+
+// Write file from backup to local file
+func writeLocalFile(fileReader io.Reader, fileInfo *tar.Header, localFile *os.File) error {
+	_, err := io.Copy(localFile, fileReader)
+	if err != nil {
+		err1 := localFile.Close()
+		if err1 != nil {
+			tracelog.ErrorLogger.Printf("Interpret: failed to close localFile '%s' because of error: %v",
+				localFile.Name(), err1)
+		}
+		err1 = os.Remove(localFile.Name())
+		if err1 != nil {
+			tracelog.ErrorLogger.Fatalf("Interpret: failed to remove localFile '%s' because of error: %v",
+				localFile.Name(), err1)
+		}
+		return errors.Wrap(err, "Interpret: copy failed")
+	}
+
+	mode := os.FileMode(fileInfo.Mode)
+	if err = os.Chmod(localFile.Name(), mode); err != nil {
+		return errors.Wrap(err, "Interpret: chmod failed")
+	}
+
+	err = localFile.Sync()
+	return errors.Wrap(err, "Interpret: fsync failed")
 }
 
 // PrepareDirs makes sure all dirs exist
