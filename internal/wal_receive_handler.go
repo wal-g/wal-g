11fff2f9c05866b086e3e600b5f401c3ba7ad303
Sebastiaan Mannem <sebas@mannem.nl>
2020-07-05 09:14:44 +0200

    wal-g receivewal is starting to work...

--- internal/wal_receive_handler.go
+++ internal/wal_receive_handler.go
@@ -9,8 +9,8 @@ import (
 	"github.com/pkg/errors"
 	"github.com/wal-g/tracelog"
 	"os"
+	"regexp"
 	"time"
-	"strconv"
 )
 
 /*
@@ -21,7 +21,6 @@ Things to test:
 * Multiple versions of postgres
 * Different wal size (>=pg11)
 */
-
 
 type GenericWalReceiveError struct {
 	error
@@ -52,9 +51,20 @@ func HandleWALReceive(uploader *Uploader, tmpFilePath string) {
 
 	// Connect to postgres.
 	var XLogPos pglogrepl.LSN
-	var slotActive bool
 
-	conn, err := pgconn.Connect(context.Background(), os.Getenv("WALG_CONN_STRING"))
+	// Check WALG_SLOTNAME env variable (can be any of [0-9A-Za-z_], and 1-63 characters long)
+	slotNameRe := regexp.MustCompile(`\w{0,63}`)
+	slotName := slotNameRe.FindString(os.Getenv("WALG_SLOTNAME"))
+	if slotName == "" {
+		tracelog.InfoLogger.Println("No (correct) replication slot specified. Using default name 'walg'.")
+		slotName = "walg"
+	} else {
+		tracelog.InfoLogger.Printf("Using slotname %s: ", slotName)
+	}
+	// Implement something that gets the timeline info from the slot.
+	// Create temp conn (pgx), read slot info, return as object, close conn.
+
+	conn, err := pgconn.Connect(context.Background(), os.Getenv("WALG_CONN_STRING")+" replication=yes")
 	tracelog.ErrorLogger.FatalOnError(err)
 	defer conn.Close(context.Background())
 
@@ -63,32 +73,19 @@ func HandleWALReceive(uploader *Uploader, tmpFilePath string) {
 	tracelog.DebugLogger.Println("SystemID:", sysident.SystemID, "Timeline:", sysident.Timeline, "XLogPos:", sysident.XLogPos.String(), "DBName:", sysident.DBName)
 	XLogPos = sysident.XLogPos
 
-	// Check WALG_SLOTNAME env variable
-	slotName := os.Getenv("WALG_SLOTNAME")
-	if slotName == "" {
-		tracelog.DebugLogger.Println("Using replication slots is disabled.")
-		// Using XLogPos = sysident.XLogPos
-	} else {
-		tracelog.DebugLogger.Printf("Using slotname %s: ", slotName)
-		// Fetch replication slot info
-		result := conn.ExecParams(context.Background(), "select restart_lsn, active from pg_replication_slots where slot_name = $1 and slot_type = 'physical'", [][]byte{[]byte(slotName)}, nil, nil, nil)
-		if result.NextRow() {
-			slotActive, err = strconv.ParseBool(string(result.Values()[1]))
-			tracelog.ErrorLogger.FatalOnError(err)
-			if slotActive {
-				tracelog.ErrorLogger.FatalOnError(newGenericWalReceiveError("slot is already active"))
-			}
-			XLogPos, err = pglogrepl.ParseLSN(string(result.Values()[0]))
-			tracelog.ErrorLogger.FatalOnError(err)
-		} else {
-			// Create replication slot
-			res, err := pglogrepl.CreateReplicationSlot(context.Background(), conn, slotName, "",
-																		           pglogrepl.CreateReplicationSlotOptions{Mode: pglogrepl.PhysicalReplication})
-			tracelog.ErrorLogger.FatalOnError(err)
-			XLogPos, err = pglogrepl.ParseLSN(res.ConsistentPoint)
-			tracelog.ErrorLogger.FatalOnError(err)
-		}
-	}
+  tracelog.InfoLogger.Println("Trying to create the replication slot")
+	_, err = pglogrepl.CreateReplicationSlot(context.Background(), conn, slotName, "",
+																           pglogrepl.CreateReplicationSlotOptions{Mode: pglogrepl.PhysicalReplication})
+	// tracelog.ErrorLogger.FatalOnError(err)
+  tracelog.DebugLogger.Println("Replication slot created.")
+	// XLogPos, err = pglogrepl.ParseLSN(res.ConsistentPoint)
+	// tracelog.ErrorLogger.FatalOnError(err)
+  // tracelog.DebugLogger.Printf("Consistent point: %s", XLogPos.String())
+
+  tracelog.DebugLogger.Printf("Starting replication from %s: ", XLogPos.String())
+	err = pglogrepl.StartReplication(context.Background(), conn, slotName, XLogPos, pglogrepl.StartReplicationOptions{Timeline: sysident.Timeline, Mode: pglogrepl.PhysicalReplication})
+	tracelog.ErrorLogger.FatalOnError(err)
+  tracelog.DebugLogger.Println("Started replication")
 
 	standbyMessageTimeout := time.Second * 10
 	nextStandbyMessageDeadline := time.Now().Add(standbyMessageTimeout)
@@ -132,117 +129,5 @@ func HandleWALReceive(uploader *Uploader, tmpFilePath string) {
 		default:
 			tracelog.DebugLogger.Printf("Received unexpected message: %#v\n", msg)
 		}
-
 	}
 }
-
-
-
-
-
-
-
-	// err = conn.StartReplication (slotName string, startLsn uint64, timeline int64, pluginArguments ...string)
-	// tracelog.ErrorLogger.FatalOnError(err)
-	// while  {
-	// 	msg, err := conn.WaitForReplicationMessage(timeout time.Duration) (r *ReplicationMessage, err error)
-	// 	if err != nil && err != pgx.ErrNotificationTimeout {
-	// 		tracelog.ErrorLogger.Printf("Error %+v while waiting for Replication Message. Will keep trying.", err)
-	// 	}
-	//
-	// 	// ******************
-	// 	// Handle the message
-	// 	// ******************
-	// 	tracelog.DebugLogger.Printf("Received wal segment %+v.", msg)
-	// 	// ******************
-	//
-	// 	status, err := pgx.NewStandbyStatus(curlsn unint64)
-	// 	if err != nil {
-	// 		tracelog.ErrorLogger.Printf("Error %+v while creating StandbyStatus.", err)
-	// 	} else {
-	// 		err = SendStandbyStatus(status)
-	// 		if err != nil && err != pgx.ErrNotificationTimeout {
-	// 			tracelog.ErrorLogger.Printf("Error %+v while updating StandbyStatus.", err)
-	// 		}
-	// 	}
-	// }
-	//
-	//
-	//
-	//
-	//
-	//
-	// if uploader.ArchiveStatusManager.isWalAlreadyUploaded(tmpFilePath) {
-	// 	err := uploader.ArchiveStatusManager.unmarkWalFile(tmpFilePath)
-	//
-	// 	if err != nil {
-	// 		tracelog.ErrorLogger.Printf("unmark wal-g status for %s file failed due following error %+v", walFilePath, err)
-	// 	}
-	// 	return
-	// }
-	//
-	// uploader.UploadingFolder = uploader.UploadingFolder.GetSubFolder(utility.WalPath)
-	//
-	// concurrency, err := getMaxUploadConcurrency()
-	// tracelog.ErrorLogger.FatalOnError(err)
-	//
-	// preventWalOverwrite := viper.GetBool(PreventWalOverwriteSetting)
-	//
-	// bgUploader := NewBgUploader(walFilePath, int32(concurrency-1), uploader, preventWalOverwrite)
-	// // Look for new WALs while doing main upload
-	// bgUploader.Start()
-	// err = uploadWALFile(uploader, walFilePath, bgUploader.preventWalOverwrite)
-	// tracelog.ErrorLogger.FatalOnError(err)
-	//
-	// bgUploader.Stop()
-	// if uploader.getUseWalDelta() {
-	// 	uploader.deltaFileManager.FlushFiles(uploader.clone())
-	// }
-// } //
-//
-// // TODO : unit tests
-// // uploadWALFile from FS to the cloud
-// func uploadWALFile(uploader *Uploader, walFilePath string, preventWalOverwrite bool) error {
-// 	if preventWalOverwrite {
-// 		overwriteAttempt, err := checkWALOverwrite(uploader, walFilePath)
-// 		if overwriteAttempt {
-// 			return err
-// 		} else if err != nil {
-// 			return errors.Wrap(err, "Couldn't check whether there is an overwrite attempt due to inner error")
-// 		}
-// 	}
-// 	walFile, err := os.Open(walFilePath)
-// 	if err != nil {
-// 		return errors.Wrapf(err, "upload: could not open '%s'\n", walFilePath)
-// 	}
-// 	err = uploader.UploadWalFile(walFile)
-// 	return errors.Wrapf(err, "upload: could not Upload '%s'\n", walFilePath)
-// }
-//
-// // TODO : unit tests
-// func checkWALOverwrite(uploader *Uploader, walFilePath string) (overwriteAttempt bool, err error) {
-// 	walFileReader, err := DownloadAndDecompressWALFile(uploader.UploadingFolder, filepath.Base(walFilePath))
-// 	if err != nil {
-// 		if _, ok := err.(ArchiveNonExistenceError); ok {
-// 			err = nil
-// 		}
-// 		return false, err
-// 	}
-//
-// 	archived, err := ioutil.ReadAll(walFileReader)
-// 	if err != nil {
-// 		return false, err
-// 	}
-//
-// 	localBytes, err := ioutil.ReadFile(walFilePath)
-// 	if err != nil {
-// 		return false, err
-// 	}
-//
-// 	if !bytes.Equal(archived, localBytes) {
-// 		return true, newCantOverwriteWalFileError(walFilePath)
-// 	} else {
-// 		tracelog.InfoLogger.Printf("WAL file '%s' already archived with equal content, skipping", walFilePath)
-// 		return true, nil
-// 	}
-// }
