5e36dcb5885b24b6d0ec6e31f853af9927020043
usernamedt <usernamedt@protonmail.ch>
2020-08-03 21:07:07 +0500

    Add regular tar ball composer

---
+++ internal/regular_tar_ball_composer.go
@@ -0,0 +1,73 @@
+package internal
+
+import (
+	"archive/tar"
+	"context"
+	"github.com/wal-g/wal-g/internal/crypto"
+	"golang.org/x/sync/errgroup"
+	"os"
+)
+
+type RegularTarBallComposer struct {
+	tarBallQueue  *TarBallQueue
+	tarFilePacker *TarBallFilePacker
+	crypter       crypto.Crypter
+	files         *RegularBundleFileList
+	tarFileSets   TarFileSets
+	errorGroup    *errgroup.Group
+}
+
+func NewRegularTarBallComposer(
+	tarBallQueue *TarBallQueue,
+	tarBallFilePacker *TarBallFilePacker,
+	files *RegularBundleFileList,
+	crypter crypto.Crypter,
+) *RegularTarBallComposer {
+	errorGroup, _ := errgroup.WithContext(context.Background())
+	return &RegularTarBallComposer{
+		tarBallQueue:  tarBallQueue,
+		tarFilePacker: tarBallFilePacker,
+		crypter:       crypter,
+		files:         files,
+		tarFileSets:   make(TarFileSets),
+		errorGroup:    errorGroup,
+	}
+}
+
+func (c *RegularTarBallComposer) AddFile(info *ComposeFileInfo) {
+	tarBall := c.tarBallQueue.Deque()
+	tarBall.SetUp(c.crypter)
+	c.tarFileSets[tarBall.Name()] = append(c.tarFileSets[tarBall.Name()], info.header.Name)
+	c.errorGroup.Go(func() error {
+		err := c.tarFilePacker.PackFileIntoTar(info, tarBall)
+		if err != nil {
+			return err
+		}
+		return c.tarBallQueue.CheckSizeAndEnqueueBack(tarBall)
+	})
+}
+
+func (c *RegularTarBallComposer) AddHeader(fileInfoHeader *tar.Header, info os.FileInfo) error {
+	tarBall := c.tarBallQueue.Deque()
+	tarBall.SetUp(c.crypter)
+	defer c.tarBallQueue.EnqueueBack(tarBall)
+	c.tarFileSets[tarBall.Name()] = append(c.tarFileSets[tarBall.Name()], fileInfoHeader.Name)
+	c.files.AddFile(fileInfoHeader, info, false)
+	return tarBall.TarWriter().WriteHeader(fileInfoHeader)
+}
+
+func (c *RegularTarBallComposer) SkipFile(tarHeader *tar.Header, fileInfo os.FileInfo) {
+	c.files.AddSkippedFile(tarHeader, fileInfo)
+}
+
+func (c *RegularTarBallComposer) PackTarballs() (TarFileSets, error) {
+	err := c.errorGroup.Wait()
+	if err != nil {
+		return nil, err
+	}
+	return c.tarFileSets, nil
+}
+
+func (c *RegularTarBallComposer) GetFiles() BundleFileList {
+	return c.files
+}
