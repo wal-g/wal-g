3cba6b01ed181e1b8e34d937d5f6423d551d966f
Pavel Perekalov <perekalov@gmail.com>
2020-08-04 20:43:04 +0300

    Add mongodb backup-delete

---
+++ cmd/mongo/backup_delete.go
@@ -0,0 +1,49 @@
+package mongo
+
+import (
+	"context"
+	"os"
+	"syscall"
+
+	"github.com/wal-g/wal-g/internal"
+	"github.com/wal-g/wal-g/internal/databases/mongo"
+	"github.com/wal-g/wal-g/internal/databases/mongo/archive"
+	"github.com/wal-g/wal-g/utility"
+
+	"github.com/spf13/cobra"
+	"github.com/wal-g/tracelog"
+)
+
+const BackupDeleteShortDescription = "Deletes backup data from storage"
+
+var (
+	confirmedBackupDelete bool
+)
+
+// backupDeleteCmd represents the backupDelete command
+var backupDeleteCmd = &cobra.Command{
+	Use:   "backup-delete <backup-name>",
+	Short: BackupDeleteShortDescription,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx, cancel := context.WithCancel(context.Background())
+		signalHandler := utility.NewSignalHandler(ctx, cancel, []os.Signal{syscall.SIGINT, syscall.SIGTERM})
+		defer func() { _ = signalHandler.Close() }()
+
+		// set up storage downloader client
+		downloader, err := archive.NewStorageDownloader(archive.NewDefaultStorageSettings())
+		tracelog.ErrorLogger.FatalOnError(err)
+
+		// set up storage downloader client
+		purger, err := archive.NewStoragePurger(archive.NewDefaultStorageSettings())
+		tracelog.ErrorLogger.FatalOnError(err)
+
+		err = mongo.HandleBackupDelete(args[0], downloader, purger, !confirmedBackupDelete)
+		tracelog.ErrorLogger.FatalOnError(err)
+	},
+}
+
+func init() {
+	backupDeleteCmd.Flags().BoolVar(&confirmedBackupDelete, internal.ConfirmFlag, false, "Confirms backup deletion")
+	Cmd.AddCommand(backupDeleteCmd)
+}
---
+++ internal/databases/mongo/backup_delete_handler_test.go
@@ -0,0 +1,89 @@
+package mongo
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/wal-g/wal-g/internal/databases/mongo/archive"
+	mocks "github.com/wal-g/wal-g/internal/databases/mongo/archive/mocks"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestHandleBackupDelete(t *testing.T) {
+	type args struct {
+		backupName string
+		downloader *mocks.Downloader
+		purger     *mocks.Purger
+		dryRun     bool
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr error
+	}{
+		{
+			name: "delete_first",
+			args: args{
+				backupName: "first",
+				downloader: func() *mocks.Downloader {
+					dl := &mocks.Downloader{}
+					dl.On("BackupMeta", mock.MatchedBy(func(backupName string) bool { return backupName == "first" })).
+						Return(archive.Backup{BackupName: "first"}, nil).Once()
+					return dl
+				}(),
+				purger: func() *mocks.Purger {
+					pr := &mocks.Purger{}
+					pr.On("DeleteBackups", mock.MatchedBy(func(backups []archive.Backup) bool { return len(backups) == 1 && backups[0].BackupName == "first" })).
+						Return(nil).Once()
+					return pr
+				}(),
+				dryRun: false,
+			},
+			wantErr: nil,
+		},
+		{
+			name: "delete_first_dryrun",
+			args: args{
+				backupName: "first",
+				downloader: func() *mocks.Downloader {
+					dl := &mocks.Downloader{}
+					dl.On("BackupMeta", mock.MatchedBy(func(backupName string) bool { return backupName == "first" })).
+						Return(archive.Backup{BackupName: "first"}, nil).Once()
+					return dl
+				}(),
+				purger: &mocks.Purger{},
+				dryRun: true,
+			},
+			wantErr: nil,
+		},
+		{
+			name: "delete_nonexistent",
+			args: args{
+				backupName: "nonexistent",
+				downloader: func() *mocks.Downloader {
+					dl := &mocks.Downloader{}
+					dl.On("BackupMeta", mock.MatchedBy(func(backupName string) bool { return backupName == "nonexistent" })).
+						Return(archive.Backup{}, fmt.Errorf("can not fetch stream sentinel: test")).Once()
+					return dl
+				}(),
+				purger: &mocks.Purger{},
+				dryRun: false,
+			},
+			wantErr: fmt.Errorf("can not fetch stream sentinel: test"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := HandleBackupDelete(tt.args.backupName, tt.args.downloader, tt.args.purger, tt.args.dryRun)
+			if tt.wantErr != nil {
+				assert.EqualError(t, err, tt.wantErr.Error())
+				return
+			}
+			assert.Nil(t, err)
+			tt.args.downloader.AssertExpectations(t)
+			tt.args.purger.AssertExpectations(t)
+		})
+	}
+}
