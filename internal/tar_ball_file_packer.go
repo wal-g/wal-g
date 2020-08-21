dee53dcfbd6dfd550552ea6409b87615e4e5a070
usernamedt <usernamedt@protonmail.ch>
2020-08-21 22:54:19 +0500

    Add page verification to tar ball file packer

--- internal/tar_ball_file_packer.go
+++ internal/tar_ball_file_packer.go
@@ -2,27 +2,57 @@ package internal
 
 import (
 	"archive/tar"
+	"context"
+	"fmt"
 	"github.com/RoaringBitmap/roaring"
 	"github.com/pkg/errors"
 	"github.com/wal-g/tracelog"
 	"github.com/wal-g/wal-g/internal/ioextensions"
 	"github.com/wal-g/wal-g/utility"
+	"golang.org/x/sync/errgroup"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
+type SkippedFileError struct {
+	error
+}
+
+func newSkippedFileError(path string) SkippedFileError {
+	return SkippedFileError{errors.Errorf("File is skipped from the current backup: %s\n", path)}
+}
+
+func (err SkippedFileError) Error() string {
+	return fmt.Sprintf(tracelog.GetErrorFormatter(), err.error)
+}
+
+type IgnoredFileError struct {
+	error
+}
+
+func newIgnoredFileError(path string) IgnoredFileError {
+	return IgnoredFileError{errors.Errorf("File is ignored: %s\n", path)}
+}
+
+func (err IgnoredFileError) Error() string {
+	return fmt.Sprintf(tracelog.GetErrorFormatter(), err.error)
+}
+
 // TarBallFilePacker is used to pack bundle file into tarball.
 type TarBallFilePacker struct {
-	deltaMap         PagedFileDeltaMap
-	incrementFromLsn *uint64
-	files            BundleFiles
+	deltaMap            PagedFileDeltaMap
+	incrementFromLsn    *uint64
+	files               BundleFiles
+	verifyPageChecksums bool
 }
 
 func newTarBallFilePacker(deltaMap PagedFileDeltaMap, incrementFromLsn *uint64, files BundleFiles) *TarBallFilePacker {
 	return &TarBallFilePacker{
-		deltaMap:         deltaMap,
-		incrementFromLsn: incrementFromLsn,
-		files:            files,
+		deltaMap:            deltaMap,
+		incrementFromLsn:    incrementFromLsn,
+		files:               files,
+		verifyPageChecksums: false,
 	}
 }
 
@@ -39,58 +69,93 @@ func (p *TarBallFilePacker) UpdateDeltaMap(deltaMap PagedFileDeltaMap) {
 
 // TODO : unit tests
 func (p *TarBallFilePacker) PackFileIntoTar(cfi *ComposeFileInfo, tarBall TarBall) error {
-	var fileReader io.ReadCloser
+	fileReadCloser, err := p.createFileReadCloser(cfi)
+	if err != nil {
+		switch err.(type) {
+		case SkippedFileError:
+			p.files.AddSkippedFile(cfi.header, cfi.fileInfo)
+			return nil
+		case IgnoredFileError:
+			return nil
+		default:
+			return err
+		}
+	}
+	errorGroup, _ := errgroup.WithContext(context.Background())
+
+	if p.verifyPageChecksums {
+		var secondReadCloser io.ReadCloser
+		// newTeeReadCloser is used to provide the fileReadCloser to two consumers:
+		// fileReadCloser is needed for PackFileTo, secondReadCloser is for the page verification
+		fileReadCloser, secondReadCloser = newTeeReadCloser(fileReadCloser)
+		errorGroup.Go(func() (err error) {
+			corruptBlocks, err := verifyFile(cfi.path, cfi.fileInfo, secondReadCloser, cfi.isIncremented)
+			if err != nil {
+				return err
+			}
+			p.files.AddFileWithCorruptBlocks(cfi.header, cfi.fileInfo, cfi.isIncremented, corruptBlocks)
+			return nil
+		})
+	} else {
+		p.files.AddFile(cfi.header, cfi.fileInfo, cfi.isIncremented)
+	}
+
+	errorGroup.Go(func() error {
+		defer utility.LoggedClose(fileReadCloser, "")
+		packedFileSize, err := PackFileTo(tarBall, cfi.header, fileReadCloser)
+		if err != nil {
+			return errors.Wrap(err, "PackFileIntoTar: operation failed")
+		}
+		if packedFileSize != cfi.header.Size {
+			return newTarSizeError(packedFileSize, cfi.header.Size)
+		}
+		return nil
+	})
+
+	return errorGroup.Wait()
+}
+
+func (p *TarBallFilePacker) createFileReadCloser(cfi *ComposeFileInfo) (io.ReadCloser, error) {
+	var fileReadCloser io.ReadCloser
 	if cfi.isIncremented {
 		bitmap, err := p.getDeltaBitmapFor(cfi.path)
 		if _, ok := err.(NoBitmapFoundError); ok { // this file has changed after the start of backup, so just skip it
-			p.files.AddSkippedFile(cfi.header, cfi.fileInfo)
-			return nil
+			return nil, newSkippedFileError(cfi.path)
 		} else if err != nil {
-			return errors.Wrapf(err, "PackFileIntoTar: failed to find corresponding bitmap '%s'\n", cfi.path)
+			return nil, errors.Wrapf(err, "PackFileIntoTar: failed to find corresponding bitmap '%s'\n", cfi.path)
 		}
-		fileReader, cfi.header.Size, err = ReadIncrementalFile(cfi.path, cfi.fileInfo.Size(), *p.incrementFromLsn, bitmap)
+		fileReadCloser, cfi.header.Size, err = ReadIncrementalFile(cfi.path, cfi.fileInfo.Size(), *p.incrementFromLsn, bitmap)
 		if os.IsNotExist(err) { // File was deleted before opening
 			// We should ignore file here as if it did not exist.
-			return nil
+			return nil, newIgnoredFileError(cfi.path)
 		}
 		switch err.(type) {
 		case nil:
-			fileReader = &ioextensions.ReadCascadeCloser{
+			fileReadCloser = &ioextensions.ReadCascadeCloser{
 				Reader: &io.LimitedReader{
-					R: io.MultiReader(fileReader, &ioextensions.ZeroReader{}),
+					R: io.MultiReader(fileReadCloser, &ioextensions.ZeroReader{}),
 					N: cfi.header.Size,
 				},
-				Closer: fileReader,
+				Closer: fileReadCloser,
 			}
 		case InvalidBlockError: // fallback to full file backup
 			tracelog.WarningLogger.Printf("failed to read file '%s' as incremented\n", cfi.header.Name)
 			cfi.isIncremented = false
-			fileReader, err = startReadingFile(cfi.header, cfi.fileInfo, cfi.path, fileReader)
+			fileReadCloser, err = startReadingFile(cfi.header, cfi.fileInfo, cfi.path, fileReadCloser)
 			if err != nil {
-				return err
+				return nil, err
 			}
 		default:
-			return errors.Wrapf(err, "PackFileIntoTar: failed reading incremental file '%s'\n", cfi.path)
+			return nil, errors.Wrapf(err, "PackFileIntoTar: failed reading incremental file '%s'\n", cfi.path)
 		}
 	} else {
 		var err error
-		fileReader, err = startReadingFile(cfi.header, cfi.fileInfo, cfi.path, fileReader)
+		fileReadCloser, err = startReadingFile(cfi.header, cfi.fileInfo, cfi.path, fileReadCloser)
 		if err != nil {
-			return err
+			return nil, err
 		}
 	}
-	defer utility.LoggedClose(fileReader, "")
-	p.files.AddFile(cfi.header, cfi.fileInfo, cfi.isIncremented)
-	packedFileSize, err := PackFileTo(tarBall, cfi.header, fileReader)
-	if err != nil {
-		return errors.Wrap(err, "PackFileIntoTar: operation failed")
-	}
-
-	if packedFileSize != cfi.header.Size {
-		return newTarSizeError(packedFileSize, cfi.header.Size)
-	}
-
-	return nil
+	return fileReadCloser, nil
 }
 
 // TODO : unit tests
@@ -109,4 +174,27 @@ func startReadingFile(fileInfoHeader *tar.Header, info os.FileInfo, path string,
 		Closer: file,
 	}
 	return fileReader, nil
+}
+
+func verifyFile(path string, fileInfo os.FileInfo, fileReader io.Reader, isIncremented bool) ([]uint32, error) {
+	if !isPagedFile(fileInfo, path) {
+		_, err := io.Copy(ioutil.Discard, fileReader)
+		return nil, err
+	}
+
+	if isIncremented {
+		return VerifyPagedFileIncrement(path, fileInfo, fileReader)
+	}
+	return VerifyPagedFileBase(path, fileInfo, fileReader)
+}
+
+// TeeReadCloser creates two io.ReadClosers from one
+func newTeeReadCloser(readCloser io.ReadCloser) (io.ReadCloser, io.ReadCloser) {
+	pipeReader, pipeWriter := io.Pipe()
+
+	// teeReader is used to provide the readCloser to two consumers
+	teeReader := io.TeeReader(readCloser, pipeWriter)
+	// MultiCloser closes both pipeWriter and readCloser on Close() call
+	closer := ioextensions.NewMultiCloser([]io.Closer{readCloser, pipeWriter})
+	return &ioextensions.ReadCascadeCloser{Reader: teeReader, Closer: closer}, pipeReader
 }
