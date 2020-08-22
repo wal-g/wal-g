1f2fb8c3e9987392ba485097147aaee22489b382
usernamedt <usernamedt@protonmail.ch>
2020-08-22 16:24:22 +0500

    Add MultiCloser to ioextensions

--- internal/ioextensions/io.go
+++ internal/ioextensions/io.go
@@ -64,3 +64,24 @@ func CreateFileWith(filePath string, content io.Reader) error {
 	_, err = utility.FastCopy(file, content)
 	return err
 }
+
+type MultiCloser struct {
+	closers []io.Closer
+}
+
+func NewMultiCloser(closers []io.Closer) *MultiCloser {
+	return &MultiCloser{
+		closers: closers,
+	}
+}
+func (m *MultiCloser) Close() error {
+	var err error
+	for _, c := range m.closers {
+		// still call Close on each, even if one returns an error
+		if e := c.Close(); e != nil {
+			err = e
+		}
+	}
+	return err
+}
+
