29e87e50813647b2fe1d1ad4fdfd2f7d994b5f79
Pavel Perekalov <perekalov@gmail.com>
2020-08-26 23:25:19 +0300

    Add mongodb backup-push permanent flag

--- internal/databases/mongo/archive/backup.go
+++ internal/databases/mongo/archive/backup.go
@@ -41,14 +41,20 @@ func NewTabbedBackupListing(minwidth, tabwidth, padding int, padchar byte, flags
 func (bl *TabbedBackupListing) Backups(backups []models.Backup, output io.Writer) error {
 	writer := tabwriter.NewWriter(output, bl.minwidth, bl.tabwidth, bl.padding, bl.padchar, bl.flags)
 
-	_, err := fmt.Fprintln(writer, "name\tfinish_local_time\tts_before\tts_after")
+	_, err := fmt.Fprintln(writer, "name\tfinish_local_time\tts_before\tts_after\tdata_size\tpermanent")
 	if err != nil {
 		return err
 	}
 	for i := len(backups) - 1; i >= 0; i-- {
 		b := backups[i]
 		_, err := fmt.Fprintf(writer,
-			"%v\t%v\t%v\t%v\n", b.BackupName, b.FinishLocalTime.Format(time.RFC3339), b.MongoMeta.Before.LastMajTS, b.MongoMeta.After.LastMajTS)
+			"%v\t%v\t%v\t%v\t%d\t%v\n",
+			b.BackupName,
+			b.FinishLocalTime.Format(time.RFC3339),
+			b.MongoMeta.Before.LastMajTS,
+			b.MongoMeta.After.LastMajTS,
+			b.DataSize,
+			b.Permanent)
 		if err != nil {
 			return err
 		}
