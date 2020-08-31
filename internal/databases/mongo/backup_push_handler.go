900579df2eef63ff996ea4e79122422a8930eae5
Pavel Perekalov <perekalov@gmail.com>
2020-08-31 15:45:08 +0300

    Fix backup-push errors

--- internal/databases/mongo/backup_push_handler.go
+++ internal/databases/mongo/backup_push_handler.go
@@ -1,19 +1,23 @@
 package mongo
 
 import (
+	"fmt"
 	"os/exec"
 
 	"github.com/wal-g/wal-g/internal/databases/mongo/archive"
 	"github.com/wal-g/wal-g/utility"
-
-	"github.com/wal-g/tracelog"
 )
 
 // HandleBackupPush starts backup procedure.
 func HandleBackupPush(uploader archive.Uploader, metaProvider archive.MongoMetaProvider, permanent bool, backupCmd *exec.Cmd) error {
-	err := metaProvider.Init(permanent)
-	tracelog.ErrorLogger.FatalOnError(err)
+	if err := metaProvider.Init(permanent); err != nil {
+		return fmt.Errorf("can not initiate meta provider: %+v", err)
+	}
+
 	stdout, err := utility.StartCommandWithStdoutPipe(backupCmd)
-	tracelog.ErrorLogger.FatalOnError(err)
+	if err != nil {
+		return fmt.Errorf("can not start backup command: %+v", err)
+	}
+
 	return uploader.UploadBackup(stdout, backupCmd, metaProvider)
 }
