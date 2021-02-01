22336b4fde21342f2b67ceaecb238f8232818241
Pavel Perekalov <perekalov@gmail.com>
2021-02-01 22:52:24 +0300

    Separate mongodb oplog push/replay configuration from builder due to propper signal handling

--- cmd/mongo/oplog_replay.go
+++ cmd/mongo/oplog_replay.go
@@ -24,29 +24,41 @@ var oplogReplayCmd = &cobra.Command{
 	Short: "Fetches oplog archives from storage and applies to database",
 	Args:  cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
+		var err error
+		defer func() { tracelog.ErrorLogger.FatalOnError(err) }()
+
 		ctx, cancel := context.WithCancel(context.Background())
 		signalHandler := utility.NewSignalHandler(ctx, cancel, []os.Signal{syscall.SIGINT, syscall.SIGTERM})
 		defer func() { _ = signalHandler.Close() }()
 
 		// resolve archiving settings
 		since, err := models.TimestampFromStr(args[0])
-		tracelog.ErrorLogger.FatalOnError(err)
+		if err != nil {
+			return
+		}
 		until, err := models.TimestampFromStr(args[1])
-		tracelog.ErrorLogger.FatalOnError(err)
+		if err != nil {
+			return
+		}
 
 		// TODO: fix ugly config
 		ignoreErrCodes := make(map[string][]int32)
 		if ignoreErrCodesStr, ok := internal.GetSetting(internal.OplogReplayIgnoreErrorCodes); ok {
-			err := json.Unmarshal([]byte(ignoreErrCodesStr), &ignoreErrCodes)
-			tracelog.ErrorLogger.FatalOnError(err)
+			if err = json.Unmarshal([]byte(ignoreErrCodesStr), &ignoreErrCodes); err != nil {
+				return
+			}
 		}
 
 		mongodbUrl, err := internal.GetRequiredSetting(internal.MongoDBUriSetting)
-		tracelog.ErrorLogger.FatalOnError(err)
+		if err != nil {
+			return
+		}
 
 		var mongoClientArgs []client.Option
 		oplogAlwaysUpsert, hasOplogAlwaysUpsert, err := internal.GetBoolSetting(internal.OplogReplayOplogAlwaysUpsert)
-		tracelog.ErrorLogger.FatalOnError(err)
+		if err != nil {
+			return
+		}
 		if hasOplogAlwaysUpsert {
 			mongoClientArgs = append(mongoClientArgs, client.OplogAlwaysUpsert(oplogAlwaysUpsert))
 		}
@@ -55,32 +67,44 @@ var oplogReplayCmd = &cobra.Command{
 			mongoClientArgs = append(mongoClientArgs, client.OplogApplicationMode(client.OplogAppMode(oplogApplicationMode)))
 		}
 
-		// set up mongodb client and oplog applier
-		mongoClient, err := client.NewMongoClient(ctx, mongodbUrl, mongoClientArgs...)
-		tracelog.ErrorLogger.FatalOnError(err)
-		err = mongoClient.EnsureIsMaster(ctx)
-		tracelog.ErrorLogger.FatalOnError(err)
-
-		dbApplier := oplog.NewDBApplier(mongoClient, false, ignoreErrCodes)
-		oplogApplier := stages.NewGenericApplier(dbApplier)
-
-		// set up storage downloader client
-		downloader, err := archive.NewStorageDownloader(archive.NewDefaultStorageSettings())
-		tracelog.ErrorLogger.FatalOnError(err)
-
-		// discover archive sequence to replay
-		archives, err := downloader.ListOplogArchives()
-		tracelog.ErrorLogger.FatalOnError(err)
-		path, err := archive.SequenceBetweenTS(archives, since, until)
-		tracelog.ErrorLogger.FatalOnError(err)
-
-		// setup storage fetcher
-		oplogFetcher := stages.NewStorageFetcher(downloader, path)
-
-		// run worker cycle
-		err = mongo.HandleOplogReplay(ctx, since, until, oplogFetcher, oplogApplier)
-		tracelog.ErrorLogger.FatalOnError(err)
+		err = runOplogReplay(ctx, since, until, mongodbUrl, mongoClientArgs, ignoreErrCodes)
 	},
+}
+
+func runOplogReplay(ctx context.Context, since, until models.Timestamp, mongodbUrl string, mongoClientArgs []client.Option, ignoreErrCodes map[string][]int32) error {
+	// set up mongodb client and oplog applier
+	mongoClient, err := client.NewMongoClient(ctx, mongodbUrl, mongoClientArgs...)
+	if err != nil {
+		return err
+	}
+
+	if err = mongoClient.EnsureIsMaster(ctx); err != nil {
+		return err
+	}
+
+	dbApplier := oplog.NewDBApplier(mongoClient, false, ignoreErrCodes)
+	oplogApplier := stages.NewGenericApplier(dbApplier)
+
+	// set up storage downloader client
+	downloader, err := archive.NewStorageDownloader(archive.NewDefaultStorageSettings())
+	if err != nil {
+		return err
+	}
+	// discover archive sequence to replay
+	archives, err := downloader.ListOplogArchives()
+	if err != nil {
+		return err
+	}
+	path, err := archive.SequenceBetweenTS(archives, since, until)
+	if err != nil {
+		return err
+	}
+
+	// setup storage fetcher
+	oplogFetcher := stages.NewStorageFetcher(downloader, path)
+
+	// run worker cycle
+	return mongo.HandleOplogReplay(ctx, since, until, oplogFetcher, oplogApplier)
 }
 
 func init() {
