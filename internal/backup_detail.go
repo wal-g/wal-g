a25051252b79ab30c484ffd3a7eafa4729d68810
Alexander Slesarev <alexander.g.slesarev@gmail.com>
2021-03-11 23:12:26 +0300

    Add wrappers for []BackupTime and []BackupDetail

--- internal/backup_detail.go
+++ internal/backup_detail.go
@@ -5,3 +5,8 @@ type BackupDetail struct {
 	BackupTime
 	ExtendedMetadataDto
 }
+
+type BackupDetailSlice struct {
+	Data []BackupDetail                 `json:"data"`
+	TimeDenotation BackupTimeDenotation `json:"time_denotation"`
+}
