09b2b49608bd5184da4c316c134323f4bbe4d126
Grigory Smolkin <gsmolkin@ozon.ru>
2021-05-19 02:40:30 +0300

    [PR #950] test coverage

---
+++ docker/pg_tests/scripts/tests/archiving_ready_rename.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+set -e -x
+CONFIG_FILE="/tmp/configs/archiving_ready_rename.json"
+COMMON_CONFIG="/tmp/configs/common_config.json"
+TMP_CONFIG="/tmp/configs/tmp_config.json"
+cat ${CONFIG_FILE} > ${TMP_CONFIG}
+echo "," >> ${TMP_CONFIG}
+cat ${COMMON_CONFIG} >> ${TMP_CONFIG}
+/tmp/scripts/wrap_config_file.sh ${TMP_CONFIG}
+
+export PGDATA="/var/lib/postgresql/10/main"
+echo ${PGDATA}
+/usr/lib/postgresql/10/bin/initdb ${PGDATA}
+
+echo "archive_mode = on" >> /var/lib/postgresql/10/main/postgresql.conf
+echo "archive_timeout = 600" >> /var/lib/postgresql/10/main/postgresql.conf
+echo "archive_command = 'exit 1'" > /var/lib/postgresql/10/main/postgresql.auto.conf
+
+/usr/lib/postgresql/10/bin/pg_ctl -D ${PGDATA} -w start
+/tmp/scripts/wait_while_pg_not_ready.sh
+#wal-g --config=${TMP_CONFIG} delete everything FORCE --confirm
+
+pgbench -i -s 20 postgres
+
+/usr/lib/postgresql/10/bin/pg_ctl -D ${PGDATA} -w stop
+
+echo "logging_collector = on" >> /var/lib/postgresql/10/main/postgresql.conf
+echo "log_filename = 'postgresql.log'" >> /var/lib/postgresql/10/main/postgresql.conf
+echo "log_min_messages = debug" >> /var/lib/postgresql/10/main/postgresql.conf
+echo "archive_command = '/usr/bin/timeout 600 /usr/bin/wal-g --config=${TMP_CONFIG} wal-push %p --pg-ready-rename=true'" > /var/lib/postgresql/10/main/postgresql.auto.conf
+/usr/lib/postgresql/10/bin/pg_ctl -D ${PGDATA} -w start
+
+sleep 20
+
+cat /var/lib/postgresql/10/main/log/postgresql.log
+count=$(grep -c 'archived write-ahead log file' /var/lib/postgresql/10/main/log/postgresql.log)
+
+if [[ $count != '1' ]]; then
+    exit 1
+fi
+
+/tmp/scripts/drop_pg.sh
