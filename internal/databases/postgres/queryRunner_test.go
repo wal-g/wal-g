2ace627bebbe2862bb374ee2d89ab062e9d13a91
Alexander Slesarev <alexander.g.slesarev@gmail.com>
2021-05-31 01:47:27 +0300

    Minor changes

--- internal/databases/postgres/queryRunner_test.go
+++ internal/databases/postgres/queryRunner_test.go
@@ -19,15 +19,15 @@ func TestBuildStartBackup(t *testing.T) {
 	assert.IsType(t, err, postgres.UnsupportedPostgresVersionError{})
 
 	queryBuilder.Version = 90321
-	queryString, _ := queryBuilder.BuildStartBackup()
+	queryString, err := queryBuilder.BuildStartBackup()
 	assert.Equal(t, "SELECT case when pg_is_in_recovery() then '' else (pg_xlogfile_name_offset(lsn)).file_name end, lsn::text, pg_is_in_recovery() FROM pg_start_backup($1, true) lsn", queryString)
 
 	queryBuilder.Version = 90600
-	queryString, _ = queryBuilder.BuildStartBackup()
+	queryString, err = queryBuilder.BuildStartBackup()
 	assert.Equal(t, "SELECT case when pg_is_in_recovery() then '' else (pg_xlogfile_name_offset(lsn)).file_name end, lsn::text, pg_is_in_recovery() FROM pg_start_backup($1, true, false) lsn", queryString)
 
 	queryBuilder.Version = 100000
-	queryString, _ = queryBuilder.BuildStartBackup()
+	queryString, err = queryBuilder.BuildStartBackup()
 	assert.Equal(t, "SELECT case when pg_is_in_recovery() then '' else (pg_walfile_name_offset(lsn)).file_name end, lsn::text, pg_is_in_recovery() FROM pg_start_backup($1, true, false) lsn", queryString)
 }
 
@@ -42,14 +42,14 @@ func TestBuildStopBackup(t *testing.T) {
 	assert.IsType(t, err, postgres.UnsupportedPostgresVersionError{})
 
 	queryBuilder.Version = 90321
-	queryString, _ := queryBuilder.BuildStopBackup()
+	queryString, err := queryBuilder.BuildStopBackup()
 	assert.Equal(t, "SELECT (pg_xlogfile_name_offset(lsn)).file_name, lpad((pg_xlogfile_name_offset(lsn)).file_offset::text, 8, '0') AS file_offset, lsn::text FROM pg_stop_backup() lsn", queryString)
 
 	queryBuilder.Version = 90600
-	queryString, _ = queryBuilder.BuildStopBackup()
+	queryString, err = queryBuilder.BuildStopBackup()
 	assert.Equal(t, "SELECT labelfile, spcmapfile, lsn FROM pg_stop_backup(false)", queryString)
 
 	queryBuilder.Version = 100000
-	queryString, _ = queryBuilder.BuildStopBackup()
+	queryString, err = queryBuilder.BuildStopBackup()
 	assert.Equal(t, "SELECT labelfile, spcmapfile, lsn FROM pg_stop_backup(false)", queryString)
 }
