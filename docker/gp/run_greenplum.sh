ac0459118a030bf87960e61955a74e69df575122
nastprol <nastprol@users.noreply.github.com>
2021-06-13 21:55:44 +0500

    fix test

--- docker/gp/run_greenplum.sh
+++ docker/gp/run_greenplum.sh
@@ -8,6 +8,6 @@ cd /usr/local/gpdb_src
 
 /usr/local/gpdb_src/bin/gpssh-exkeys -h `hostname`
 
-make create-demo-cluster > /dev/nul
+make create-demo-cluster
 
 . /usr/local/gpdb_src/gpAux/gpdemo/gpdemo-env.sh
