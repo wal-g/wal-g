27e0f08e28a73bfabd086dd1157085d9d3f163e4
Mikhail Fesenko <proggga@gmail.com>
2021-06-24 02:38:31 +0300

    Add 6.2 support, add redis_cli.sh workaround

---
+++ redis_cli.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+# All args passed to this script will be passed to "redis-cli .. --rdb -"
+#
+# This script - workaround with redis-cli >= 6.2 bug, when redis-cli tries to fsync /dev/stdout and exit with error
+# see https://github.com/redis/redis/pull/9135
+#
+
+FILENAME=$(mktemp --suffix=redis-cli-stderr)
+redis-cli $@ --rdb - 2>$FILENAME
+exit_code=$?
+cat $FILENAME >&2
+if [[ $exit_code -ne 0 ]] ; then
+	grep "Fail to fsync" $FILENAME | grep -q "Invalid argument" || exit $exit_code
+fi
+rm $FILENAME || exit 0
