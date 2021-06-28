ca037d50df97787f8e46a3c8be99128848f9c04b
hariton27sy <hariton27sy@gmail.com>
2021-06-28 17:42:33 +0500

    Add delete handler

--- cmd/rocksdb/backup_delete.go
+++ cmd/rocksdb/backup_delete.go
@@ -1,34 +1,63 @@
 package rocksdb
 
-import (
-	"github.com/spf13/cobra"
-	"github.com/wal-g/wal-g/internal"
-	// "github.com/wal-g/wal-g/utility"
-)
+// import (
+// 	"strconv"
+// 	"strings"
+// 	"time"
 
-const (
-	retainAfterFlag = "retain-after"
-	retainCountFlag = "retain-count"
-)
+// 	"github.com/spf13/cobra"
+// 	"github.com/wal-g/tracelog"
+// 	"github.com/wal-g/wal-g/internal"
+// 	"github.com/wal-g/wal-g/utility"
+// 	"google.golang.org/api/storage/v1"
+// 	// "github.com/wal-g/wal-g/utility"
+// )
 
-var (
-	retainAfter string
-	retainCount uint
-)
+// const (
+// 	retainAfterFlag = "retain-after"
+// 	retainCountFlag = "retain-count"
+// 	confirmFlag     = "confirm"
+// )
 
-// deleteCmd represents the delete command
-var deleteCmd = &cobra.Command{
-	Use:   "delete",
-	Short: "Delete old backups",
-	Run:   runDelete,
-}
+// var (
+// 	retainAfter string
+// 	retainCount uint
+// 	confirmed   bool
+// )
 
-func runDelete(cmd *cobra.Command, args []string) {
-	internal.ConfigureFolder()
-}
+// // deleteCmd represents the delete command
+// var deleteCmd = &cobra.Command{
+// 	Use:   "delete",
+// 	Short: "Delete old backups",
+// 	Run:   runDelete,
+// }
 
-func init() {
-	cmd.AddCommand(deleteCmd)
-	deleteCmd.Flags().StringVar(&retainAfter, retainAfterFlag, "", "Keep backups newer")
-	deleteCmd.Flags().UintVar(&retainCount, retainCountFlag, 0, "Keep minimum count")
-}
+// func runDelete(cmd *cobra.Command, args []string) {
+// 	folder, err := internal.ConfigureFolder()
+// 	tracelog.ErrorLogger.FatalOnError(err)
+
+// 	objects, err := internal.GetBackupSentinelObjects(folder)
+// 	tracelog.ErrorLogger.FatalOnError(err)
+
+// 	backups := make([]internal.BackupObject, 0, len(objects))
+// 	for _, object := range objects {
+// 		backups = append(backups, internal.NewDefaultBackupObject(object))
+// 	}
+
+// 	folder = folder.GetSubFolder(utility.BaseBackupPath)
+// 	deleteHandler := internal.NewDeleteHandler(folder, backups, lessFunc)
+// 	if cmd.Flags().Changed(retainAfterFlag) {
+
+// 	}
+// }
+
+// func lessFunc(object1 storage.Object, object2 storage.Object) bool {
+// 	return (strings.Compare(object1.Name, object2.Name) > 0)
+// }
+
+// func init() {
+// 	cmd.AddCommand(deleteCmd)
+// 	deleteCmd.Flags().BoolVar(&confirmed, confirmFlag, false, "Confirms backup deletion")
+// 	deleteCmd.Flags().StringVar(&retainAfter, retainAfterFlag, "", "Keep backups newer")
+// 	deleteCmd.Flags().UintVar(&retainCount, retainCountFlag, 0, "Keep minimum count")
+// }
