7851148f79f7266d1f48b4a0d1b78f28b69a5e37
Philip Dubé <phdub@microsoft.com>
2021-08-11 14:10:56 +0000

    Fix typos (then vs than)

--- docker/pg_tests/scripts/tests/delta_backup_fullscan_test.sh
+++ docker/pg_tests/scripts/tests/delta_backup_fullscan_test.sh
@@ -46,7 +46,7 @@ psql -f /tmp/scripts/amcheck.sql -v "ON_ERROR_STOP=1" postgres
 wal-g --config=${TMP_CONFIG} delete everything FORCE --confirm
 /tmp/scripts/drop_pg.sh
 
-# check that we cant make delta from other database then previous backup
+# check that we can't make delta from other database than previous backup
 
 # create db
 /usr/lib/postgresql/10/bin/initdb ${PGDATA}
