b07a312ec551dc3d7e323021de8c878224e912b1
Mikhail Fesenko <proggga@gmail.com>
2021-09-09 18:25:29 +0300

    Simplified debug log for s3Reader

--- pkg/storages/s3/folder.go
+++ pkg/storages/s3/folder.go
@@ -191,7 +191,7 @@ type s3Reader struct {
 	maxRetryDelay time.Duration
 	minRetryDelay time.Duration
 	reconnectId   int
-	id            string // hash from filename and id - unique id for s3reader instance
+	logDebugId    string // hash from filename and logDebugId - unique logDebugId used only for debug
 }
 
 func (reader *s3Reader) getObjectRange(from, to int64) (*s3.GetObjectOutput, error) {
@@ -204,13 +204,13 @@ func (reader *s3Reader) getObjectRange(from, to int64) (*s3.GetObjectOutput, err
 		Key:    aws.String(reader.objectPath),
 		Range:  aws.String(bytesRange),
 	}
-	tracelog.DebugLogger.Printf("GetObject [%s] with range %s", reader.id, bytesRange)
+	reader.debugLog("GetObject with range %s", bytesRange)
 	return reader.folder.S3API.GetObject(input)
 }
 
 
 func (reader *s3Reader) Read(p []byte) (n int, err error) {
-	tracelog.DebugLogger.Printf("s3Reader [%s] Read to buffer [%d] bytes", reader.id, len(p))
+	reader.debugLog("Read to buffer [%d] bytes", len(p))
 	reconnect := false
 	if reader.lastBody == nil { // initial connect
 		reconnect = true
@@ -220,21 +220,30 @@ func (reader *s3Reader) Read(p []byte) (n int, err error) {
 			reconnect = false
 			connErr := reader.reconnect()
 			if connErr != nil {
-				tracelog.DebugLogger.Printf("s3Reader [%s] reconnect failed %s", reader.id, connErr)
+				reader.debugLog("reconnect failed %s", connErr)
 				return 0, connErr
 			}
 		}
 
 		n, err = reader.lastBody.Read(p)
-		tracelog.DebugLogger.Printf("s3Reader [%s] read %d, err %s", reader.id, n, err)
+		reader.debugLog("read %d, err %s", n, err)
 		if err != nil && err != io.EOF {
 			reconnect = true
 			continue
 		}
 		reader.storageCursor += int64(n)
-		tracelog.DebugLogger.Printf("s3Reader [%s] success read", reader.id)
+		reader.debugLog("success read")
 		return n, err
 	}
+}
+func (reader *s3Reader) getDebugLogLine(format string, v ...interface{}) string {
+	prefix := fmt.Sprintf("s3Reader [%s] ", reader.logDebugId)
+	message := fmt.Sprintf(format, v...)
+	return prefix + message
+}
+
+func (reader *s3Reader) debugLog(format string, v ...interface{}) {
+	tracelog.DebugLogger.Print(reader.getDebugLogLine(format, v...))
 }
 
 func (reader *s3Reader) reconnect() error {
@@ -245,13 +254,12 @@ func (reader *s3Reader) reconnect() error {
 		object, err := reader.getObjectRange(reader.storageCursor, 0)
 		if err != nil {
 			failed += 1
-			tracelog.DebugLogger.Printf("s3Reader [%s] reconnect failed [%d/%d]: %s",
-				reader.id, failed, reader.maxRetries, err)
+			reader.debugLog("reconnect failed [%d/%d]: %s", failed, reader.maxRetries, err)
 			if failed >= reader.maxRetries {
-				return errors.Wrap(err, fmt.Sprintf("s3Reader [%s] Too much reconnecting retries", reader.id))
+				return errors.Wrap(err, reader.getDebugLogLine("Too much reconnecting retries"))
 			}
 			sleepTime := reader.getIncrSleep(failed)
-			tracelog.DebugLogger.Printf("s3Reader [%s] sleep: %s", reader.id, sleepTime)
+			reader.debugLog("sleep: %s", sleepTime)
 			time.Sleep(sleepTime)
 			continue
 		}
@@ -259,13 +267,13 @@ func (reader *s3Reader) reconnect() error {
 		if reader.lastBody != nil {
 			err = reader.lastBody.Close()
 			if err != nil {
-				msg := fmt.Sprintf("s3Reader [%s] We have problems with closing previous connection", reader.id)
+				msg := reader.getDebugLogLine("We have problems with closing previous connection")
 				tracelog.DebugLogger.Print(msg)
 				return errors.Wrap(err, msg)
 			}
 		}
 		reader.lastBody = object.Body
-		tracelog.DebugLogger.Printf("s3Reader [%s] reconnect #%d succeeded", reader.id, reader.reconnectId)
+		reader.debugLog("reconnect #%d succeeded", reader.reconnectId)
 		break
 	}
 	return nil
@@ -296,10 +304,10 @@ func NewS3Reader(objectPath string, retriesCount int,  folder *Folder,
 	minRetryDelay, maxRetryDelay time.Duration) *s3Reader {
 
 	S3BufferCounter++
-	reader := &s3Reader{objectPath: objectPath, maxRetries: retriesCount, id: getHash(objectPath, S3BufferCounter),
+	reader := &s3Reader{objectPath: objectPath, maxRetries: retriesCount, logDebugId: getHash(objectPath, S3BufferCounter),
 		folder: folder, minRetryDelay: minRetryDelay, maxRetryDelay: maxRetryDelay}
 
-	tracelog.DebugLogger.Printf("Init s3reader hash %s path %s", reader.id, objectPath)
+	reader.debugLog("Init s3reader path %s", objectPath)
 	return reader
 }
 
