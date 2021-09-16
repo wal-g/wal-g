4bf695e5bb1f4bda670b7bb5aefa8f73ed6d89f1
Daniil Zakhlystov <47750602+usernamedt@users.noreply.github.com>
2021-09-16 15:42:07 +0500

    Add greenplum backup-list (#1093)

---
+++ cmd/gp/backup_list.go
@@ -0,0 +1,39 @@
+package gp
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/wal-g/tracelog"
+	"github.com/wal-g/wal-g/internal"
+	"github.com/wal-g/wal-g/utility"
+)
+
+const (
+	backupListShortDescription = "Prints available backups"
+	PrettyFlag                 = "pretty"
+	JSONFlag                   = "json"
+)
+
+var (
+	// backupListCmd represents the backupList command
+	backupListCmd = &cobra.Command{
+		Use:   "backup-list",
+		Short: backupListShortDescription, // TODO : improve description
+		Args:  cobra.NoArgs,
+		Run: func(cmd *cobra.Command, args []string) {
+			folder, err := internal.ConfigureFolder()
+			tracelog.ErrorLogger.FatalOnError(err)
+			internal.DefaultHandleBackupList(folder.GetSubFolder(utility.BaseBackupPath), pretty, jsonOutput)
+		},
+	}
+	pretty     = false
+	jsonOutput = false
+)
+
+func init() {
+	cmd.AddCommand(backupListCmd)
+
+	// TODO: Merge similar backup-list functionality
+	// to avoid code duplication in command handlers
+	backupListCmd.Flags().BoolVar(&pretty, PrettyFlag, false, "Prints more readable output")
+	backupListCmd.Flags().BoolVar(&jsonOutput, JSONFlag, false, "Prints output in json format")
+}
