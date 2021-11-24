564ef0df93d8fe1a2b442a2c9a2333e49a9837fc
usernamedt <usernamedt@protonmail.ch>
2021-11-24 10:38:28 +0300

    Fix linter

--- internal/config.go
+++ internal/config.go
@@ -256,7 +256,7 @@ var (
 		"S3_ENDPOINT_SOURCE":          true,
 		"S3_ENDPOINT_PORT":            true,
 		"S3_USE_LIST_OBJECTS_V1":      true,
-		"S3_LOG_LEVEL":					true,
+		"S3_LOG_LEVEL":                true,
 		"S3_RANGE_BATCH_ENABLED":      true,
 		"S3_RANGE_MAX_RETRIES":        true,
 		"S3_MAX_RETRIES":              true,
