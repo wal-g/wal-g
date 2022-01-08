1bf10929bd8a3cbd1ef5d8123615e683c7db2381
Philip Dubé <serprex@users.noreply.github.com>
2022-01-08 12:44:54 +0000

    Split writeLocalFile & clearLocalFile into free functions (#1194)
    Use WriteLocalFile in unwrapRegularFileOld
    
    Don't close file on error in WriteLocalFile; caller handles

--- internal/databases/postgres/file_unwrapper.go
+++ internal/databases/postgres/file_unwrapper.go
@@ -4,9 +4,6 @@ import (
 	"archive/tar"
 	"io"
 	"os"
-
-	"github.com/pkg/errors"
-	"github.com/wal-g/tracelog"
 )
 
 type FileUnwrapperType int
@@ -68,44 +65,4 @@ type IBackupFileUnwrapper interface {
 
 type BackupFileUnwrapper struct {
 	options *BackupFileOptions
-}
-
-// truncate local file and set reader offset to zero
-func (u *BackupFileUnwrapper) clearLocalFile(file *os.File) error {
-	err := file.Truncate(0)
-	if err != nil {
-		return err
-	}
-	_, err = file.Seek(0, 0)
-	return err
-}
-
-// write file from reader to local file
-func (u *BackupFileUnwrapper) writeLocalFile(fileReader io.Reader, header *tar.Header, localFile *os.File, fsync bool) error {
-	_, err := io.Copy(localFile, fileReader)
-	if err != nil {
-		err1 := localFile.Close()
-		if err1 != nil {
-			tracelog.ErrorLogger.Printf("Interpret: failed to close localFile '%s' because of error: %v",
-				localFile.Name(), err1)
-		}
-		err1 = os.Remove(localFile.Name())
-		if err1 != nil {
-			tracelog.ErrorLogger.Fatalf("Interpret: failed to remove localFile '%s' because of error: %v",
-				localFile.Name(), err1)
-		}
-		return errors.Wrap(err, "Interpret: copy failed")
-	}
-
-	mode := os.FileMode(header.Mode)
-	if err = os.Chmod(localFile.Name(), mode); err != nil {
-		return errors.Wrap(err, "Interpret: chmod failed")
-	}
-
-	if fsync {
-		err = localFile.Sync()
-		return errors.Wrap(err, "Interpret: fsync failed")
-	}
-
-	return nil
 }
