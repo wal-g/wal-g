a4e5e29d089b44fbdcc3b427a0097271fbfda5cc
Копылов Дмитрий <32416034+Dm17r1y@users.noreply.github.com>
2022-05-19 12:01:42 +0500

    Download file without extension as last resort

--- internal/fetch_helper.go
+++ internal/fetch_helper.go
@@ -168,14 +168,6 @@ func putCachedDecompressorInFirstPlace(decompressors []compression.Decompressor)
 
 // TODO : unit tests
 func DownloadAndDecompressStorageFile(folder storage.Folder, fileName string) (io.ReadCloser, error) {
-	reader, exists, err := TryDownloadFile(folder, fileName)
-	if err != nil {
-		return nil, err
-	}
-	if exists {
-		return reader, nil
-	}
-
 	for _, decompressor := range putCachedDecompressorInFirstPlace(compression.Decompressors) {
 		archiveReader, exists, err := TryDownloadFile(folder, fileName+"."+decompressor.FileExtension())
 		if err != nil {
@@ -197,6 +189,15 @@ func DownloadAndDecompressStorageFile(folder storage.Folder, fileName string) (i
 			Closer: ioextensions.NewMultiCloser([]io.Closer{archiveReader, decompressedReaded}),
 		}, nil
 	}
+
+	reader, exists, err := TryDownloadFile(folder, fileName)
+	if err != nil {
+		return nil, err
+	}
+	if exists {
+		return reader, nil
+	}
+
 	return nil, newArchiveNonExistenceError(fileName)
 }
 
