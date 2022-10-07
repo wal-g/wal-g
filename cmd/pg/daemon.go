7a9c2d8865db318d812c0fb9e8ffb2a7172e93c4
Artem Galkin <artem-galkin@yandex-team.ru>
2022-10-07 13:06:07 +0300

    communication into one socket connection

--- cmd/pg/daemon.go
+++ cmd/pg/daemon.go
@@ -22,19 +22,27 @@ func handleConn(c net.Conn, f func(string) error) {
 	buf := make([]byte, 512)
 	nr, err := c.Read(buf)
 	if err != nil {
-		tracelog.ErrorLogger.Printf("Failed to read message from client %s, err: %v\n", c.RemoteAddr(), err)
+		tracelog.ErrorLogger.Printf("Failed to read checking message from client %s, err: %v\n", c.RemoteAddr(), err)
+		_, _ = c.Write([]byte("READ_FAILED"))
+		return
+	}
+	if nr == 5 && string(buf[0:5]) == "CHECK" {
+		_, _ = c.Write([]byte("CHECKOK"))
+		tracelog.InfoLogger.Printf("Sucessful configuration check")
+	} else {
+		tracelog.ErrorLogger.Printf("Error on configuration check")
+		return
+	}
+	n, err := c.Read(buf)
+	if err != nil {
+		tracelog.ErrorLogger.Printf("Failed to read message with file from client %s, err: %v\n", c.RemoteAddr(), err)
 		_, _ = c.Write([]byte("READ_FAILED"))
 		return
 	}
 
-	if nr < 24 {
-		if nr == 5 && string(buf[0:5]) == "CHECK" {
-			_, _ = c.Write([]byte("CHECKOK"))
-			tracelog.InfoLogger.Printf("sucessful configuration check")
-			return
-		}
-		if nr > 0 {
-			tracelog.ErrorLogger.Printf("Received incorrect message %s from %s", buf[0:nr], c.RemoteAddr())
+	if n < 24 {
+		if n > 0 {
+			tracelog.ErrorLogger.Printf("Received incorrect message %s from %s", buf[0:n], c.RemoteAddr())
 		} else {
 			tracelog.ErrorLogger.Printf("Received empty message from %s", c.RemoteAddr())
 		}
@@ -58,7 +66,7 @@ func handleConn(c net.Conn, f func(string) error) {
 	}
 }
 
-// walPushCmd represents the walPush command
+// daemonCmd represents the daemon command
 var daemonCmd = &cobra.Command{
 	Use:   "daemon wal_filepath",
 	Short: DaemonShortDescription, // TODO : improve description
