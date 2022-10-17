ec6b490c7eb2713daaefa917ebd4e416369a7f98
Artem Galkin <artem-galkin@yandex-team.ru>
2022-10-17 18:54:12 +0300

    message type implementation

--- internal/databases/postgres/daemon_handler.go
+++ internal/databases/postgres/daemon_handler.go
@@ -3,12 +3,63 @@ package postgres
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
+	"fmt"
 	"net"
 	"os"
 	"path"
 
 	"github.com/wal-g/tracelog"
 )
+
+type CheckType byte
+type FileType byte
+
+type MessageSocketHandler interface {
+	Handle(messageBody []byte, c net.Conn, f func(string) error) error
+}
+
+func MessageTypeConstruct(messageType byte) MessageSocketHandler {
+	switch messageType {
+	case 'C':
+		return CheckType('C')
+	case 'F':
+		return FileType('F')
+	default:
+		return nil
+	}
+}
+
+func (msg CheckType) Handle(messageBody []byte, c net.Conn, f func(string) error) error {
+	_, err := c.Write([]byte{'O', 0, 3})
+	if err != nil {
+		tracelog.ErrorLogger.Printf("Error on writing in socket: %v", err)
+		return err
+	}
+	return nil
+}
+
+func (msg FileType) Handle(messageBody []byte, c net.Conn, f func(string) error) error {
+	if len(messageBody) < 24 {
+		if len(messageBody) > 0 {
+			tracelog.ErrorLogger.Printf("Received incorrect message %s", messageBody)
+		} else {
+			tracelog.ErrorLogger.Println("Received empty message")
+		}
+		return errors.New(fmt.Sprint("Incorrect message accepted"))
+	}
+	tracelog.InfoLogger.Printf("wal file name: %s\n", messageBody)
+	err := f(string(messageBody))
+	if err != nil {
+		tracelog.ErrorLogger.Printf("wal-push failed: %v\n", err)
+		return err
+	}
+	_, err = c.Write([]byte{'O', 0, 3})
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 // HandleDaemon is invoked to perform daemon mode
 func HandleDaemon(uploader *WalUploader, pathToSocket string, pathToWal string) {
@@ -42,65 +93,40 @@ func DaemonProcess(c net.Conn, f func(string) error) {
 		nr, err := c.Read(buf)
 		if err != nil {
 			tracelog.ErrorLogger.Printf("Failed to read checking message from client %s, err: %v\n", c.RemoteAddr(), err)
-			_, _ = c.Write([]byte("READ_FAILED"))
+			_, _ = c.Write([]byte{'E', 0, 3})
 			return
 		}
-		requestType, requestLength, requestBody := MessageParser(buf)
-		response := HandleMessage(requestType, requestLength, requestBody, f)
-		_, _ = c.Write([]byte(response))
-		if response == "OK" {
+
+		byteType, byteLength, byteBody := MessageParser(buf)
+		requestType := MessageTypeConstruct(byteType)
+		err = requestType.Handle(byteBody, c, f)
+		if err != nil {
+			tracelog.ErrorLogger.Println("Failed to handle message, err:", err)
+		}
+		if byteType == 'F' {
 			return
 		}
-		if int(requestLength) < nr {
+		if int(byteLength) < nr {
 			tracelog.InfoLogger.Println("Read remaining buffer...")
-			requestType, requestLength, requestBody = MessageParser(buf[requestLength:])
-			response = HandleMessage(requestType, requestLength, requestBody, f)
-			_, _ = c.Write([]byte(response))
+			byteType, byteLength, byteBody = MessageParser(buf[byteLength:])
+			err = requestType.Handle(byteBody, c, f)
+			if err != nil {
+				tracelog.ErrorLogger.Println("Failed to handle message, err:", err)
+			}
 			return
 		}
 	}
 }
 
 // MessageParser is invoked to read bytes from buffer
-func MessageParser(buf []byte) (string, uint16, string) {
-	messageType := string(buf[0])
+func MessageParser(buf []byte) (byte, uint16, []byte) {
+	messageType := buf[0]
 	var messageLength uint16
 	l := bytes.NewReader(buf[1:3])
 	err := binary.Read(l, binary.BigEndian, &messageLength)
 	if err != nil {
 		tracelog.ErrorLogger.Printf("Failed to read message length, err: %v", err)
 	}
-	messageBody := string(buf[3 : messageLength-1])
+	messageBody := buf[3 : messageLength-1]
 	return messageType, messageLength, messageBody
-}
-
-// HandleMessage is invoked to validate message and to return a correct response
-func HandleMessage(messageType string, messageLength uint16, messageBody string, f func(string) error) string {
-	if messageType == "C" {
-		if messageBody[0:5] == "CHECK" {
-			tracelog.InfoLogger.Println("Successful configuration check")
-			return "CHECKED"
-		}
-		tracelog.ErrorLogger.Printf("Incorrect message body: '%s', expected: '%s'", messageBody, "CHECK")
-		return "BAD_MSG"
-	}
-	if messageType == "F" {
-		if messageLength < 28 {
-			if messageLength > 0 {
-				tracelog.ErrorLogger.Printf("Received incorrect message %s", messageBody)
-			} else {
-				tracelog.ErrorLogger.Println("Received empty message")
-			}
-			return "BAD_MSG"
-		}
-		tracelog.InfoLogger.Printf("wal file name: %s\n", messageBody)
-		err := f(messageBody)
-		if err != nil {
-			tracelog.ErrorLogger.Printf("wal-push failed: %v\n", err)
-			return "FAIL"
-		}
-		return "OK"
-	}
-	tracelog.ErrorLogger.Println("Incorrect message type")
-	return "BAD_MSG"
 }
