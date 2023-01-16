a58e280a4283541a4c3ff7967007d98662ad90be
kravchenko99 <kravchenko99@yandex-team.ru>
2023-01-16 15:56:18 +0300

    [MongoDB] add opportunity restore mongodb backup with + 1 version

--- internal/databases/mongo/binary/utilities.go
+++ internal/databases/mongo/binary/utilities.go
@@ -4,6 +4,7 @@ import (
 	"github.com/pkg/errors"
 	"github.com/wal-g/tracelog"
 	"golang.org/x/mod/semver"
+	"strconv"
 )
 
 // MajorMinorVersion return version in format '<MajorVersion>.<MinorVersion>' (without patch, prerelease, build, ...)
@@ -19,11 +20,43 @@ func MajorMinorVersion(version string) string {
 	return semver.MajorMinor(version)
 }
 
+// MajorVersion return version in format '<MajorVersion>' (without minor, patch, prerelease, build, ...)
+func MajorVersion(version string) string {
+	if len(version) == 0 {
+		return version
+	}
+
+	if version[0] != 'v' {
+		version = "v" + version
+	}
+
+	return semver.Major(version)
+}
+
+// NextMajorVersion return next major version in format '<MajorVersion> + 1' (without minor, patch, prerelease, build, ...)
+func NextMajorVersion(version string) string {
+	if len(version) == 0 {
+		return version
+	}
+
+	minorVersion := MajorMinorVersion(version)
+	if minorVersion == "v4.2" {
+		return "v4"
+	}
+
+	majorVersionStr := MajorVersion(minorVersion)
+	majorVersionInt, _ := strconv.Atoi(majorVersionStr[1:])
+	return "v" + strconv.Itoa(majorVersionInt+1)
+}
+
 func EnsureCompatibilityToRestoreMongodVersions(backupMongodVersion, restoreMongodVersion string) error {
 	tracelog.InfoLogger.Printf("Check compatibility backup version %v and current mongod version %v",
 		backupMongodVersion, restoreMongodVersion)
 
-	if semver.Compare(MajorMinorVersion(backupMongodVersion), MajorMinorVersion(restoreMongodVersion)) != 0 {
+	var nextMajorBackup = NextMajorVersion(backupMongodVersion)
+
+	if semver.Compare(MajorMinorVersion(backupMongodVersion), MajorMinorVersion(restoreMongodVersion)) != 0 &&
+		semver.Compare(MajorVersion(restoreMongodVersion), nextMajorBackup) != 0 {
 		return errors.Errorf("Backup's Mongo version (%s) is not compatible with Mongo %s",
 			backupMongodVersion, restoreMongodVersion)
 	}
