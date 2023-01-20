77a8885f8a40f0919880f5e26d187e683c6bf977
kravchenko99 <kravchenko99@yandex-team.ru>
2023-01-20 19:47:04 +0300

    fix bug with mixed up arguments

--- internal/databases/mongo/binary/utilities_test.go
+++ internal/databases/mongo/binary/utilities_test.go
@@ -7,10 +7,10 @@ import (
 )
 
 func TestEnsureCompatibilityToRestoreMongodVersions(t *testing.T) {
-	assert.Equal(t, nil, EnsureCompatibilityToRestoreMongodVersions("4.2", "4.4"))
+	assert.Equal(t, nil, EnsureCompatibilityToRestoreMongodVersions("4.4", "4.2"))
 	assert.Equal(t, nil, EnsureCompatibilityToRestoreMongodVersions("4.2", "4.2"))
 	assert.Equal(t, nil, EnsureCompatibilityToRestoreMongodVersions("4.4", "4.4"))
-	assert.Equal(t, nil, EnsureCompatibilityToRestoreMongodVersions("4.4", "5.0"))
-	assert.Error(t, EnsureCompatibilityToRestoreMongodVersions("5.0", "4.4"))
-	assert.Error(t, EnsureCompatibilityToRestoreMongodVersions("4.4", "4.2"))
+	assert.Equal(t, nil, EnsureCompatibilityToRestoreMongodVersions("5.0", "4.4"))
+	assert.Error(t, EnsureCompatibilityToRestoreMongodVersions("4.4", "5.0"))
+	assert.Error(t, EnsureCompatibilityToRestoreMongodVersions("4.2", "4.4"))
 }
