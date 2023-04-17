9c55bbf5ec4975b122ccff74a0254c2866265f27
alex3453 <46063591+alex3453@users.noreply.github.com>
2023-04-17 19:05:59 +0500

    Add test for isIncremental() sentinel dto check (#1455)
    Co-authored-by: Соколов Алексей <av.sok@skbkontur.ru>

---
+++ internal/databases/postgres/backup_sentinel_dto_test.go
@@ -0,0 +1,43 @@
+package postgres
+
+import (
+	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"testing"
+)
+
+func TestBackupSentinelDto_IsIncremental(t *testing.T) {
+	testFromLSN := LSN(uint64(rand.Uint32())<<32 + uint64(rand.Uint32()))
+	testFrom := "fromString"
+	testFullName := "fullNameString"
+	testCount := rand.Int()
+
+	tests := []struct {
+		name              string
+		dto               BackupSentinelDto
+		wantIsIncremental bool
+	}{
+		{
+			name: "ShouldReturn_False_When_IncrementFromIsNil",
+			dto: BackupSentinelDto{
+				IncrementFrom: nil,
+			},
+			wantIsIncremental: false,
+		},
+		{
+			name: "ShouldReturn_True_When_IncrementFromIsNotNil_And_RequiredFieldsFilled",
+			dto: BackupSentinelDto{
+				IncrementFromLSN:  &testFromLSN,
+				IncrementFrom:     &testFrom,
+				IncrementFullName: &testFullName,
+				IncrementCount:    &testCount,
+			},
+			wantIsIncremental: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.wantIsIncremental, tt.dto.IsIncremental(), "IsIncremental()")
+		})
+	}
+}
