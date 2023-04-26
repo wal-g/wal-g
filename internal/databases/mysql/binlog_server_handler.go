9856b0d3a645719d8c91c3236ff188172428bd2b
Fizic <kirya.firsov.04@gmail.com>
2023-04-26 14:36:41 +0300

    fix: query, parsing time

--- internal/databases/mysql/binlog_server_handler.go
+++ internal/databases/mysql/binlog_server_handler.go
@@ -8,6 +8,7 @@ import (
 	"os"
 	"path"
 	"strconv"
+	"strings"
 	"time"
 
 	"github.com/go-mysql-org/go-mysql/mysql"
@@ -142,7 +143,7 @@ func sendEventsFromBinlogFiles(logFilesProvider *storage.ObjectProvider, pos mys
 	}
 }
 
-func syncBinlogFiles(pos mysql.Position, s *replication.BinlogStreamer) error {
+func syncBinlogFiles(pos mysql.Position, startTS time.Time, s *replication.BinlogStreamer) error {
 	// get necessary settings
 	folder, err := internal.ConfigureFolder()
 	if err != nil {
@@ -171,49 +172,52 @@ func (h Handler) HandleRegisterSlave(data []byte) error {
 func (h Handler) HandleBinlogDump(pos mysql.Position) (*replication.BinlogStreamer, error) {
 	s := replication.NewBinlogStreamer()
 
-	err := syncBinlogFiles(pos, s)
+	folder, err := internal.ConfigureFolder()
+	if err != nil {
+		return nil, err
+	}
+
+	startTime, err := GetBinlogTS(folder, pos.Name)
+	if err != nil {
+		return nil, err
+	}
+	err = syncBinlogFiles(pos, startTime, s)
 	return s, err
 }
 
 func (h Handler) HandleBinlogDumpGTID(gtidSet *mysql.MysqlGTIDSet) (*replication.BinlogStreamer, error) {
 	s := replication.NewBinlogStreamer()
-	folder, _ := internal.ConfigureFolder()
 
-	pos, err := getPositionBeforeGTID(folder, gtidSet, "mysql")
-	if err != nil {
-		return nil, err
-	}
-
-	err = syncBinlogFiles(pos, s)
+	err := syncBinlogFiles(mysql.Position{Name: "host-binlog-file", Pos: 4}, startTS, s)
 	return s, err
 }
 
 func (h Handler) HandleQuery(query string) (*mysql.Result, error) {
-	switch query {
-	case "SELECT @master_binlog_checksum":
+	switch strings.ToLower(query) {
+	case "select @master_binlog_checksum":
 		resultSet, _ := mysql.BuildSimpleTextResultset([]string{"master_binlog_checksum"}, [][]interface{}{{"NONE"}})
 		return &mysql.Result{Status: 34, Warnings: 0, InsertId: 0, AffectedRows: 0, Resultset: resultSet}, nil
-	case "SHOW GLOBAL VARIABLES LIKE 'BINLOG_CHECKSUM'":
+	case "show global variables like 'binlog_checksum'":
 		resultSet, _ := mysql.BuildSimpleTextResultset([]string{"BINLOG_CHECKSUM"}, [][]interface{}{{"NONE"}})
 		return &mysql.Result{Status: 34, Warnings: 0, InsertId: 0, AffectedRows: 0, Resultset: resultSet}, nil
-	case "SELECT @@GLOBAL.SERVER_ID":
+	case "select @@global.server_id":
 		serverID, err := internal.GetRequiredSetting(internal.MysqlBinlogServerID)
 		tracelog.ErrorLogger.FatalOnError(err)
 		resultSet, err := mysql.BuildSimpleTextResultset([]string{"SERVER_ID"}, [][]interface{}{{serverID}})
 		tracelog.ErrorLogger.FatalOnError(err)
 		return &mysql.Result{Status: 34, Warnings: 0, InsertId: 0, AffectedRows: 0, Resultset: resultSet}, nil
-	case "SELECT @source_binlog_checksum":
+	case "select @source_binlog_checksum":
 		resultSet, _ := mysql.BuildSimpleTextResultset([]string{"source_binlog_checksum"}, [][]interface{}{{"1"}})
 		return &mysql.Result{Status: 34, Warnings: 0, InsertId: 0, AffectedRows: 0, Resultset: resultSet}, nil
-	case "SELECT @@GLOBAL.GTID_MODE":
+	case "select @@global.gtid_mode":
 		resultSet, _ := mysql.BuildSimpleTextResultset([]string{"GTID_MODE"}, [][]interface{}{{"ON"}})
 		return &mysql.Result{Status: 34, Warnings: 0, InsertId: 0, AffectedRows: 0, Resultset: resultSet}, nil
-	case "SELECT @@GLOBAL.SERVER_UUID":
+	case "select @@global.server_uuid":
 		// the server uuid received by the query does not affect replication.
 		// during replication, the uuid is taken from events
 		resultSet, _ := mysql.BuildSimpleTextResultset([]string{"SERVER_UUID"}, [][]interface{}{{"0"}})
 		return &mysql.Result{Status: 34, Warnings: 0, InsertId: 0, AffectedRows: 0, Resultset: resultSet}, nil
-	case "SELECT @@global.rpl_semi_sync_master_enabled":
+	case "select @@global.rpl_semi_sync_master_enabled":
 		resultSet, _ := mysql.BuildSimpleTextResultset([]string{"@@global.rpl_semi_sync_master_enabled"}, [][]interface{}{{"0"}})
 		return &mysql.Result{Status: 34, Warnings: 0, InsertId: 0, AffectedRows: 0, Resultset: resultSet}, nil
 	default:
