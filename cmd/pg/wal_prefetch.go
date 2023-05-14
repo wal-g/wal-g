f5e58163fdaf3bd77ff3d2933b30bdf4b4a9d922
Denis Surkov <surkov0denis@yandex.ru>
2023-05-14 18:14:39 +0500

    Add prefetch prefix for wal-prefetch cmd (#1471)
    Add prefetch prefix for wal-prefetch cmd & discard loggers if using non-DEVEL logging level.

--- cmd/pg/wal_prefetch.go
+++ cmd/pg/wal_prefetch.go
@@ -1,10 +1,13 @@
 package pg
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"github.com/wal-g/tracelog"
 	"github.com/wal-g/wal-g/internal"
 	"github.com/wal-g/wal-g/internal/databases/postgres"
+	"io"
 	"github.com/wal-g/wal-g/internal/multistorage"
 )
 
@@ -18,6 +21,8 @@ var WalPrefetchCmd = &cobra.Command{
 	Args:   cobra.ExactArgs(2),
 	Hidden: true,
 	Run: func(cmd *cobra.Command, args []string) {
+		reconfigureLoggers()
+
 		folder, err := internal.ConfigureFolder()
 		tracelog.ErrorLogger.FatalOnError(err)
 
@@ -33,4 +38,29 @@ var WalPrefetchCmd = &cobra.Command{
 
 func init() {
 	Cmd.AddCommand(WalPrefetchCmd)
+}
+
+// wal-prefetch (WalPrefetchCmd) is internal tool, so to avoid confusion about errors in restoration process
+// we reconfigure loggers specially for internal use. All logs having PREFETCH prefix can be safely ignored
+func reconfigureLoggers() {
+	if viper.Get(internal.LogLevelSetting) == tracelog.DevelLogLevel {
+		addPrefetchPrefixToAllLoggers()
+		return
+	}
+
+	discardAllLoggers()
+}
+
+func discardAllLoggers() {
+	tracelog.ErrorLogger.SetOutput(io.Discard)
+	tracelog.WarningLogger.SetOutput(io.Discard)
+	tracelog.InfoLogger.SetOutput(io.Discard)
+	tracelog.DebugLogger.SetOutput(io.Discard)
+}
+
+func addPrefetchPrefixToAllLoggers() {
+	tracelog.ErrorLogger.SetPrefix(fmt.Sprintf("PREFETCH %s", tracelog.ErrorLogger.Prefix()))
+	tracelog.WarningLogger.SetPrefix(fmt.Sprintf("PREFETCH %s", tracelog.WarningLogger.Prefix()))
+	tracelog.InfoLogger.SetPrefix(fmt.Sprintf("PREFETCH %s", tracelog.InfoLogger.Prefix()))
+	tracelog.DebugLogger.SetPrefix(fmt.Sprintf("PREFETCH %s", tracelog.DebugLogger.Prefix()))
 }
