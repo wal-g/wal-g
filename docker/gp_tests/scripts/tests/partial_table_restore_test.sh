1017cb3e3e5dc2bea6b903895fd8a9bb56e66fa6
Konstantin Nikiforov <kostya.nik.3854@gmail.com>
2023-06-19 11:54:35 +0500

    add test for aovisimap

--- docker/gp_tests/scripts/tests/partial_table_restore_test.sh
+++ docker/gp_tests/scripts/tests/partial_table_restore_test.sh
@@ -18,18 +18,43 @@ enable_pitr_extension
 setup_wal_archiving
 
 # insert_data
+n=10000
+it=10
+expected_count=$(($n + $it * 5))
+
 psql -p 6000 -c "DROP DATABASE IF EXISTS db"
 psql -p 6000 -c "CREATE DATABASE db"
-psql -p 6000 -d db -c "CREATE TABLE heap_to_restore AS SELECT a FROM generate_series(1,10000) AS a;"
-psql -p 6000 -d db -c "CREATE TABLE heap_to_skip AS SELECT a FROM generate_series(1,10000) AS a;"
+psql -p 6000 -d db -c "CREATE TABLE heap_to_restore AS SELECT a FROM generate_series(1,$n) AS a;"
+psql -p 6000 -d db -c "CREATE TABLE heap_to_skip AS SELECT a FROM generate_series(1,$n) AS a;"
 psql -p 6000 -d db -c "CREATE TABLE ao_to_restore(a int, b int) WITH (appendoptimized = true) DISTRIBUTED BY (a);"
 psql -p 6000 -d db -c "CREATE TABLE ao_to_skip(a int, b int) WITH (appendoptimized = true) DISTRIBUTED BY (a);"
 psql -p 6000 -d db -c "CREATE TABLE co_to_restore(a int, b int) WITH (appendoptimized = true, orientation = column) DISTRIBUTED BY (a);"
 psql -p 6000 -d db -c "CREATE TABLE co_to_skip(a int, b int) WITH (appendoptimized = true, orientation = column) DISTRIBUTED BY (a);"
-psql -p 6000 -d db -c "INSERT INTO ao_to_restore SELECT i, i FROM generate_series(1,10000)i;"
-psql -p 6000 -d db -c "INSERT INTO ao_to_skip SELECT i, i FROM generate_series(1,10000)i;"
-psql -p 6000 -d db -c "INSERT INTO co_to_restore SELECT i, i FROM generate_series(1,10000)i;"
-psql -p 6000 -d db -c "INSERT INTO co_to_skip SELECT i, i FROM generate_series(1,10000)i;"
+psql -p 6000 -d db -c "INSERT INTO ao_to_restore SELECT i, i FROM generate_series(1,$n)i;"
+psql -p 6000 -d db -c "INSERT INTO ao_to_skip SELECT i, i FROM generate_series(1,$n)i;"
+psql -p 6000 -d db -c "INSERT INTO co_to_restore SELECT i, i FROM generate_series(1,$n)i;"
+psql -p 6000 -d db -c "INSERT INTO co_to_skip SELECT i, i FROM generate_series(1,$n)i;"
+
+# check aovisimap
+insert_10_delete_5() {
+  start_val=$1
+  stop_val=$(($start_val + 9))
+  stop_val_d=$(($start_val + 4))
+  psql -p 6000 -d db -c "INSERT INTO ao_to_restore SELECT i, i FROM generate_series($start_val,$stop_val)i;"
+  psql -p 6000 -d db -c "INSERT INTO ao_to_skip SELECT i, i FROM generate_series($start_val,$stop_val)i;"
+  psql -p 6000 -d db -c "INSERT INTO co_to_restore SELECT i, i FROM generate_series($start_val,$stop_val)i;"
+  psql -p 6000 -d db -c "INSERT INTO co_to_skip SELECT i, i FROM generate_series($start_val,$stop_val)i;"
+
+  psql -p 6000 -d db -c "DELETE FROM ao_to_restore WHERE a >= $start_val and a <= $stop_val_d;"
+  psql -p 6000 -d db -c "DELETE FROM ao_to_skip WHERE a >= $start_val and a <= $stop_val_d;"
+  psql -p 6000 -d db -c "DELETE FROM co_to_restore WHERE a >= $start_val and a <= $stop_val_d;"
+  psql -p 6000 -d db -c "DELETE FROM co_to_skip WHERE a >= $start_val and a <= $stop_val_d;"
+}
+
+for i in $(seq 1 $it);
+do
+  insert_10_delete_5 $(($n + 1 + 10*($i-1)))
+done
 
 run_backup_logged ${TMP_CONFIG} ${PGDATA}
 stop_and_delete_cluster_dir
@@ -38,13 +63,13 @@ wal-g --config=${TMP_CONFIG} backup-fetch LATEST --in-place --restore-only=db/he
 
 start_cluster
 
-if [ "$(psql -p 6000 -t -c "SELECT count(*) FROM heap_to_restore;" -d db -A)" != 10000 ]; then
+if [ "$(psql -p 6000 -t -c "SELECT count(*) FROM heap_to_restore;" -d db -A)" != $n ]; then
   echo "Error: Heap table in db database must be restored after partial fetch"
   exit 1
-elif [ "$(psql -p 6000 -t -c "SELECT count(*) FROM ao_to_restore;" -d db -A)" != 10000 ]; then
+elif [ "$(psql -p 6000 -t -c "SELECT count(*) FROM ao_to_restore;" -d db -A)" != $expected_count ]; then
   echo "Error: Append optimized table in db database must be restored after partial fetch"
   exit 1
-elif [ "$(psql -p 6000 -t -c "SELECT count(*) FROM co_to_restore;" -d db -A)" != 10000 ]; then
+elif [ "$(psql -p 6000 -t -c "SELECT count(*) FROM co_to_restore;" -d db -A)" != $expected_count ]; then
   echo "Error: Column oriented table in db database must be restored after partial fetch"
   exit 1
 fi
