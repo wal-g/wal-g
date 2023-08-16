f607a61a860120dbc86337dd2afcc6a2599e44e6
ElectricR <chulkovroma@mail.ru>
2023-08-16 22:01:30 +0300

    MongoDB binary restore: Added flag for particular replica set member ids, fix tests

--- internal/databases/mongo/binary/mongod_test.go
+++ internal/databases/mongo/binary/mongod_test.go
@@ -8,13 +8,30 @@ import (
 )
 
 func TestMakeBsonRsMembers(t *testing.T) {
-	assert.Equal(t, bson.A{}, makeBsonRsMembers(""))
-	assert.Equal(t, bson.A{bson.M{"_id": 0, "host": "localhost:1234"}}, makeBsonRsMembers("localhost:1234"))
+	assert.Equal(t, bson.A{}, makeBsonRsMembers(RsConfig{}))
+	assert.Equal(t, bson.A{bson.M{"_id": 0, "host": "localhost:1234"}}, makeBsonRsMembers(RsConfig{
+		RsMembers: []string{"localhost:1234"},
+	}))
 	assert.Equal(t,
 		bson.A{
 			bson.M{"_id": 0, "host": "localhost:1234"},
 			bson.M{"_id": 1, "host": "localhost:5678"},
 			bson.M{"_id": 2, "host": "remotehost:9876"},
 		},
-		makeBsonRsMembers("localhost:1234,localhost:5678,remotehost:9876"))
+		makeBsonRsMembers(RsConfig{
+			RsName:      "",
+			RsMembers:   []string{"localhost:1234", "localhost:5678", "remotehost:9876"},
+			RsMemberIds: []int{},
+		}))
+	assert.Equal(t,
+		bson.A{
+			bson.M{"_id": 4, "host": "localhost:1234"},
+			bson.M{"_id": 5, "host": "localhost:5678"},
+			bson.M{"_id": 0, "host": "remotehost:9876"},
+		},
+		makeBsonRsMembers(RsConfig{
+			RsName:      "",
+			RsMembers:   []string{"localhost:1234", "localhost:5678", "remotehost:9876"},
+			RsMemberIds: []int{4, 5, 0},
+		}))
 }
