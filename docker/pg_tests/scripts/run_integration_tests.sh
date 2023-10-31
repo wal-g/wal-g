c805f004537ccd1b3f5d612999c9b55c4c487a04
Stepan Filippov <stepan.filippov@yandex.ru>
2023-10-31 14:24:01 +0500

    testing

--- docker/pg_tests/scripts/run_integration_tests.sh
+++ docker/pg_tests/scripts/run_integration_tests.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
-set -e -x
+set -e -x -v
 
 pushd /tmp
+
+#while true
+#do
+#    sleep 10
+#done
+
 for i in tests/*.sh; do
   # Exclude pgbackrest because it needs to be run in separate container. It should be run individually.
   if [ "$i" = "tests/pgbackrest_backup_fetch_test.sh" ]; then continue ;fi
   # Exclude ssh test because of ssh server container dependency.
   if [ "$i" = "tests/ssh_backup_test.sh" ]; then continue ;fi
+  if [ "$i" != "tests/zzz.sh" ]; then continue ;fi
   echo
   echo "===== RUNNING $i ====="
   set -x
-
+  
   ./"$i";
 
   set +x
   echo "===== SUCCESS $i ====="
   echo
+
+while true
+do
+    sleep 10
+done
 done
 popd
---
+++ docker/pg_tests/scripts/tests/zzz.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+set -e -x
+
+rm -rf /var/lib/postgresql/10/main
+
+CONFIG_FILE="/tmp/configs/wal_perftest_config.json"
+
+COMMON_CONFIG="/tmp/configs/common_config.json"
+TMP_CONFIG="/tmp/configs/tmp_config.json"
+cat ${CONFIG_FILE} > ${TMP_CONFIG}
+
+echo "," >> ${TMP_CONFIG}
+cat ${COMMON_CONFIG} >> ${TMP_CONFIG}
+/tmp/scripts/wrap_config_file.sh ${TMP_CONFIG}
+
+WAL_PUSH_LOGS="/tmp/logs/pg_wal_perftest_push"
+WAL_FETCH_LOGS="/tmp/logs/pg_wal_perftest_fetch"
+echo "" > ${WAL_PUSH_LOGS}
+echo "" > ${WAL_FETCH_LOGS}
+
+/usr/lib/postgresql/10/bin/initdb "${PGDATA}"
+/usr/lib/postgresql/10/bin/pg_ctl -D "${PGDATA}" -w start
+
+/tmp/scripts/wait_while_pg_not_ready.sh
+
+wal-g --config=${TMP_CONFIG} delete everything FORCE --confirm
+
+pgbench -i -s 100 postgres
+pgbench -c 100 -t 10000 postgres
+
+du -hs ${PGDATA}
+sleep 1
+WAL=$(ls -l ${PGDATA}/pg_wal | head -n2 | tail -n1 | egrep -o "[0-9A-F]{24}")
+
+du -hs "${PGDATA}"
+/usr/bin/time -v -a --output ${WAL_PUSH_LOGS} wal-g --config=${TMP_CONFIG} wal-push "${PGDATA}"/pg_wal/"${WAL}" &
+i=0
+while [ "$i" -le 1001 ];
+do
+    wal-g --config=${TMP_CONFIG} wal-push "${PGDATA}"/pg_wal/"${WAL}" &
+    i=$(( i + 1 ))
+done
+sleep 20
+/tmp/scripts/drop_pg.sh
+
+/usr/bin/time -v -a --output ${WAL_FETCH_LOGS} wal-g --config=${TMP_CONFIG} wal-fetch "${WAL}" "${PGDATA}"
+sleep 1
+/tmp/scripts/drop_pg.sh
+/tmp/scripts/parselogs.sh ${WAL_PUSH_LOGS}
+/tmp/scripts/parselogs.sh ${WAL_FETCH_LOGS}
