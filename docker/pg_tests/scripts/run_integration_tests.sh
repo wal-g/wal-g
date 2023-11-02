f9c02ddff85cdd32dcbc86ae806aec823324ae3a
Stepan Filippov <stepan.filippov@yandex.ru>
2023-11-02 14:02:08 +0500

    changed names

--- docker/pg_tests/scripts/run_integration_tests.sh
+++ docker/pg_tests/scripts/run_integration_tests.sh
@@ -4,13 +4,12 @@ set -e -x
 pushd /tmp
 
 for i in tests/*.sh; do
-echo "===== SUCCESS $i ====="
-continue
   # Exclude pgbackrest because it needs to be run in separate container. It should be run individually.
   if [ "$i" = "tests/pgbackrest_backup_fetch_test.sh" ]; then continue ;fi
   # Exclude ssh test because of ssh server container dependency.
   if [ "$i" = "tests/ssh_backup_test.sh" ]; then continue ;fi
-  if [ "$i" = "tests/zzz.sh" ]; then continue ;fi
+  # Exclude throttling test because of different s3 container.
+  if [ "$i" = "tests/wal_perftest_with_throttling.sh" ]; then continue ;fi
   echo
   echo "===== RUNNING $i ====="
   set -x
