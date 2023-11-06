1f6547b747fd9eeb6016320472e7918cc37145b8
KosovGrigorii <72564996+KosovGrigorii@users.noreply.github.com>
2023-11-06 19:56:58 +0500

    fix tests

--- docker/etcd_tests/scripts/run_integration_tests.sh
+++ docker/etcd_tests/scripts/run_integration_tests.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e -x
 
-for i in tests/*; do
+for i in /tmp/tests/*; do
   echo
   echo "===== RUNNING $i ====="
   set -x
-  ./"$i";
+  bash "$i";
 
   set +x
   echo "===== SUCCESS $i ====="
   echo
 done
-popd
--- docker/etcd_tests/scripts/tests/etcd_full_backup_test.sh
+++ docker/etcd_tests/scripts/tests/etcd_full_backup_test.sh
@@ -5,11 +5,11 @@ export WALG_STREAM_CREATE_COMMAND='TMP_DIR=$(mktemp) && etcdctl --endpoints=loca
 export WALG_STREAM_RESTORE_COMMAND='TMP_DIR=$(mktemp) && cat > $TMP_DIR && etcdctl snapshot restore $TMP_DIR --data-dir /tmp/etcd/cluster'
 export WALG_FILE_PREFIX='/tmp/wal-g'
 
-brew services start etcd
+etcd &
 
 etcdctl put greeting "Hello, etcd"
 
-mkdir $WALG_FILE_PREFIX
+mkdir -p $WALG_FILE_PREFIX
 
 wal-g backup-push
 
@@ -25,5 +25,3 @@ if [ "$actual_output" != "$expected_output" ]; then
   echo "Actual output: $actual_output"
   exit 1
 fi
-
-brew services stop etcd
