ddbf002b847ea471a3342779753b9547126f50c3
KosovGrigorii <72564996+KosovGrigorii@users.noreply.github.com>
2023-11-21 14:08:26 +0500

    etcd delete and backup_list commmands added

---
+++ cmd/etcd/delete.go
@@ -0,0 +1,101 @@
+package etcd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/wal-g/tracelog"
+	"github.com/wal-g/wal-g/internal"
+	"github.com/wal-g/wal-g/pkg/storages/storage"
+	"github.com/wal-g/wal-g/utility"
+)
+
+var confirmed = false
+
+// deleteCmd represents the delete command
+var deleteCmd = &cobra.Command{
+	Use:   "delete",
+	Short: internal.DeleteShortDescription,
+}
+
+var deleteBeforeCmd = &cobra.Command{
+	Use:     internal.DeleteBeforeUsageExample, // TODO : improve description
+	Example: internal.DeleteBeforeExamples,
+	Args:    internal.DeleteBeforeArgsValidator,
+	Run:     runDeleteBefore,
+}
+
+var deleteRetainCmd = &cobra.Command{
+	Use:       internal.DeleteRetainUsageExample, // TODO : improve description
+	Example:   internal.DeleteRetainExamples,
+	ValidArgs: internal.StringModifiers,
+	Args:      internal.DeleteRetainArgsValidator,
+	Run:       runDeleteRetain,
+}
+
+var deleteEverythingCmd = &cobra.Command{
+	Use:       internal.DeleteEverythingUsageExample,
+	Example:   internal.DeleteEverythingExamples,
+	ValidArgs: internal.StringModifiersDeleteEverything,
+	Args:      internal.DeleteEverythingArgsValidator,
+	Run:       runDeleteEverything,
+}
+
+func runDeleteBefore(cmd *cobra.Command, args []string) {
+	folder, err := internal.ConfigureFolder()
+	tracelog.ErrorLogger.FatalOnError(err)
+
+	deleteHandler, err := newEtcdDeleteHandler(folder)
+	tracelog.ErrorLogger.FatalOnError(err)
+
+	deleteHandler.HandleDeleteBefore(args, confirmed)
+}
+
+func runDeleteRetain(cmd *cobra.Command, args []string) {
+	folder, err := internal.ConfigureFolder()
+	tracelog.ErrorLogger.FatalOnError(err)
+
+	deleteHandler, err := newEtcdDeleteHandler(folder)
+	tracelog.ErrorLogger.FatalOnError(err)
+
+	deleteHandler.HandleDeleteRetain(args, confirmed)
+}
+
+func runDeleteEverything(cmd *cobra.Command, args []string) {
+	folder, err := internal.ConfigureFolder()
+	tracelog.ErrorLogger.FatalOnError(err)
+
+	deleteHandler, err := newEtcdDeleteHandler(folder)
+	tracelog.ErrorLogger.FatalOnError(err)
+
+	deleteHandler.DeleteEverything(confirmed)
+}
+
+func init() {
+	cmd.AddCommand(deleteCmd)
+	deleteCmd.AddCommand(deleteBeforeCmd, deleteRetainCmd, deleteEverythingCmd)
+	deleteCmd.PersistentFlags().BoolVar(&confirmed, internal.ConfirmFlag, false, "Confirms backup deletion")
+}
+
+func newEtcdDeleteHandler(folder storage.Folder) (*internal.DeleteHandler, error) {
+	backups, err := internal.GetBackupSentinelObjects(folder)
+	if err != nil {
+		return nil, err
+	}
+
+	backupObjects := make([]internal.BackupObject, 0, len(backups))
+	for _, object := range backups {
+		backupObjects = append(backupObjects, internal.NewDefaultBackupObject(object))
+	}
+
+	return internal.NewDeleteHandler(folder, backupObjects, makeLessFunc()), nil
+}
+
+func makeLessFunc() func(object1, object2 storage.Object) bool {
+	return func(object1, object2 storage.Object) bool {
+		time1, ok1 := utility.TryFetchTimeRFC3999(object1.GetName())
+		time2, ok2 := utility.TryFetchTimeRFC3999(object2.GetName())
+		if !ok1 || !ok2 {
+			return object2.GetLastModified().After(object1.GetLastModified())
+		}
+		return time1 < time2
+	}
+}
