54fe73fbf59bbe2ff92578b9b5555a53ece3e557
Stepan Filippov <stepan.filippov@yandex.ru>
2024-01-18 14:24:21 +0500

    better test

--- docker/gp_tests/scripts/tests/partial_namespace_restoe.sh
+++ docker/gp_tests/scripts/tests/partial_namespace_restoe.sh
@@ -23,19 +23,24 @@ it=10
 
 psql -p 6000 -c "DROP DATABASE IF EXISTS db"
 psql -p 6000 -c "CREATE DATABASE db"
-psql -p 6000 -d db -c "CREATE SCHEMA restore"
+psql -p 6000 -d db -c "CREATE SCHEMA restore1"
+psql -p 6000 -d db -c "CREATE SCHEMA restore2"
 psql -p 6000 -d db -c "CREATE SCHEMA partial"
 psql -p 6000 -d db -c "CREATE SCHEMA skip"
 
-psql -p 6000 -d db -c "CREATE TABLE restore.table1 AS SELECT a FROM generate_series(1,$n) AS a;"
-psql -p 6000 -d db -c "CREATE TABLE restore.table2 AS SELECT a FROM generate_series(1,$n) AS a;"
+psql -p 6000 -d db -c "CREATE TABLE restore1.table1 AS SELECT a FROM generate_series(1,$n) AS a;"
+psql -p 6000 -d db -c "CREATE TABLE restore1.table2 AS SELECT a FROM generate_series(1,$n) AS a;"
+psql -p 6000 -d db -c "CREATE TABLE restore2.table1 AS SELECT a FROM generate_series(1,$n) AS a;"
+psql -p 6000 -d db -c "CREATE TABLE restore2.table2 AS SELECT a FROM generate_series(1,$n) AS a;"
 psql -p 6000 -d db -c "CREATE TABLE partial.table1 AS SELECT a FROM generate_series(1,$n) AS a;"
 psql -p 6000 -d db -c "CREATE TABLE partial.table2 AS SELECT a FROM generate_series(1,$n) AS a;"
 psql -p 6000 -d db -c "CREATE TABLE skip.table1 AS SELECT a FROM generate_series(1,$n) AS a;"
 psql -p 6000 -d db -c "CREATE TABLE skip.table2 AS SELECT a FROM generate_series(1,$n) AS a;"
 
-psql -p 6000 -d db -c "INSERT INTO restore.table1 SELECT i FROM generate_series(1,$n)i;"
-psql -p 6000 -d db -c "INSERT INTO restore.table2 SELECT i FROM generate_series(1,$n)i;"
+psql -p 6000 -d db -c "INSERT INTO restore1.table1 SELECT i FROM generate_series(1,$n)i;"
+psql -p 6000 -d db -c "INSERT INTO restore1.table2 SELECT i FROM generate_series(1,$n)i;"
+psql -p 6000 -d db -c "INSERT INTO restore2.table1 SELECT i FROM generate_series(1,$n)i;"
+psql -p 6000 -d db -c "INSERT INTO restore2.table2 SELECT i FROM generate_series(1,$n)i;"
 psql -p 6000 -d db -c "INSERT INTO partial.table1 SELECT i FROM generate_series(1,$n)i;"
 psql -p 6000 -d db -c "INSERT INTO partial.table2 SELECT i FROM generate_series(1,$n)i;"
 psql -p 6000 -d db -c "INSERT INTO skip.table1 SELECT i FROM generate_series(1,$n)i;"
@@ -44,15 +49,21 @@ psql -p 6000 -d db -c "INSERT INTO skip.table2 SELECT i FROM generate_series(1,$
 run_backup_logged ${TMP_CONFIG} ${PGDATA}
 stop_and_delete_cluster_dir
 
-wal-g --config=${TMP_CONFIG} backup-fetch LATEST --in-place --restore-only=db/restore/*,db/partial/table2
+wal-g --config=${TMP_CONFIG} backup-fetch LATEST --in-place --restore-only=db/restore*/*,db/partial/table2
 
 start_cluster
 
-if [ "$(psql -p 6000 -t -c "SELECT count(*) FROM restore.table1;" -d db -A)" != $($n * 2) ]; then
-  echo "Error: restore namespace must be restored after partial fetch"
+if [ "$(psql -p 6000 -t -c "SELECT count(*) FROM restore1.table1;" -d db -A)" != $($n * 2) ]; then
+  echo "Error: restore1 namespace must be restored after partial fetch"
   exit 1
-elif [ "$(psql -p 6000 -t -c "SELECT count(*) FROM restore.table2;" -d db -A)" != $($n * 2) ]; then
-  echo "Error: restore namespace must be restored after partial fetch"
+elif [ "$(psql -p 6000 -t -c "SELECT count(*) FROM restore1.table2;" -d db -A)" != $($n * 2) ]; then
+  echo "Error: restore1 namespace must be restored after partial fetch"
+  exit 1
+elif [ "$(psql -p 6000 -t -c "SELECT count(*) FROM restore2.table1;" -d db -A)" != $($n * 2) ]; then
+  echo "Error: restore2 namespace must be restored after partial fetch"
+  exit 1
+elif [ "$(psql -p 6000 -t -c "SELECT count(*) FROM restore2.table2;" -d db -A)" != $($n * 2) ]; then
+  echo "Error: restore2 namespace must be restored after partial fetch"
   exit 1
 elif [ "$(psql -p 6000 -t -c "SELECT count(*) FROM partial.table2;" -d db -A)" != $($n * 2) ]; then
   echo "Error: table2 in partial namespace must be restored after partial fetch"
@@ -68,7 +79,7 @@ output3=$(psql -p 6000 -t -c "SELECT count(*) FROM skip.table2;" -d db -A 2>&1)
 set -e
 
 if ! echo $output1 | grep -q "$EXPECTED_ERROR_MSG"; then
-  echo "Error: skip namespace must be emtpy after partial fetch"
+  echo "Error: table1 in partial namespace must be empty after partial fetch"
   echo $output1
   exit 1
 elif ! echo $output2 | grep -q "$EXPECTED_ERROR_MSG"; then
@@ -76,7 +87,7 @@ elif ! echo $output2 | grep -q "$EXPECTED_ERROR_MSG"; then
   echo $output2
   exit 1
 elif ! echo $output3 | grep -q "$EXPECTED_ERROR_MSG"; then
-  echo "Error: to_skip database directory must be emtpy after partial fetch"
+  echo "Error: skip namespace must be emtpy after partial fetch"
   echo $output3
   exit 1
 fi
