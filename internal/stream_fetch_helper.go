c6f4860892fe00d5d3628c13ce05d252926331d7
Nikolay Antonov <ostinru@gmail.com>
2024-01-21 20:50:41 +0600

    WALG-1627. Fix file double Close() Don't close file in calling code, because MergeWriter gets ownership of file descriptor (as it stated in godoc)

--- internal/stream_fetch_helper.go
+++ internal/stream_fetch_helper.go
@@ -70,7 +70,6 @@ func DownloadAndDecompressStream(backup Backup, writeCloser io.WriteCloser) erro
 // DownloadAndDecompressSplittedStream downloads, decompresses and writes stream to stdout
 func DownloadAndDecompressSplittedStream(backup Backup, blockSize int, extension string,
 	writeCloser io.WriteCloser, maxDownloadRetry int) error {
-	defer utility.LoggedClose(writeCloser, "")
 
 	decompressor := compression.FindDecompressor(extension)
 	if decompressor == nil {
