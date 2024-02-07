6f1a9cc7c470915cd53f81ab81a33cecd5f4fea1
Pavel Reshetnikov <92433826+ReshetnikovPavel@users.noreply.github.com>
2024-02-07 14:36:33 +0500

    Add tests for partitionStrings (#1598)
    * Add tests for partitionStrings
    
    * Add testcases with longer strings
    
    * Handle nonpositive blockSize
    
    * Fix return value if <= 0
    
    ---------
    
    Co-authored-by: Pavel Reshetnikov <pavelresh@yandex.ru>

---
+++ pkg/storages/s3/uploader_test.go
@@ -0,0 +1,62 @@
+package s3
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPartitionStrings(t *testing.T) {
+	testCases := []struct {
+		strings   []string
+		blockSize int
+		expected  [][]string
+	}{
+		{[]string{"1", "2", "3", "4", "5"}, 2, [][]string{{"1", "2"}, {"3", "4"}, {"5"}}},
+		{[]string{"1", "2", "3", "4", "5", "6"}, 3, [][]string{{"1", "2", "3"}, {"4", "5", "6"}}},
+		{[]string{"1", "2", "3", "4", "5"}, 1000, [][]string{{"1", "2", "3", "4", "5"}}},
+		{[]string{"1", "2", "3", "4", "5"}, 1, [][]string{{"1"}, {"2"}, {"3"}, {"4"}, {"5"}}},
+		{[]string{"1", "2", "3", "4", "5"}, 0, [][]string{{"1", "2", "3", "4", "5"}}},
+		{[]string{"1", "2", "3", "4", "5"}, -1, [][]string{{"1", "2", "3", "4", "5"}}},
+		{[]string{"1", "2"}, 5, [][]string{{"1", "2"}}},
+		{[]string{"1"}, 1, [][]string{{"1"}}},
+		{[]string{}, 1, [][]string{}},
+		{[]string{"foo", "bar", "baz"}, 4, [][]string{{"foo", "bar", "baz"}}},
+		{[]string{"foo", "bar", "baz"}, 3, [][]string{{"foo", "bar", "baz"}}},
+		{[]string{"foo", "bar", "baz"}, 2, [][]string{{"foo", "bar"}, {"baz"}}},
+		{[]string{"foo", "bar", "baz"}, 1, [][]string{{"foo"}, {"bar"}, {"baz"}}},
+		{[]string{"foo", "bar", "baz"}, 0, [][]string{{"foo", "bar", "baz"}}},
+		{[]string{"foo", "bar", "baz"}, -1, [][]string{{"foo", "bar", "baz"}}},
+		{
+			[]string{
+				"This is a long string that contains a lot of words and characters for testing purposes.",
+				"The quick brown fox jumps over the lazy dog",
+				"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+				"Hello, World!",
+				" ",
+				"",
+			},
+			2,
+			[][]string{
+				{
+					"This is a long string that contains a lot of words and characters for testing purposes.",
+					"The quick brown fox jumps over the lazy dog",
+				},
+				{
+					"Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+					"Hello, World!",
+				},
+				{
+					" ",
+					"",
+				},
+			},
+		},
+	}
+	for i, tc := range testCases {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			actual := partitionStrings(tc.strings, tc.blockSize)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
