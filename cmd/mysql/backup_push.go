4a1be4a48010068b495f8c2f1916354fcb091138
Nikolay Antonov <ostinru@gmail.com>
2024-03-01 17:01:49 +0600

    `xtrabackup --fifo-streams=N` PoC

--- cmd/mysql/backup_push.go
+++ cmd/mysql/backup_push.go
@@ -29,6 +29,7 @@ var (
 			tracelog.ErrorLogger.FatalOnError(err)
 		},
 		Run: func(cmd *cobra.Command, args []string) {
+			const backupStreamsCnt = 1
 			internal.ConfigureLimiters()
 
 			uploader, err := internal.ConfigureSplitUploader()
@@ -48,6 +49,7 @@ var (
 				backupCmd,
 				permanent,
 				true,
+				backupStreamsCnt,
 				userData,
 				mysql.NewNoDeltaBackupConfigurator(),
 			)
