4e9d9fafd40d1a7c0fc8568e5c454a084f065e5a
Stepan Filippov <stepan.filippov@yandex.ru>
2024-03-06 15:21:29 +0500

    fixed libsodium

--- internal/configure_crypter_libsodium.go
+++ internal/configure_crypter_libsodium.go
@@ -7,17 +7,18 @@ import (
 	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 
+	conf "github.com/wal-g/wal-g/internal/config"
 	"github.com/wal-g/wal-g/internal/crypto"
 	"github.com/wal-g/wal-g/internal/crypto/libsodium"
 )
 
 func configureLibsodiumCrypter(config *viper.Viper) (crypto.Crypter, error) {
-	if viper.IsSet(LibsodiumKeySetting) {
-		return libsodium.CrypterFromKey(viper.GetString(LibsodiumKeySetting), viper.GetString(LibsodiumKeyTransform)), nil
+	if viper.IsSet(conf.LibsodiumKeySetting) {
+		return libsodium.CrypterFromKey(viper.GetString(conf.LibsodiumKeySetting), viper.GetString(conf.LibsodiumKeyTransform)), nil
 	}
 
-	if viper.IsSet(LibsodiumKeyPathSetting) {
-		return libsodium.CrypterFromKeyPath(viper.GetString(LibsodiumKeyPathSetting), viper.GetString(LibsodiumKeyTransform)), nil
+	if viper.IsSet(conf.LibsodiumKeyPathSetting) {
+		return libsodium.CrypterFromKeyPath(viper.GetString(conf.LibsodiumKeyPathSetting), viper.GetString(conf.LibsodiumKeyTransform)), nil
 	}
 
 	return nil, errors.New("there is no any supported libsodium crypter configuration")
