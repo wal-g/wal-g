7dd97de7256aae136307273c8dd8ccaa85eb7141
Stepan Filippov <stepan.filippov@yandex.ru>
2024-03-29 10:04:33 +0500

    fix for success check

--- pkg/storages/s3/transport_with_logging.go
+++ pkg/storages/s3/transport_with_logging.go
@@ -21,7 +21,7 @@ func (s *loggingTransport) RoundTrip(r *http.Request) (*http.Response, error) {
 	statistics.WriteStatusCodeMetric(resp.StatusCode)
 	tracelog.DebugLogger.Printf("request %s response: %d request: %d", r.Method, resp.ContentLength, r.ContentLength)
 
-	if resp.StatusCode != http.StatusAccepted && r.Method == "GET" {
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 && r.Method == "GET" {
 		statistics.WalgMetrics.S3BytesRead.Add(float64(resp.ContentLength))
 	}
 	if resp.StatusCode >= 200 && resp.StatusCode < 300 && (r.Method == "PUT" || r.Method == "POST") {
