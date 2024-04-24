f56303b7685a9e21e1ff953f4fdc7bbe9c8d5c8c
ValeriaIM <50073123+ValeriaIM@users.noreply.github.com>
2024-04-24 18:05:03 +0500

    unit test for internal/backup.go/FetchMetadata
    Выполнение ToDo в рамках курса.
    // TODO : unit tests
    func (backup *Backup) FetchMetadata(metadataDto interface{}) error {
    	return FetchDto(backup.Folder, metadataDto, backup.getMetadataPath())
    }
    
    студентка Мухаметдинова Валерия

--- internal/backup_fetch_test.go
+++ internal/backup_fetch_test.go
@@ -2,7 +2,9 @@ package internal_test
 
 import (
 	"bytes"
+	"path"
 	"testing"
+	"time"
 
 	"github.com/wal-g/wal-g/internal"
 	conf "github.com/wal-g/wal-g/internal/config"
@@ -16,6 +18,17 @@ func init() {
 	internal.ConfigureSettings("")
 	conf.InitConfig()
 	conf.Configure()
+}
+
+var testBackup = internal.GenericMetadata{
+	BackupName:       "stream_20231118T140000Z",
+	UncompressedSize: int64(10),
+	CompressedSize:   int64(100),
+	Hostname:         "TestHost",
+	StartTime:        time.Date(2002, 3, 21, 0, 0, 0, 0, time.UTC),
+	FinishTime:       time.Date(2002, 3, 21, 0, 0, 0, 0, time.UTC),
+	IsPermanent:      false,
+	UserData:         "Data",
 }
 
 func TestGetBackupByName_Latest(t *testing.T) {
@@ -47,4 +60,40 @@ func TestGetBackupByName_NotExists(t *testing.T) {
 	_, err := internal.GetBackupByName("base_321", utility.BaseBackupPath, folder)
 	assert.Error(t, err)
 	assert.IsType(t, internal.NewBackupNonExistenceError(""), err)
+}
+
+func TestFetchMetadata(t *testing.T) {
+	folder := testtools.CreateMockStorageFolder()
+
+	b := path.Join(utility.BaseBackupPath, testLatestBackup.BackupName)
+	_ = folder.PutObject(b, &bytes.Buffer{})
+
+	// Создание объекта Backup с помощью вспомогательной функции
+	backup, err0 := internal.GetBackupByName(internal.LatestString, utility.BaseBackupPath, folder)
+	assert.NoError(t, err0)
+
+	// Вызов функции FetchMetadata
+	meta := internal.GenericMetadata{
+		UncompressedSize: int64(10),
+		CompressedSize:   int64(100),
+		Hostname:         "TestHost",
+		StartTime:        time.Date(2002, 3, 21, 0, 0, 0, 0, time.UTC),
+		FinishTime:       time.Date(2002, 3, 21, 0, 0, 0, 0, time.UTC),
+		IsPermanent:      false,
+		UserData:         "Data",
+	}
+
+	err := backup.FetchMetadata(&meta)
+
+	// Проверка результата
+	assert.NoError(t, err)
+	assert.Equal(t, testLatestBackup.BackupName, meta.BackupName)
+
+	isEqualTimeStart := testLatestBackup.StartTime.Equal(meta.StartTime)
+	assert.True(t, isEqualTimeStart)
+
+	isEqualTimeFinish := testLatestBackup.FinishTime.Equal(meta.FinishTime)
+	assert.True(t, isEqualTimeFinish)
+
+	assert.Equal(t, testLatestBackup, meta)
 }
