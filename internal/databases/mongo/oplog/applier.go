2a0d1455cda883e5533c422f0736f144e122a3ca
sayedppqq <sayed@appscode.com>
2024-04-25 13:10:41 +0600

    update
    Signed-off-by: sayedppqq <sayed@appscode.com>

--- internal/databases/mongo/oplog/applier.go
+++ internal/databases/mongo/oplog/applier.go
@@ -254,8 +254,7 @@ func indexSpecFromCommitIndexBuilds(op db.Oplog) (string, []client.IndexDocument
 				if !ok {
 					return "", nil, NewTypeAssertionError("bson.D", fmt.Sprintf("indexes[%d]", i), elemE.Value)
 				}
-				for j := range elements {
-					elemE = elements[j]
+				for _, elemE := range elements {
 					if elemE.Key == "key" {
 						if indexSpecs[i].Key, ok = elemE.Value.(bson.D); !ok {
 							return "", nil, NewTypeAssertionError("bson.D", "key", elemE.Value)
