84c7f3e1cab471847322d12a4922f5a911fc4c6b
Danil Kazakov <cmaster057@gmail.com>
2024-04-28 16:57:05 +0500

    move universal tool preparing to cmd

--- main/universal/main.go
+++ main/universal/main.go
@@ -1,56 +1,9 @@
 package main
 
 import (
-	"github.com/spf13/cobra"
-	"github.com/wal-g/wal-g/cmd/etcd"
-	"github.com/wal-g/wal-g/cmd/fdb"
-	"github.com/wal-g/wal-g/cmd/gp"
-	"github.com/wal-g/wal-g/cmd/mongo"
-	"github.com/wal-g/wal-g/cmd/mysql"
-	"github.com/wal-g/wal-g/cmd/pg"
-	"github.com/wal-g/wal-g/cmd/redis"
-	"github.com/wal-g/wal-g/cmd/sqlserver"
-)
-
-var (
-	universalCmd = &cobra.Command{
-		Use:   "wal-g",
-		Short: "Universal database backup tool",
-	}
+	"github.com/wal-g/wal-g/cmd/universal"
 )
 
 func main() {
-	etcdCmd := etcd.GetCmd()
-	etcdCmd.Use = "etcd"
-	universalCmd.AddCommand(etcdCmd)
-
-	fdbCmd := fdb.GetCmd()
-	fdbCmd.Use = "fdb"
-	universalCmd.AddCommand(fdbCmd)
-
-	gpCmd := gp.GetCmd()
-	gpCmd.Use = "gp"
-	universalCmd.AddCommand(gpCmd)
-
-	mongoCmd := mongo.GetCmd()
-	mongoCmd.Use = "mongo"
-	universalCmd.AddCommand(mongoCmd)
-
-	mysqlCmd := mysql.GetCmd()
-	mysqlCmd.Use = "mysql"
-	universalCmd.AddCommand(mysqlCmd)
-
-	pgCmd := pg.GetCmd()
-	pgCmd.Use = "pg"
-	universalCmd.AddCommand(pgCmd)
-
-	redisCmd := redis.GetCmd()
-	redisCmd.Use = "redis"
-	universalCmd.AddCommand(redisCmd)
-
-	sqlserverCmd := sqlserver.GetCmd()
-	sqlserverCmd.Use = "sqlserver"
-	universalCmd.AddCommand(sqlserverCmd)
-
-	universalCmd.Execute()
+	universal.Execute()
 }
