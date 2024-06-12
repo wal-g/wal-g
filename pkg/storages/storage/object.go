63a6b3a21fd7fdbf33ba0b7abbc0d7b261f8a6cf
Alex Nikolaev <nlxn@yandex.ru>
2024-06-12 09:52:33 +0400

    Create MockObject

--- pkg/storages/storage/object.go
+++ pkg/storages/storage/object.go
@@ -4,6 +4,8 @@ import (
 	"time"
 )
 
+//go:generate mockgen -destination=../../../test/mocks/mock_object.go -package mocks -build_flags -mod=readonly github.com/wal-g/wal-g/pkg/storages/storage Object
+
 type Object interface {
 	GetName() string
 	GetLastModified() time.Time
