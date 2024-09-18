532b35a3438887faf775c3b211b1921347d44db7
believemenot <believemenot@yandex-team.ru>
2024-09-18 15:44:17 +0300

    delete redis backup

---
+++ cmd/redis/delete.go
@@ -0,0 +1,43 @@
+package redis
+
+import (
+	"context"
+	"github.com/wal-g/wal-g/internal/databases/redis"
+	"os"
+	"syscall"
+
+	"github.com/spf13/cobra"
+	"github.com/wal-g/tracelog"
+	"github.com/wal-g/wal-g/internal"
+	"github.com/wal-g/wal-g/utility"
+)
+
+const backupDeleteShortDescription = "Deletes backup data from storage"
+
+var (
+	confirmedBackupDelete bool
+)
+
+// backupDeleteCmd represents the backupDelete command
+var backupDeleteCmd = &cobra.Command{
+	Use:   "backup-delete <backup-name>",
+	Short: backupDeleteShortDescription,
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		ctx, cancel := context.WithCancel(context.Background())
+		signalHandler := utility.NewSignalHandler(ctx, cancel, []os.Signal{syscall.SIGINT, syscall.SIGTERM})
+		defer func() { _ = signalHandler.Close() }()
+
+		backupName := args[0]
+
+		storage, err := internal.ConfigureStorage()
+
+		err = redis.HandleBackupDelete(storage.RootFolder().GetSubFolder(utility.BaseBackupPath), backupName, !confirmedBackupDelete)
+		tracelog.ErrorLogger.FatalOnError(err)
+	},
+}
+
+func init() {
+	backupDeleteCmd.Flags().BoolVar(&confirmedBackupDelete, internal.ConfirmFlag, false, "Confirms backup deletion")
+	cmd.AddCommand(backupDeleteCmd)
+}
