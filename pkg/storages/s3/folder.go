43f0427c719e9527c25cb0ba372085ba0562ee53
debebantur <stepan.filippov@yandex.ru>
2025-01-10 05:48:12 +0000

    fixes

--- pkg/storages/s3/folder.go
+++ pkg/storages/s3/folder.go
@@ -135,7 +135,7 @@ func (folder *Folder) ListFolder() (objects []storage.Object, subFolders []stora
 				continue
 			}
 			objectRelativePath := strings.TrimPrefix(*object.Key, folder.path)
-			objects = append(objects, storage.NewLocalObject(objectRelativePath, *object.LastModified, *object.Size))
+			objects = append(objects, storage.NewLocalObject(objectRelativePath, *object.LastModified, *object.Size))//TODO also list versions
 		}
 	}
 
@@ -198,9 +198,11 @@ func (folder *Folder) listObjectsPagesV2(prefix *string, delimiter *string, maxK
 
 func (folder *Folder) DeleteObjects(objectRelativePaths []string) error {
 	parts := partitionStrings(objectRelativePaths, 1000)
+	needsVersioning := folder.isVersioningEnabled() //TODO add manyal deletion
+
 	for _, part := range parts {
 		input := &s3.DeleteObjectsInput{Bucket: folder.bucket, Delete: &s3.Delete{
-			Objects: folder.partitionToObjects(part),
+			Objects: folder.partitionToObjects(part, needsVersioning),
 		}}
 		_, err := folder.s3API.DeleteObjects(input)
 		if err != nil {
@@ -208,6 +210,42 @@ func (folder *Folder) DeleteObjects(objectRelativePaths []string) error {
 		}
 	}
 	return nil
+}
+
+func (folder *Folder) getObjectVersions(key string) ([]*s3.ObjectIdentifier, error) {
+	inp := &s3.ListObjectVersionsInput{
+		Bucket: folder.bucket,
+		Prefix: aws.String(folder.path + key),
+	}
+
+	out, err := folder.s3API.ListObjectVersions(inp)
+	if err != nil {
+		return nil, err
+	}
+	list := make([]*s3.ObjectIdentifier, 0)
+	for _, version := range out.Versions {
+		list = append(list, &s3.ObjectIdentifier{Key: version.Key, VersionId: version.VersionId})
+	}
+
+	for _, deleteMarker := range out.DeleteMarkers {
+		list = append(list, &s3.ObjectIdentifier{Key: deleteMarker.Key, VersionId: deleteMarker.VersionId})
+	}
+
+	return list, nil
+}
+
+func (folder *Folder) isVersioningEnabled() bool {
+	result, err := folder.s3API.GetBucketVersioning(&s3.GetBucketVersioningInput{
+		Bucket: folder.bucket,
+	})
+	if err != nil {
+		return false
+	}
+
+	if result.Status != nil && *result.Status == s3.BucketVersioningStatusEnabled {
+		return true
+	}
+	return false
 }
 
 func (folder *Folder) Validate() error {
@@ -227,10 +265,20 @@ func (folder *Folder) Validate() error {
 	return nil
 }
 
-func (folder *Folder) partitionToObjects(keys []string) []*s3.ObjectIdentifier {
-	objects := make([]*s3.ObjectIdentifier, len(keys))
-	for id, key := range keys {
-		objects[id] = &s3.ObjectIdentifier{Key: aws.String(folder.path + key)}
+func (folder *Folder) partitionToObjects(keys []string, versioningEnabled bool) []*s3.ObjectIdentifier {
+	objects := make([]*s3.ObjectIdentifier, 0, len(keys))
+	for _, key := range keys {
+		if versioningEnabled {
+			versions, err := folder.getObjectVersions(key)
+			if err != nil {
+				tracelog.ErrorLogger.Printf("failed to list versions: %v", err)
+				//TODO to error or not to error
+			}
+			objects = append(objects, versions...)
+		} else {
+			objects = append(objects, &s3.ObjectIdentifier{Key: aws.String(folder.path + key)})
+		}
+		//objects[id] = &s3.ObjectIdentifier{Key: aws.String(folder.path + key)}
 	}
 	return objects
 }
