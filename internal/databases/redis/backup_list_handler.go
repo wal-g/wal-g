e8fa0ae414a72b2dec5e9e79971e6c2f2f776f8b
Михаил Козлов <110295967+kozlov-ma@users.noreply.github.com>
2025-02-09 13:04:30 +0500

    Add unit tests for internal/databases/redis.HandleDetailedBackupList (#1876)

--- internal/databases/redis/backup_list_handler.go
+++ internal/databases/redis/backup_list_handler.go
@@ -11,7 +11,6 @@ import (
 	"github.com/wal-g/wal-g/pkg/storages/storage"
 )
 
-// TODO : unit tests
 func HandleDetailedBackupList(folder storage.Folder, pretty bool, json bool) {
 	backups, err := internal.GetBackups(folder)
 	if len(backups) == 0 {
