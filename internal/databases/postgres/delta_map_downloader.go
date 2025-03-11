1519a8fe508b5bd283762bef40741ee2173396e3
wangxiaoran <fanfuxiaoran@gmail.com>
2025-03-11 18:11:09 +0800

    Fix delta file not exist in getDeltaMap
    If the first wal file pushed by the wal-g is not started as xxx0
    after the archive_mode enabled, and we start the basebackup, then
    the first delta segment will be missing for that basebackup. So
    does the last delta file, if the last delta segment is the same
    as the first delta segment.
    
    The first wal log file after starting the basebackup doesn't
    contain XLogRecord tail in the head, so we can parse the wal files
    to extract locations.
    
    Add test 'delta_backup_wal_delta_missing_test.sh' for this commit

--- internal/databases/postgres/delta_map_downloader.go
+++ internal/databases/postgres/delta_map_downloader.go
@@ -4,22 +4,55 @@ import (
 	"github.com/pkg/errors"
 	"github.com/wal-g/tracelog"
 	"github.com/wal-g/wal-g/internal"
+	"github.com/wal-g/wal-g/internal/walparser"
 )
 
 func getDeltaMap(reader internal.StorageFolderReader,
 	timeline uint32,
 	firstUsedLSN,
 	firstNotUsedLSN LSN) (PagedFileDeltaMap, error) {
+	firstUsedWalSegmentNo := NewWalSegmentNo(firstUsedLSN)
+	firstNotUsedWalSegmentNo := NewWalSegmentNo(firstNotUsedLSN)
 	tracelog.InfoLogger.Printf("Timeline: %d, FirstUsedLsn: %s, FirstNotUsedLsn: %s\n",
 		timeline, firstUsedLSN, firstNotUsedLSN)
 	tracelog.InfoLogger.Printf("First WAL should participate in building delta map: %s",
-		NewWalSegmentNo(firstUsedLSN).GetFilename(timeline))
+		firstUsedWalSegmentNo.GetFilename(timeline))
 	tracelog.InfoLogger.Printf("First WAL shouldn't participate in building delta map: %s",
-		NewWalSegmentNo(firstNotUsedLSN).GetFilename(timeline))
+		firstNotUsedWalSegmentNo.GetFilename(timeline))
 	deltaMap := NewPagedFileDeltaMap()
 	firstUsedDeltaNo, firstNotUsedDeltaNo := getDeltaRange(firstUsedLSN, firstNotUsedLSN)
-	// Get locations from [firstUsedDeltaNo, lastUsedDeltaNo). We use lastUsedDeltaNo in next step
-	err := deltaMap.getLocationsFromDeltas(reader, timeline, firstUsedDeltaNo, firstNotUsedDeltaNo.previous())
+	tracelog.InfoLogger.Printf("First DELTA should participate in building delta map: %s",
+		firstUsedDeltaNo.getFilename(timeline))
+	tracelog.InfoLogger.Printf("First DELTA shouldn't participate in building delta map: %s",
+		firstNotUsedDeltaNo.getFilename(timeline))
+	firstDeltaFile, err := getDeltaFile(reader, firstUsedDeltaNo.getFilename(timeline))
+	var walparser *walparser.WalParser = nil
+	/* First delta file not found , parse it from wal log
+	 * If the first wal file pushed to the wal-g is not started with xxx0, and the basebackup
+	 * started, then the first delta file will always be missing for that basebackup. For this
+	 * case, we will parse the delta map from wals for the first delta segment
+	 */
+	if err != nil {
+		err, walparser = handlFirstDeltaFileMiss(firstUsedDeltaNo, firstUsedWalSegmentNo, firstNotUsedWalSegmentNo, timeline, reader, deltaMap)
+		if err != nil {
+			return deltaMap, errors.Wrapf(err, "Error during downloading first delta file.\n")
+		}
+	} else {
+		deltaMap.AddLocationsToDelta(firstDeltaFile.Locations)
+		walparser = firstDeltaFile.WalParser
+	}
+	if firstUsedDeltaNo == firstNotUsedDeltaNo {
+		return deltaMap, nil
+	}
+
+	/* The last delta segment is the same as the first delta segment */
+	if firstUsedDeltaNo == firstNotUsedDeltaNo.previous() {
+		err = deltaMap.getLocationsFromWals(reader, timeline, firstNotUsedDeltaNo.firstWalSegmentNo(),
+			firstNotUsedWalSegmentNo, walparser)
+		return deltaMap, err
+	}
+	// Get locations from (firstUsedDeltaNo, lastUsedDeltaNo). We use lastUsedDeltaNo in next step
+	err = deltaMap.getLocationsFromDeltas(reader, timeline, firstUsedDeltaNo.next(), firstNotUsedDeltaNo.previous())
 	if err != nil {
 		return deltaMap, errors.Wrapf(err, "Error during fetch locations from delta files.\n")
 	}
@@ -31,7 +64,6 @@ func getDeltaMap(reader internal.StorageFolderReader,
 	}
 	deltaMap.AddLocationsToDelta(lastDeltaFile.Locations)
 
-	firstUsedWalSegmentNo, firstNotUsedWalSegmentNo := getWalSegmentRange(firstNotUsedDeltaNo, firstNotUsedLSN)
 	// we handle WAL files from [firstUsedWalSegmentNo, lastUsedWalSegmentNo]
 	err = deltaMap.getLocationsFromWals(reader, timeline, firstUsedWalSegmentNo,
 		firstNotUsedWalSegmentNo, lastDeltaFile.WalParser)
@@ -52,4 +84,25 @@ func getWalSegmentRange(firstNotUsedDeltaNo DeltaNo, firstNotUsedLsn LSN) (WalSe
 	lastUsedLsn := firstNotUsedLsn - 1
 	lastUsedWalSegmentNo := NewWalSegmentNo(lastUsedLsn)
 	return firstUsedWalSegmentNo, lastUsedWalSegmentNo.Next()
+}
+
+func handlFirstDeltaFileMiss(firstUsedDeltaNo DeltaNo,
+	firstUsedWalSegmentNo WalSegmentNo,
+	firstNotUsedWalSegmentNo WalSegmentNo,
+	timeline uint32,
+	reader internal.StorageFolderReader,
+	deltaMap PagedFileDeltaMap) (error, *walparser.WalParser) {
+	tracelog.InfoLogger.Printf("First delta file is missing, get locations from wals\n")
+	/* The wal file of firstUsedWalSegmentNo should not contain a partitial XLogRecord in the head
+	 * as it is the first wal file after the basebackup
+	 */
+	lastWalSegmengNo := firstUsedDeltaNo.firstWalSegmentNo().add(WalFileInDelta)
+	if lastWalSegmengNo > firstNotUsedWalSegmentNo {
+		lastWalSegmengNo = firstNotUsedWalSegmentNo
+	}
+	walparser := walparser.NewWalParser()
+	err := deltaMap.getLocationsFromWals(reader, timeline, firstUsedWalSegmentNo,
+		lastWalSegmengNo,
+		walparser)
+	return err, walparser
 }
