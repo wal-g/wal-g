acb7fbfd3013b9238ff1048432b7151b1693da23
huluhuifeng <huluhuifeng@hashdata.cn>
2025-05-22 17:15:27 +0800

    Feat: Add new commands to support gpdb/cbdb continuous recovery
    - FollowPrimary: The cluster applies the latest WAL logs and sets the
     recovery_target_action parameter to "shutdown".
    - RecoveryAction: Modifies the recovery_target_action parameter in the
    recovery.conf configuration file across all nodes in the cluster.
    Supported values: "promote", "pause", or "shutdown".

---
+++ internal/databases/greenplum/follow_primary_handler.go
@@ -0,0 +1,110 @@
+package greenplum
+
+import (
+	"fmt"
+	"github.com/spf13/viper"
+	conf "github.com/wal-g/wal-g/internal/config"
+	"path"
+	"strings"
+
+	"github.com/greenplum-db/gp-common-go-libs/cluster"
+	"github.com/wal-g/tracelog"
+)
+
+type FollowPrimaryHandler struct {
+	cluster            *cluster.Cluster
+	stopAtRestorePoint string
+	timeoutInSeconds   int
+}
+
+// nolint:gocritic
+func NewFollowPrimaryHandler(
+	logsDir string,
+	restoreCfgPath, stopAtRestorePoint string,
+	timeoutInSeconds int,
+) *FollowPrimaryHandler {
+	restoreCfg, err := readRestoreConfig(restoreCfgPath)
+	tracelog.ErrorLogger.FatalOnError(err)
+
+	initGpLog(logsDir)
+
+	segmentConfigs := make([]cluster.SegConfig, 0)
+	for contentID, segRestoreCfg := range restoreCfg.Segments {
+		segmentConfigs = append(segmentConfigs, segRestoreCfg.ToSegConfig(contentID))
+	}
+
+	globalCluster := cluster.NewCluster(segmentConfigs)
+	tracelog.DebugLogger.Printf("cluster %v\n", globalCluster)
+
+	return &FollowPrimaryHandler{
+		cluster:            globalCluster,
+		stopAtRestorePoint: stopAtRestorePoint,
+		timeoutInSeconds:   timeoutInSeconds,
+	}
+}
+
+func (fh *FollowPrimaryHandler) Follow() {
+	tracelog.InfoLogger.Println("Updating recovery.conf on segments and master...")
+	fh.updateRecoveryConfigs()
+
+	tracelog.InfoLogger.Println("Running recovery on segments and master...")
+	// Run WAL-G to restore the each segment as a single Postgres instance
+	remoteOutput := fh.cluster.GenerateAndExecuteCommand("Running wal-g",
+		cluster.ON_SEGMENTS|cluster.INCLUDE_MASTER,
+		func(contentID int) string {
+			return fh.buildSegmentStartCommand(contentID)
+		})
+
+	fh.cluster.CheckClusterError(remoteOutput, "Unable to run wal-g", func(contentID int) string {
+		return "Unable to run wal-g"
+	})
+
+	for _, command := range remoteOutput.Commands {
+		tracelog.DebugLogger.Printf("WAL-G output (segment %d):\n%s\n", command.Content, command.Stderr)
+	}
+}
+
+// updateRecoveryConfigs generates and uploads the correct recovery.conf
+// files to each segment instance (including master) so they can recover correctly
+// up to the expected restore point
+func (fh *FollowPrimaryHandler) updateRecoveryConfigs() {
+	recoveryTarget := fh.stopAtRestorePoint
+	tracelog.InfoLogger.Printf("Recovery target is %s", recoveryTarget)
+	restoreCfgMaker := NewRecoveryConfigMaker(
+		"wal-g", conf.CfgFile, recoveryTarget, true)
+
+	remoteOutput := fh.cluster.GenerateAndExecuteCommand("Updating recovery.conf on segments and master",
+		cluster.ON_SEGMENTS|cluster.INCLUDE_MASTER,
+		func(contentID int) string {
+			segment := fh.cluster.ByContent[contentID][0]
+			pathToRestore := path.Join(segment.DataDir, viper.GetString(conf.GPRelativeRecoveryConfPath))
+			fileContents := restoreCfgMaker.Make(contentID, 0)
+			cmd := fmt.Sprintf("cat > %s << EOF\n%s\nEOF", pathToRestore, fileContents)
+			tracelog.DebugLogger.Printf("Command to run on segment %d: %s", contentID, cmd)
+			return cmd
+		})
+
+	fh.cluster.CheckClusterError(remoteOutput, "Unable to update recovery.conf", func(contentID int) string {
+		return fmt.Sprintf("Unable to create recovery.conf on segment %d", contentID)
+	})
+
+	for _, command := range remoteOutput.Commands {
+		tracelog.DebugLogger.Printf("Update recovery.conf output (segment %d):\n%s\n", command.Content, command.Stderr)
+	}
+}
+
+func (fh *FollowPrimaryHandler) buildSegmentStartCommand(contentID int) string {
+	segment := fh.cluster.ByContent[contentID][0]
+	cmd := []string{
+		"pg_ctl",
+		fmt.Sprintf("-t %d", fh.timeoutInSeconds),
+		fmt.Sprintf("-D %s", segment.DataDir),
+		"start",
+		// forward STDOUT& STDERR to log file
+		">>", formatSegmentLogPath(contentID), "2>&1",
+	}
+
+	cmdLine := strings.Join(cmd, " ")
+	tracelog.DebugLogger.Printf("Command to run on segment %d: %s", contentID, cmdLine)
+	return cmdLine
+}
