0b9df0e98581fe5013792569de483f4a4da40b8f
kronwerk <tetragon@mail.ru>
2025-06-16 22:57:09 +0300

    add slots info for rdb-backups also

--- internal/ioextensions/named_reader.go
+++ internal/ioextensions/named_reader.go
@@ -5,17 +5,27 @@ import "io"
 type NamedReader interface {
 	io.Reader
 	Name() string
+	IsExactPath() bool
 }
 
 type NamedReaderImpl struct {
 	io.Reader
 	name string
+	isExactPath bool
 }
 
 func (reader *NamedReaderImpl) Name() string {
 	return reader.name
 }
 
+func (reader *NamedReaderImpl) IsExactPath() bool {
+	return reader.isExactPath
+}
+
 func NewNamedReaderImpl(reader io.Reader, name string) *NamedReaderImpl {
-	return &NamedReaderImpl{reader, name}
+	return &NamedReaderImpl{reader, name, false}
+}
+
+func NewNamedReaderExactPathImpl(reader io.Reader, name string) *NamedReaderImpl {
+	return &NamedReaderImpl{reader, name, true}
 }
