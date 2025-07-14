7f5dc58ccf9cc65dddd6c02618efed59bcb26c82
Imre Nagi <imre.nagi2812@gmail.com>
2025-07-15 06:27:29 +0700

    fix: use ram arn credential provider to allow sts token refresh
    Signed-off-by: Imre Nagi <imre.nagi2812@gmail.com>

--- pkg/storages/oss/storage.go
+++ pkg/storages/oss/storage.go
@@ -3,12 +3,6 @@ package oss
 import (
 	"fmt"
 
-	"github.com/alibabacloud-go/darabonba-openapi/v2/models"
-	sts "github.com/alibabacloud-go/sts-20150401/v2/client"
-	"github.com/alibabacloud-go/tea/dara"
-	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss"
-	osscred "github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss/credentials"
-	"github.com/aliyun/credentials-go/credentials"
 	"github.com/wal-g/wal-g/pkg/storages/storage"
 )
 
@@ -71,70 +65,4 @@ func (s *Storage) ConfigHash() string {
 func (s *Storage) Close() error {
 	// Nothing to close: the oss session doesn't require to be closed
 	return nil
-}
-
-func configureClient(config *Config) (*oss.Client, error) {
-	if config.Region == "" {
-		return nil, fmt.Errorf("oss region is required")
-	}
-
-	accessKeyId := config.AccessKeyId
-	accessKeySecret := config.AccessKeySecret
-	securityToken := config.SecurityToken
-
-	if config.RoleARN != "" {
-		cred, err := credentials.NewCredential(&credentials.Config{
-			Type:            oss.Ptr("access_key"),
-			AccessKeyId:     oss.Ptr(accessKeyId),
-			AccessKeySecret: oss.Ptr(accessKeySecret),
-			SecurityToken:   oss.Ptr(securityToken),
-		})
-		if err != nil {
-			return nil, fmt.Errorf("create credential: %w", err)
-		}
-
-		cfg := &models.Config{
-			RegionId:   oss.Ptr(config.Region),
-			Credential: cred,
-		}
-
-		stsClient, err := sts.NewClient(cfg)
-		if err != nil {
-			return nil, fmt.Errorf("create STS client: %w", err)
-		}
-
-		// Create an AssumeRole request.
-		request := &sts.AssumeRoleRequest{
-			RoleArn:         dara.String(config.RoleARN),
-			RoleSessionName: dara.String(config.RoleSessionName),
-		}
-
-		// Initiate the request.
-		response, err := stsClient.AssumeRole(request)
-		if err != nil {
-			return nil, fmt.Errorf("assume role: %w", err)
-		}
-
-		stsCreds := response.Body.Credentials
-		if stsCreds == nil {
-			return nil, fmt.Errorf("assume role response is missing credentials")
-		}
-
-		accessKeyId = *stsCreds.AccessKeyId
-		accessKeySecret = *stsCreds.AccessKeySecret
-		securityToken = *stsCreds.SecurityToken
-	}
-
-	credentialProvider := osscred.NewStaticCredentialsProvider(
-		accessKeyId,
-		accessKeySecret,
-		securityToken,
-	)
-
-	ossConfig := oss.LoadDefaultConfig().
-		WithRegion(config.Region).
-		WithCredentialsProvider(credentialProvider).
-		WithSignatureVersion(oss.SignatureVersionV4)
-
-	return oss.NewClient(ossConfig), nil
 }
