4657570080bc6a3af227cd277ff0a15f787169bc
Ilia Kovalchuk <sckremyulock@gmail.com>
2025-08-01 11:53:13 +0500

    debug prints removed

--- internal/databases/mongo/binary_backup_fetch_handler.go
+++ internal/databases/mongo/binary_backup_fetch_handler.go
@@ -3,7 +3,6 @@ package mongo
 import (
 	"context"
 	"fmt"
-	"github.com/wal-g/tracelog"
 	"github.com/wal-g/wal-g/internal"
 	"github.com/wal-g/wal-g/internal/databases/mongo/binary"
 	"github.com/wal-g/wal-g/utility"
@@ -24,48 +23,46 @@ func HandleBinaryFetchPush(
 	if err != nil {
 		return err
 	}
-	tracelog.InfoLogger.Println("t1")
+
 	localStorage := binary.CreateLocalStorage(config.GetDBPath())
-	tracelog.InfoLogger.Println("t2")
 	uploader, err := internal.ConfigureUploader()
 	if err != nil {
 		return err
 	}
 	uploader.ChangeDirectory(utility.BaseBackupPath + "/")
-	tracelog.InfoLogger.Println("t3")
+
 	if minimalConfigPath == "" {
 		minimalConfigPath, err = config.SaveConfigToTempFile("storage", "systemLog")
 		if err != nil {
 			return err
 		}
 	}
-	tracelog.InfoLogger.Println("t4")
+
 	restoreService, err := binary.CreateRestoreService(ctx, localStorage, uploader, minimalConfigPath)
 	if err != nil {
 		return err
 	}
-	tracelog.InfoLogger.Println("t5")
+
 	rsConfig := binary.NewRsConfig(rsName, rsMembers, rsMemberIDs)
 	if err = rsConfig.Validate(); err != nil {
 		return err
 	}
-	tracelog.InfoLogger.Println("t6")
+
 	shConfig := binary.NewShConfig(shardName, mongoCfgConnectionString)
 	if err = shConfig.Validate(); err != nil {
 		return fmt.Errorf("ShConfig validation failed: %v", err)
 	}
-	tracelog.InfoLogger.Println("t7")
+
 	mongocfgConfig, err := binary.NewMongoCfgConfig(shardConnectionStrings)
 	if err != nil {
 		return err
 	}
-	tracelog.InfoLogger.Println("t8")
+
 	// check backup existence and resolve flag LATEST
 	backup, err := internal.GetBackupByName(backupName, "", uploader.Folder())
 	if err != nil {
 		return err
 	}
-	tracelog.InfoLogger.Println("t9")
 
 	var replyOplogConfig binary.ReplyOplogConfig
 	if pitrSince != "" && pitrUntil != "" {
@@ -74,7 +71,6 @@ func HandleBinaryFetchPush(
 			return err
 		}
 	}
-	tracelog.InfoLogger.Println("t10")
 
 	return restoreService.DoRestore(
 		rsConfig,
