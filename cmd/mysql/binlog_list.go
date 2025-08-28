6dd3223d1a6e8e0beea1641062823ae1b707b579
zkramorev <zkramorev@gmail.com>
2025-08-28 10:32:41 +0300

    Refactor code long lines

--- cmd/mysql/binlog_list.go
+++ cmd/mysql/binlog_list.go
@@ -30,8 +30,10 @@ var binlogListCmd = &cobra.Command{
 }
 
 func init() {
-	binlogListCmd.PersistentFlags().StringVar(&listSince, "since", "", "show binlogs modified since this time (e.g., '2h', '30m', '2023-01-01T10:00:00Z', 'LATEST')")
-	binlogListCmd.PersistentFlags().StringVar(&listUntil, "until", "", "show binlogs modified until this time (e.g., '1h', '2023-01-01T12:00:00Z')")
+	binlogListCmd.PersistentFlags().StringVar(&listSince, "since", "",
+		"show binlogs modified since this time (e.g., '2h', '30m', '2023-01-01T10:00:00Z', 'LATEST')")
+	binlogListCmd.PersistentFlags().StringVar(&listUntil, "until", "",
+		"show binlogs modified until this time (e.g., '1h', '2023-01-01T12:00:00Z')")
 	binlogListCmd.PersistentFlags().BoolVar(&prettyOutput, "pretty", false, "pretty print the output (table format)")
 	binlogListCmd.PersistentFlags().BoolVar(&jsonOutput, "json", false, "output in JSON format")
 	cmd.AddCommand(binlogListCmd)
