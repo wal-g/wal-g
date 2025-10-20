be764c93efb7d6c3507f627e8cdfb278fa0e8aa6
zkramorev <zkramorev@yandex-team.ru>
2025-10-20 11:40:35 +0300

    rerun docker test

--- internal/databases/mysql/binlog_server_handler.go
+++ internal/databases/mysql/binlog_server_handler.go
@@ -229,10 +229,12 @@ func (h Handler) HandleBinlogDump(pos mysql.Position) (*replication.BinlogStream
 		if err != nil {
 			return nil, err
 		}
+
 		err = syncBinlogFiles(pos, startTime, globalStreamer)
 		if err != nil {
 			return nil, err
 		}
+
 	} else {
 		syncMutex.Unlock()
 		tracelog.InfoLogger.Println("Sync already started")
