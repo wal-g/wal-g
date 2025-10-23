9b55ea6b3f02f993b86fcad037176c6b98e10c65
zkramorev <zkramorev@yandex-team.ru>
2025-10-23 16:17:53 +0300

    refactor: reconnection test

--- internal/databases/mysql/binlog_server_handler.go
+++ internal/databases/mysql/binlog_server_handler.go
@@ -229,17 +229,14 @@ func (h Handler) HandleBinlogDump(pos mysql.Position) (*replication.BinlogStream
 		if err != nil {
 			return nil, err
 		}
-
 		err = syncBinlogFiles(pos, startTime, globalStreamer)
 		if err != nil {
 			return nil, err
 		}
-
 	} else {
 		syncMutex.Unlock()
 		tracelog.InfoLogger.Println("Sync already started")
 	}
-
 	return globalStreamer, nil
 }
 
