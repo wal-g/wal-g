53a24d57b937c6dc6dcd1c6be302f4914eed4d4b
den-ibr <147547815+den-ibr@users.noreply.github.com>
2026-01-20 10:52:50 +0500

    Add 'none' compression method (#2156)
    * Add none compression
    
    * Refactor registration of none compression
    
    * Fix (?) TestCompressAndEncryptErrorPropagation()
    
    * Change the logic of testCompressAndEncryptErrorPropagation()

--- internal/compression/compression_default.go
+++ internal/compression/compression_default.go
@@ -7,17 +7,20 @@ import (
 	"github.com/wal-g/wal-g/internal/compression/gzip"
 	"github.com/wal-g/wal-g/internal/compression/lz4"
 	"github.com/wal-g/wal-g/internal/compression/lzma"
+	"github.com/wal-g/wal-g/internal/compression/none"
 )
 
-var CompressingAlgorithms = []string{lz4.AlgorithmName, lzma.AlgorithmName}
+var CompressingAlgorithms = []string{lz4.AlgorithmName, lzma.AlgorithmName, none.AlgorithmName}
 
 var Compressors = map[string]Compressor{
 	lz4.AlgorithmName:  lz4.Compressor{},
 	lzma.AlgorithmName: lzma.Compressor{},
+	none.AlgorithmName: none.Compressor{},
 }
 
 var Decompressors = []Decompressor{
 	lz4.Decompressor{},
 	lzma.Decompressor{},
 	gzip.Decompressor{},
+	none.Decompressor{},
 }
