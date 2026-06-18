1cd4935f22535d6cc6bbbd89f80b612c380bf571
Stepan Filippov <43007025+debebantur@users.noreply.github.com>
2026-06-18 19:58:56 +0500

    Daemon crash fix (#2341)
    - Daemon message length check
    - Calling defer after nil check
    - Addressing after length check
    - Fix possible skip of empty arg

--- internal/databases/postgres/daemon_handler.go
+++ internal/databases/postgres/daemon_handler.go
@@ -194,6 +194,9 @@ func (r SocketMessageReader) Next() (messageType daemon.SocketMessageType, messa
 	if err != nil {
 		return daemon.ErrorType, nil, fmt.Errorf("fail to read message len: %w", err)
 	}
+	if messageLength < 3 {
+		return daemon.ErrorType, nil, fmt.Errorf("message too short: %d", messageLength)
+	}
 	messageBody = make([]byte, messageLength-3)
 	_, err = io.ReadFull(r.c, messageBody)
 	if err != nil {
@@ -222,11 +225,11 @@ func HandleDaemon(options DaemonOptions) {
 	SetupSignalListener()
 
 	multiSt, err := internal.ConfigureMultiStorage(true)
-	defer utility.LoggedClose(multiSt, "close multi-storage")
 	if err != nil {
 		tracelog.ErrorLogger.Fatal("configure multi-storage: %w", err)
 		return
 	}
+	defer utility.LoggedClose(multiSt, "close multi-storage")
 
 	for {
 		fd, err := l.Accept()
