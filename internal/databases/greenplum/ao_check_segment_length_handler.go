e4843b849ca05a27655794888341de2662035853
Stepan Filippov <43007025+debebantur@users.noreply.github.com>
2026-06-24 23:50:52 +0500

    Greenplum: fix length check query (#2365)
    * fix query
    
    * Update internal/databases/greenplum/ao_check_segment_length_handler.go
    
    Co-authored-by: Philip Dubé <159546+serprex@users.noreply.github.com>
    
    * style fixes
    
    ---------
    
    Co-authored-by: reshke <reshkekirill@gmail.com>
    Co-authored-by: Philip Dubé <159546+serprex@users.noreply.github.com>

--- internal/databases/greenplum/ao_check_segment_length_handler.go
+++ internal/databases/greenplum/ao_check_segment_length_handler.go
@@ -180,10 +180,36 @@ func (checker *AOLengthCheckSegmentHandler) connect(ctx context.Context, db stri
 }
 
 func (checker *AOLengthCheckSegmentHandler) getTablesSizes(ctx context.Context, conn *pgx.Conn, dbOID uint32) (map[string]relNames, error) {
-	rows, err := conn.Query(ctx, `SELECT a.relfilenode file, a.relname tname, b.relname segname 
-	FROM (SELECT relname, relid, segrelid, relpersistence, relfilenode FROM pg_class JOIN pg_appendonly ON oid = relid) a,
-	(SELECT relname, segrelid FROM pg_class JOIN pg_appendonly ON oid = segrelid) b
-	WHERE a.relpersistence = 'p' AND a.segrelid = b.segrelid;`)
+	rows, err := conn.Query(ctx, `SELECT 
+  a.relfilenode file, 
+  pg_catalog.quote_ident(n.nspname) OPERATOR(pg_catalog.||) '.' OPERATOR(pg_catalog.||) pg_catalog.quote_ident(a.relname) tname, 
+  b.relname segname 
+FROM 
+  (
+    SELECT 
+      relname, 
+      relid, 
+      segrelid, 
+      relpersistence, 
+      relfilenode, 
+      relnamespace 
+    FROM 
+      pg_class 
+      JOIN pg_appendonly ON oid OPERATOR(pg_catalog.=) relid
+  ) a, 
+  (
+    SELECT 
+      relname, 
+      segrelid 
+    FROM 
+      pg_class 
+      JOIN pg_appendonly ON oid OPERATOR(pg_catalog.=) segrelid
+  ) b, 
+  pg_namespace n 
+WHERE 
+  a.relpersistence OPERATOR(pg_catalog.=) 'p' 
+  AND a.segrelid OPERATOR(pg_catalog.=) b.segrelid 
+  AND a.relnamespace OPERATOR(pg_catalog.=) n.oid;`)
 	if err != nil {
 		return nil, fmt.Errorf("unable to get ao/aocs tables %v", err)
 	}
@@ -246,7 +272,7 @@ func (checker *AOLengthCheckSegmentHandler) getTableMetadataEOF(ctx context.Cont
 	if !strings.Contains(row.SegRelName, "aocs") {
 		query = fmt.Sprintf("SELECT sum(eof) FROM pg_aoseg.%s", row.SegRelName)
 	} else {
-		query = fmt.Sprintf("SELECT sum(eof) FROM gp_toolkit.__gp_aocsseg('\"%s\"')", row.TableName)
+		query = fmt.Sprintf("SELECT sum(eof) FROM gp_toolkit.__gp_aocsseg('%s')", row.TableName)
 	}
 
 	size, err := conn.Query(ctx, query)
