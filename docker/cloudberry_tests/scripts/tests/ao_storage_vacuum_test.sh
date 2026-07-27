f7ef63c01536302b0dc9ad11b47197760dfff9a6
debebantur <stepan.filippov@yandex.ru>
2026-07-27 09:31:04 +0000

    fix cloudberry test

--- docker/cloudberry_tests/scripts/tests/ao_storage_vacuum_test.sh
+++ docker/cloudberry_tests/scripts/tests/ao_storage_vacuum_test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -e -x
 CONFIG_FILE="/tmp/configs/ao_storage_test_config.json"
 
@@ -15,12 +16,12 @@ setup_wal_archiving
 wal-g --config=${TMP_CONFIG} delete everything FORCE --confirm
 
 # 1st full backup (init tables ao, co)
-psql -p 6000 -c "DROP DATABASE IF EXISTS test"
-psql -p 6000 -c "CREATE DATABASE test"
-psql -p 6000 -d test -c "CREATE TABLE ao(a int, b int) WITH (appendoptimized = true) DISTRIBUTED BY (a);"
-psql -p 6000 -d test -c "CREATE TABLE co(a int, b int) WITH (appendoptimized = true, orientation = column) DISTRIBUTED BY (a);"
-psql -p 6000 -d test -c "INSERT INTO co select i, i FROM generate_series(1,1000000)i;"
-psql -p 6000 -d test -c "INSERT INTO ao select i, i FROM generate_series(1,1000000)i;"
+psql -p 7000 -c "DROP DATABASE IF EXISTS test"
+psql -p 7000 -c "CREATE DATABASE test"
+psql -p 7000 -d test -c "CREATE TABLE ao(a int, b int) WITH (appendoptimized = true) DISTRIBUTED BY (a);"
+psql -p 7000 -d test -c "CREATE TABLE co(a int, b int) WITH (appendoptimized = true, orientation = column) DISTRIBUTED BY (a);"
+psql -p 7000 -d test -c "INSERT INTO co select i, i FROM generate_series(1,1000000)i;"
+psql -p 7000 -d test -c "INSERT INTO ao select i, i FROM generate_series(1,1000000)i;"
 run_backup_logged ${TMP_CONFIG} ${PGDATA} "--full"
 var=$(wal-g --config=${TMP_CONFIG} st ls segments_005/seg0/basebackups_005/aosegments/ | grep aoseg | wc -l)
 if [ "$var" -ne 3 ] ; then
@@ -29,10 +30,10 @@ if [ "$var" -ne 3 ] ; then
 fi
 
 # 2nd backup (populate ao, co tables without deleting)
-psql -p 6000 -d test -c "INSERT INTO ao select i, i FROM generate_series(1000000,2000000)i;"
-psql -p 6000 -d test -c "INSERT INTO co select i, i FROM generate_series(1000000,2000000)i;"
-psql -p 6000 -d test -c "BEGIN; INSERT INTO ao select i, i FROM generate_series(10000000,20000000)i; SELECT pg_sleep(20); ROLLBACK;" &
-psql -p 6000 -d test -c "BEGIN; INSERT INTO co select i, i FROM generate_series(10000000,20000000)i; SELECT pg_sleep(20); ROLLBACK;" &
+psql -p 7000 -d test -c "INSERT INTO ao select i, i FROM generate_series(1000000,2000000)i;"
+psql -p 7000 -d test -c "INSERT INTO co select i, i FROM generate_series(1000000,2000000)i;"
+psql -p 7000 -d test -c "BEGIN; INSERT INTO ao select i, i FROM generate_series(10000000,20000000)i; SELECT pg_sleep(20); ROLLBACK;" &
+psql -p 7000 -d test -c "BEGIN; INSERT INTO co select i, i FROM generate_series(10000000,20000000)i; SELECT pg_sleep(20); ROLLBACK;" &
 sleep 5
 run_backup_logged ${TMP_CONFIG} ${PGDATA}
 sleep 15
@@ -48,11 +49,11 @@ if [ "$var" -ne 3 ] ; then
 fi
 
 # 3rd backup (repopulate ao and co tables after delete and vacuum)
-psql -p 6000 -d test -c "DELETE FROM co WHERE a>0;"
-psql -p 6000 -d test -c "DELETE FROM ao WHERE a>0;"
-psql -p 6000 -d test -c "VACUUM FULL;"
-psql -p 6000 -d test -c "INSERT INTO ao select i, i FROM generate_series(10000000,20000000)i;"
-psql -p 6000 -d test -c "INSERT INTO co select i, i FROM generate_series(10000000,20000000)i;"
+psql -p 7000 -d test -c "DELETE FROM co WHERE a>0;"
+psql -p 7000 -d test -c "DELETE FROM ao WHERE a>0;"
+psql -p 7000 -d test -c "VACUUM FULL;"
+psql -p 7000 -d test -c "INSERT INTO ao select i, i FROM generate_series(10000000,20000000)i;"
+psql -p 7000 -d test -c "INSERT INTO co select i, i FROM generate_series(10000000,20000000)i;"
 run_backup_logged ${TMP_CONFIG} ${PGDATA}
 var=$(wal-g --config=${TMP_CONFIG} st ls segments_005/seg0/basebackups_005/aosegments/ | grep aoseg | wc -l)
 if [ "$var" -ne 12 ] ; then
@@ -70,16 +71,15 @@ stop_and_delete_cluster_dir
 
 wal-g backup-fetch LATEST --in-place --config=${TMP_CONFIG}
 start_cluster
-sleep 5
-psql -p 6000 -d test -c "SELECT COUNT(*) FROM ao;"
+psql -p 7000 -d test -c "SELECT COUNT(*) FROM ao;"
 
-psql -p 6000 -d test -c "SELECT COUNT(*) FROM ao;" | grep 10000001 && EXIT_STATUS=$? || EXIT_STATUS=$?
+psql -p 7000 -d test -c "SELECT COUNT(*) FROM ao;" | grep 10000001 && EXIT_STATUS=$? || EXIT_STATUS=$?
 if [ "$EXIT_STATUS" -ne 0 ] ; then
     echo "Error: Failed to read from ao table after restore"
     exit 1
 fi
 
-psql -p 6000 -d test -c "SELECT COUNT(*) FROM co;" | grep 10000001 && EXIT_STATUS=$? || EXIT_STATUS=$?
+psql -p 7000 -d test -c "SELECT COUNT(*) FROM co;" | grep 10000001 && EXIT_STATUS=$? || EXIT_STATUS=$?
 if [ "$EXIT_STATUS" -ne 0 ] ; then
     echo "Error: Failed to read from co table after restore"
     exit 1
