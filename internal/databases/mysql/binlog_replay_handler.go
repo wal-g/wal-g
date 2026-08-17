a064a7377e9a7debbb7544495c9bfad6cb951e29
Dmytro Opria <Dmytro.Opria_ext@external.mail.schwarz>
2026-08-17 17:03:58 +0200

    mysql: track binlog position and GTID from backup sentinel for replay

--- internal/databases/mysql/binlog_replay_handler.go
+++ internal/databases/mysql/binlog_replay_handler.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"strconv"
 	"time"
 
 	"github.com/pkg/errors"
@@ -18,14 +19,16 @@ import (
 const binlogFetchAhead = 2
 
 type replayHandler struct {
-	logCh chan string
-	errCh chan error
-	endTS string
+	logCh           chan string
+	errCh           chan error
+	endTS           string
+	backupBinlogPos BinlogPos
 }
 
-func newReplayHandler(ctx context.Context, endTS time.Time) *replayHandler {
+func newReplayHandler(ctx context.Context, endTS time.Time, backupBinlogPos BinlogPos) *replayHandler {
 	rh := new(replayHandler)
 	rh.endTS = endTS.Local().Format(TimeMysqlFormat)
+	rh.backupBinlogPos = backupBinlogPos
 	rh.logCh = make(chan string, binlogFetchAhead)
 	rh.errCh = make(chan error, 1)
 	go rh.replayLogs(ctx)
@@ -34,11 +37,13 @@ func newReplayHandler(ctx context.Context, endTS time.Time) *replayHandler {
 
 func (rh *replayHandler) replayLogs(ctx context.Context) {
 	for binlogPath := range rh.logCh {
-		tracelog.InfoLogger.Printf("replaying %s ...", path.Base(binlogPath))
+		binlogName := path.Base(binlogPath)
+
+		tracelog.InfoLogger.Printf("replaying %s ...", binlogName)
 		err := rh.replayLog(ctx, binlogPath)
 		os.Remove(binlogPath)
 		if err != nil {
-			tracelog.ErrorLogger.Printf("failed to replay %s: %v", path.Base(binlogPath), err)
+			tracelog.ErrorLogger.Printf("failed to replay %s: %v", binlogName, err)
 			rh.errCh <- err
 			break
 		}
@@ -54,7 +59,24 @@ func (rh *replayHandler) replayLog(ctx context.Context, binlogPath string) error
 	env := os.Environ()
 	env = append(env,
 		fmt.Sprintf("%s=%s", "WALG_MYSQL_CURRENT_BINLOG", binlogPath),
-		fmt.Sprintf("%s=%s", "WALG_MYSQL_BINLOG_END_TS", rh.endTS))
+		fmt.Sprintf("%s=%s", "WALG_MYSQL_BINLOG_END_TS", rh.endTS),
+	)
+
+	if rh.backupBinlogPos.LastGTID != "" {
+		env = append(env, fmt.Sprintf("%s=%s", "WALG_MYSQL_BINLOG_LAST_GTID", rh.backupBinlogPos.LastGTID))
+	}
+
+	// Safe even though this may not be the first binlog fetched: the
+	// backup tool guarantees everything before startPosition in this
+	// specific file is already covered by the backup.
+	startPosition := rh.backupBinlogPos.FilePosition
+	binlogName := path.Base(binlogPath)
+	if binlogName == rh.backupBinlogPos.FileName && startPosition > 0 {
+		tracelog.InfoLogger.Printf("replaying %s from position %d (backup boundary)", binlogName, startPosition)
+		env = append(env,
+			fmt.Sprintf("%s=%s", "WALG_MYSQL_BINLOG_START_POSITION", strconv.FormatInt(startPosition, 10)),
+		)
+	}
 	cmd.Env = env
 	return cmd.Run()
 }
@@ -77,10 +99,22 @@ func HandleBinlogReplay(ctx context.Context, folder storage.Folder, backupName s
 	dstDir, err := internal.GetLogsDstSettings(conf.MysqlBinlogDstSetting)
 	tracelog.ErrorLogger.FatalOnError(err)
 
-	startTS, endTS, endBinlogTS, err := getTimestamps(ctx, folder, backupName, untilTS, untilBinlogLastModifiedTS)
+	backup, err := internal.GetBackupByName(ctx, backupName, utility.BaseBackupPath, folder)
+	tracelog.ErrorLogger.FatalfOnError("Unable to get backup: %v", err)
+
+	var sentinel StreamSentinelDto
+	err = backup.FetchSentinel(ctx, &sentinel)
+	tracelog.ErrorLogger.FatalfOnError("Unable to fetch sentinel: %v", err)
+
+	startTS, endTS, endBinlogTS, err := getTimestampsFromSentinel(ctx, folder, &sentinel, backup.Name, untilTS, untilBinlogLastModifiedTS)
 	tracelog.ErrorLogger.FatalOnError(err)
 
-	handler := newReplayHandler(ctx, endTS)
+	pos := sentinel.GetBinlogPos()
+	if pos.FileName != "" {
+		tracelog.InfoLogger.Printf("Backup binlog boundary: file=%s position=%d gtid=%s", pos.FileName, pos.FilePosition, pos.LastGTID)
+	}
+
+	handler := newReplayHandler(ctx, endTS, pos)
 
 	tracelog.InfoLogger.Printf("Fetching binlogs since %s until %s", startTS, endTS)
 	err = fetchLogs(ctx, folder, dstDir, startTS, endTS, endBinlogTS, handler)
@@ -97,7 +131,18 @@ func getTimestamps(ctx context.Context,
 		return time.Time{}, time.Time{}, time.Time{}, errors.Wrap(err, "Unable to get backup")
 	}
 
-	startTS, err := getBinlogSinceTS(ctx, folder, backup)
+	var sentinel StreamSentinelDto
+	err = backup.FetchSentinel(ctx, &sentinel)
+	tracelog.ErrorLogger.FatalfOnError("Unable to fetch sentinel: %v", err)
+
+	return getTimestampsFromSentinel(ctx, folder, &sentinel, backup.Name, untilTS, untilBinlogLastModifiedTS)
+}
+
+func getTimestampsFromSentinel(
+	ctx context.Context, folder storage.Folder, sentinel *StreamSentinelDto,
+	backupName, untilTS, untilBinlogLastModifiedTS string,
+) (time.Time, time.Time, time.Time, error) {
+	startTS, err := getBinlogSinceTS(ctx, folder, backupName, sentinel)
 	if err != nil {
 		return time.Time{}, time.Time{}, time.Time{}, err
 	}
